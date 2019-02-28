55b60d65f765177be043377828248e7ceb9fc244
himyu <himyuuuu1@gmail.com>
2019-03-01 03:37:53 +0900

    BOJ 1980 AC

--- PS.cpp
+++ PS.cpp
@@ -18,6 +18,23 @@ typedef long long ll;
 typedef pair<int, int> pi;
 typedef tuple<int, int, int> ti;
 
-int main() {
+vector<pi> states;
 
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  // Code Start
+  int n,m,t;
+  cin>>n>>m>>t;
+  int cnt=0;
+  while(true) {
+    if(n*cnt>t) break;
+    states.push_back(pi((t-n*cnt)%m, -(cnt+(t-n*cnt)/m)));
+    cnt++;
+  }
+  sort(states.begin(), states.end());
+  cout<<-states[0].second<<" "<<states[0].first<<endl;
+  return 0;
 }
