4d0e86336bd08ebeea9a4ca9c9b3f2a708499828
himyu <himyuuuu1@gmail.com>
2019-03-03 01:50:08 +0900

    Cpp template update

--- PS.cpp
+++ PS.cpp
@@ -14,27 +14,28 @@
 #include <cstdlib>
 
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef tuple<int, int, int> ti;
+using ll = long long;
+using pi = pair<int, int>;
+using ti = tuple<int, int, int>;
+using pli = pair<ll, ll>;
+using pti = tuple<ll, ll, ll>;
 
-vector<pi> states;
+// Classes
 
+
+// Global Variables
+
+
+// Function
+
+
+// Main
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
   // Code Start
-  int n,m,t;
-  cin>>n>>m>>t;
-  int cnt=0;
-  while(true) {
-    if(n*cnt>t) break;
-    states.push_back(pi((t-n*cnt)%m, -(cnt+(t-n*cnt)/m)));
-    cnt++;
-  }
-  sort(states.begin(), states.end());
-  cout<<-states[0].second<<" "<<states[0].first<<endl;
+
   return 0;
 }
