8a615aeaa1e22f5a105638798f91760981a84a25
himyu <himyuuuu1@gmail.com>
2019-03-03 02:35:54 +0900

    BOJ 10931 AC

--- PS.py
+++ PS.py
@@ -1,6 +1,4 @@
-for i in range(int(input())):
-  s=[1,2,4]
-  t=int(input())
-  l=lambda x:x if x>0 else 0
-  exec("s.append((s[-1]+s[-2]+s[-3]));"*l(t-3))
-  print(s[t-1])
+import hashlib
+m=hashlib.sha384()
+m.update(input().encode())
+print(m.hexdigest())
