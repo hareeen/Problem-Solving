42bebf86f56d5c94101ccf6a05b4bf6dbfe0adf1
himyu <himyuuuu1@gmail.com>
2019-03-03 01:57:36 +0900

    BOJ 14241 AC

--- PS.cpp
+++ PS.cpp
@@ -24,7 +24,8 @@ using pti = tuple<ll, ll, ll>;
 
 
 // Global Variables
-
+int N;
+vector<int> slimes;
 
 // Function
 
@@ -36,6 +37,20 @@ int main() {
   cout.tie(NULL);
 
   // Code Start
+  cin>>N;
+  for(int i=0;i<N;i++) {
+    int _t;
+    cin>>_t;
+    slimes.push_back(_t);
+  }
+  sort(slimes.begin(), slimes.end(), greater<int>());
 
+  int _merged=slimes[0]+slimes[1], res=slimes[0]*slimes[1];
+  for(int i=2;i<N;i++) {
+    res+=(_merged*slimes[i]);
+    _merged+=slimes[i];
+  }
+
+  cout<<res<<endl;
   return 0;
 }
