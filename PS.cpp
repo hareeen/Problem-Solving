4710724fcb849b78a75e4f79a667f69a8d9ea539
himyu <himyuuuu1@gmail.com>
2019-03-05 01:56:02 +0900

    BOJ 1260 AC

--- PS.cpp
+++ PS.cpp
@@ -23,41 +23,66 @@ using pli = pair<ll, ll>;
 using pti = tuple<ll, ll, ll>;
 
 // Classes
-int C, B;
-vector<int> calories;
+int N, M, V;
+vector<vector<int> > adjaList;
+vector<bool> visited;
 
 // Global Variables
 
 
 // Function
+void DFS(int _v) {
+	cout<<_v+1<<" ";
+	visited[_v]=true;
+	for(auto i:adjaList[_v]) if(!visited[i]) DFS(i);
+	return;
+}
 
+void BFS(int _v) {
+	visited[_v]=true;
+	queue<int> _que;
+	_que.push(_v);
+	while(!_que.empty()) {
+		for(auto i:adjaList[_que.front()]) {
+			if(!visited[i]) {
+				_que.push(i);
+				visited[i]=true;
+			}
+		}
+		cout<<_que.front()+1<<" ";
+		_que.pop();
+	}
+}
 
 // Main
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-  // Code Start
-  cin>>C>>B;
-  for(int i=0;i<B;i++) {
-    int _t;
-    cin>>_t;
-    calories.push_back(_t);
-  }
+	// Code Start
+	cin>>N>>M>>V;
+	V--;
+	adjaList.resize(N);
+	for(int i=0; i<M; i++) {
+		int _v1, _v2;
+		cin>>_v1>>_v2;
+		_v1--; _v2--;
+		adjaList[_v1].push_back(_v2);
+		adjaList[_v2].push_back(_v1);
+	}
 
-  bitset<21> _bits;
-  _bits.set();
-  auto _pow=_bits.to_ulong();
-  int _max=-1;
+	for(int i=0;i<N;i++) sort(adjaList[i].begin(), adjaList[i].end());
 
-  for(uint i=0;i<=_pow;i++) {
-    _bits=bitset<21>(i);
-    auto _sum=0;
-    for(int j=0;j<B;j++) _sum+=_bits[j]?calories[j]:0;
-    _max=max(_max, _sum>C?-1:_sum);
-  }
+	visited.clear();
+	visited.resize(N);
+	DFS(V);
+	cout<<endl;
 
-  cout<<_max<<endl;
-  return 0;
+	visited.clear();
+	visited.resize(N);
+	BFS(V);
+	cout<<endl;
+
+	return 0;
 }
