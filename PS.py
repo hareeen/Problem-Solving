2614bf5241e32bd4c7592385dc4914e873afd79b
himyu <himyuuuu1@gmail.com>
2019-03-07 01:50:56 +0900

    BOJ 2292 AC

--- PS.py
+++ PS.py
@@ -1,4 +1,4 @@
-import hashlib
-m=hashlib.sha384()
-m.update(input().encode())
-print(m.hexdigest())
+from math import sqrt
+s=lambda x: int(sqrt(x))
+x=(int(input())-2)//6*2
+print(1 if x<0 else s(x-s(x))+2)
