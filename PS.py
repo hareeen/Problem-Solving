94c65b5596ce7974c2e0ba9510d02fde38fb2e21
himyu <himyuuuu1@gmail.com>
2019-04-30 20:37:45 +0900

    BOJ 13119 AC

--- PS.py
+++ PS.py
@@ -1,8 +1,23 @@
-def vps(s):
-  a=[0,0]
-  for i in s:
-    a[int(i=='(')]+=1
-    if a[0]>a[1]: return False
-  return a[0]==a[1]
-for i in range(int(input())):
-  print('YES' if vps(input()) else 'NO')
+N, M, X = map(int, input().split())
+Hl = list(map(int, input().split()))
+res = []
+for i in range(N):
+  res.append(['.'] * M)
+for i in range(M):
+  for j in range(N - Hl[i], N):
+    res[j][i] = '#'
+br_l = []
+for i in range(M):
+  if res[N - X][i] == '.':
+    if i % 3 == 2:
+      br_l.append(i)
+    res[N - X][i] = '-'
+  else:
+    res[N - X][i] = '*'
+for i in br_l:
+  for j in range(N - X + 1, N):
+    if res[j - 1][i] in ('-', '|') and res[j][i] != '#':
+      res[j][i] = '|'
+for i in range(N):
+  res[i] = ''.join(res[i])
+print('\n'.join(res))
