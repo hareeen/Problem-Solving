a5ae77e5465a67c71de402db0beeda5de2bef74e
himyu <himyuuuu1@gmail.com>
2019-05-04 01:16:40 +0900

    BOJ 8741 AC

--- PS.py
+++ PS.py
@@ -1,16 +1,6 @@
-def process(case_num):
-  N = int(input())
-  s = ''.join(map(lambda x: '0' if x == 'O' else '1', input()))
-  res = []
-  for i in range(N):
-    res.append(chr(int(s[8 * i:8 * (i + 1)], 2)))
-  print('Case #{}: {}'.format(case_num + 1, ''.join(res)))
-
-
 def main():
-  TC = int(input())
-  for _ in range(TC):
-    process(_)
+  n = int(input())
+  print('1' * n + '0' * (n - 1))
 
 
 main()
