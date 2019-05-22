e1e3a751296f850168e7318e4bea2540baff01e6
himyu <himyuuuu1@gmail.com>
2019-05-22 19:32:04 +0900

    BOJ 1157 AC

--- PS.py
+++ PS.py
@@ -1,8 +1,13 @@
 def main():
-  tc = int(input())
-  for _ in range(tc):
-    n, s = input().split()
-    print(''.join(map(lambda x: x * int(n), list(s))))
+  l = []
+  s = list(map(ord, input().upper()))
+  for i in range(65, 65 + 26):
+    l.append((s.count(i), chr(i)))
+  l.sort(reverse=True)
+  if l[0][0]==l[1][0]:
+    print('?')
+  else:
+    print(l[0][1])
 
 
 main()
