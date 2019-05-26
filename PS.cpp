0aff30ad965c063c6c1a14341a0f47d221468e32
himyu <himyuuuu1@gmail.com>
2019-05-26 17:53:02 +0900

    BOJ 1780 AC

--- PS.cpp
+++ PS.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <algorithm>
 #include <functional>
+#include <numeric>
 
 #include <cmath>
 #include <cstdio>
@@ -19,17 +20,58 @@ using pli = pair<i64, i64>;
 using ti  = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-void hanoi(int current, int target, int slates) {
-  int remain = 6 - target - current;
+int dx[9] = { 0, 1, 2, 0, 1, 2, 0, 1, 2 };
+int dy[9] = { 0, 0, 0, 1, 1, 1, 2, 2, 2 };
 
-  if(slates == 1) {
-    cout << current << " " << target << '\n';
-    return;
-  }
-  hanoi(current, remain, slates - 1);
-  cout << current << " " << target << '\n';
-  hanoi(remain, target, slates - 1);
+int _sum(vector<int> arr) {
+  int _s = 0;
+
+  return accumulate(arr.begin(), arr.end(), _s);
 }
+
+int isSimple(vector<int> arr) {
+  if(_sum(arr) != 1) return -1;
+
+  for(int i = 0; i < arr.size(); i++) {
+    if(arr[i] == 1) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+vector<int> paper(vector<vector<int> > &data, pi st, pi ed) {
+  auto sz = ed.first - st.first + 1;
+
+  if(sz == 1) {
+    vector<int> ret(3);
+    ret[data[st.first][st.second]]++;
+    return ret;
+  }
+  sz /= 3;
+  vector<vector<int> > res;
+  for(int i = 0; i < 9; i++) {
+    res.push_back(paper(data, pi(st.first + sz * dx[i], st.second + sz * dy[i]),
+      pi(ed.first - sz * dx[8 - i], ed.second - sz * dy[8 - i])));
+  }
+
+  vector<int> ret(3);
+  int tpe = -2;
+  bool compressAble = true;
+  for(auto &i:res) {
+    auto t = isSimple(i);
+    if(t == -1) compressAble = false;
+    if(tpe == -2) tpe = t;
+    else compressAble &= (tpe == t);
+    for(int j = 0; j < 3; j++) ret[j] += i[j];
+  }
+  if(compressAble) {
+    ret.clear();
+    ret.resize(3);
+    ret[tpe]++;
+  }
+  return ret;
+} // paper
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -38,6 +80,15 @@ int main() {
 
   int N;
   cin >> N;
-  cout << static_cast<int>(pow(2, N) - 1) << endl;
-  hanoi(1, 3, N);
+  vector<vector<int> > data(N, vector<int>(N));
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j < N; j++) {
+      int a;
+      cin >> a;
+      data[i][j] = a + 1;
+    }
+  }
+
+  for(auto &i:paper(data, pi(0, 0), pi(N - 1, N - 1))) cout << i << '\n';
+  return 0;
 } // main
