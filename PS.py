b67830bba782553386d56eb76ca5da1cfd14b42d
himyu <himyuuuu1@gmail.com>
2019-06-02 12:57:25 +0900

    BOJ 11052 AC

--- PS.py
+++ PS.py
@@ -1,1 +1,17 @@
-n=int(input());x=1;z=3;exec("x,z=z,z+x*2;"*(n-2));print(1 if n==1 else z%10007)
+N=int(input())
+l=list(map(int, input().split()))
+dp=[]
+for _ in range(N):
+  dp.append([-10**10]*(N+1))
+for i in range(N):
+  for j in range(N+1):
+    if i==0:
+      dp[i][j]=l[i]*j
+    else:
+      M=-10**10
+      k=0
+      while j-k*(i+1)>=0:
+        M=max(M, dp[i-1][j-k*(i+1)]+l[i]*k)
+        k+=1
+      dp[i][j]=M
+print(max(dp[-1]))
