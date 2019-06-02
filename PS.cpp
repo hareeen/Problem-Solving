123c4bbe9800782b5df964dd78c5d160307b2848
himyu <himyuuuu1@gmail.com>
2019-06-02 10:18:55 +0900

    BOJ 10827 AC

--- PS.cpp
+++ PS.cpp
@@ -20,44 +20,101 @@ using pli = pair<i64, i64>;
 using ti  = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-using pii = pair<pi, int>;
+using psi = pair<string, int>;
+
+void addZero(string &s, int n) {
+  for(int i = 0; i < n; i++) s += '0';
+}
+
+void addDigit(psi &t, int adder) {
+  addZero(t.first, adder);
+  t.second += adder;
+}
+
+char ntoCh(int n) {
+  return static_cast<char>(48 + n);
+}
+
+int chtoN(char ch) {
+  return static_cast<int>(ch - '0');
+}
+
+psi add_(psi l, psi r) {
+  if(l.second > r.second) swap(l, r);
+  addDigit(l, r.second - l.second);
+  if(l.first.length() > r.first.length()) swap(l, r);
+  string ret;
+  int carry = 0;
+  string st_l = l.first, st_r = r.first;
+  int lenL = st_l.length(), lenR = st_r.length();
+  reverse(st_l.begin(), st_l.end());
+  reverse(st_r.begin(), st_r.end());
+  for(int i = 0; i < lenR; i++) {
+    int plus;
+    if(i < lenL) plus = carry + chtoN(st_l[i]) + chtoN(st_r[i]);
+    else plus = carry + chtoN(st_r[i]);
+    ret  += ntoCh(plus % 10);
+    carry = plus / 10;
+  }
+  if(carry != 0) ret += ntoCh(carry);
+  reverse(ret.begin(), ret.end());
+  return psi(ret, l.second);
+}
+
+string mul_one(string s, int n) {
+  string ret;
+  int carry = 0;
+
+  reverse(s.begin(), s.end());
+  for(const auto &i:s) {
+    int plus = carry + chtoN(i) * n;
+    ret  += ntoCh(plus % 10);
+    carry = plus / 10;
+  }
+  if(carry != 0) ret += ntoCh(carry);
+  reverse(ret.begin(), ret.end());
+  return ret;
+}
+
+psi mul_(psi l, psi r) {
+  if(l.first.length() > r.first.length()) swap(l, r);
+  string st_l = l.first, st_r = r.first;
+  reverse(st_l.begin(), st_l.end());
+  psi ret = psi(string("0"), 0);
+  for(int i = 0; i < st_l.length(); i++) {
+    int ntomul   = chtoN(st_l[i]);
+    string toAdd = mul_one(st_r, ntomul);
+    addZero(toAdd, i);
+    ret = add_(ret, psi(toAdd, 0));
+  }
+  return psi(ret.first, l.second + r.second);
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
+  string a;
   int n;
-  cin >> n;
-  vector<pi> v1, v2;
-  vector<pii> ans;
-  for(int i = 0; i < n; i++) {
-    int a;
-    cin >> a;
-    v1.push_back(pi(a, i));
+  string filtered;
+  int digit;
+  cin >> a >> n;
+  for(int i = 0; i < a.length(); i++) {
+    if(a[i] == '.') digit = a.length() - i - 1;
+    else filtered += a[i];
   }
-  for(int i = 0; i < n; i++) {
-    int a;
-    cin >> a;
-    v2.push_back(pi(a, i));
+  psi toCalc(filtered, digit);
+  psi result("10", 0);
+  while(n > 0) {
+    if(n % 2) result = mul_(result, toCalc);
+    toCalc = mul_(toCalc, toCalc);
+    n     /= 2;
   }
-  sort(v1.begin(), v1.end(), greater<pair<int, int> >());
-  sort(v2.begin(), v2.end(), greater<pair<int, int> >());
-  for(int i = 0; i < n - 1; i++) {
-    int diff = v1[i].first - v2[i].first;
-    if(diff < 0) {
-      cout << "NO" << endl;
-      return 0;
-    }
-    ans.push_back(pii(pi(v1[i].second, v1[i + 1].second), diff));
-    v1[i].first     -= diff;
-    v1[i + 1].first += diff;
+  for(int i = 0; i < result.first.length() - 1; i++) {
+    if(i == result.first.length() - result.second - 1) cout << '.';
+    cout << result.first[i];
   }
-  if(v1.back().first != v2.back().first) {
-    cout << "NO" << endl;
-    return 0;
-  }
-  cout << ans.size() << endl;
-  for(auto i:ans) cout << i.first.first << " " << i.first.second << " " << i.second << '\n';
+  cout << endl;
   return 0;
 } // main
