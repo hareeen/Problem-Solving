5ac5ea38fb1bbfd4c6f076d1fe9a49e615a5bcb9
himyu <himyuuuu1@gmail.com>
2019-06-02 12:38:41 +0900

    BOJ 11727 AC

--- PS.py
+++ PS.py
@@ -1,11 +1,1 @@
-N = int(input())
-dp = [[0] + [1] * 9]
-for i in range(1, N):
-  apd = [0] * 10
-  for j in range(10):
-    if j in (0, 9):
-      apd[j] = dp[-1][7 * (j // 9) + 1]
-    else:
-      apd[j] = dp[-1][j - 1] + dp[-1][j + 1]
-  dp.append(apd)
-print(sum(dp[-1]) % 1000000000)
+n=int(input());x=1;z=3;exec("x,z=z,z+x*2;"*(n-2));print(1 if n==1 else z%10007)
