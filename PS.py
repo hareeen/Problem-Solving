01e59c6388b96cdf0007e492ceb2b9f62cd933f4
himyu <himyuuuu1@gmail.com>
2019-06-02 12:28:38 +0900

    BOJ 1932 AC

--- PS.py
+++ PS.py
@@ -1,4 +1,13 @@
-a,b,c=map(int, input().split())
-if a>b: a,b=b,a
-b=min(a+1, b)
-print(a+b+2*c)
+N = int(input())
+l = []
+dp = []
+for i in range(N):
+  l.append(list(map(int, input().split())))
+  dp.append([0] * (i + 1))
+dp[0][0] = l[0][0]
+for i in range(1, N):
+  dp[i][0] = dp[i - 1][0] + l[i][0]
+  dp[i][i] = dp[i - 1][i - 1] + l[i][i]
+  for j in range(1, i):
+    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + l[i][j]
+print(max(dp[-1]))
