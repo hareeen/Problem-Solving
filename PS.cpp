d80084daca291e959009e57767334ec374338543
himyu <himyuuuu1@gmail.com>
2019-06-02 12:15:07 +0900

    BOJ 9251, 9252 AC

--- PS.cpp
+++ PS.cpp
@@ -20,25 +20,25 @@ using pli = pair<i64, i64>;
 using ti  = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
+using psi = pair<string, int>;
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N;
-  vector<int> input;
-  vector<int> lis;
-  cin >> N;
-  for(int i = 0; i < N; i++) {
-    int a;
-    cin >> a;
-    input.push_back(a);
+  string s1, s2;
+  cin >> s1 >> s2;
+  vector<vector<psi> > vec(s1.length() + 1, vector<psi>(s2.length() + 1));
+  for(int i = 0; i < s1.length(); i++) {
+    for(int j = 0; j < s2.length(); j++) {
+      if(s1[i] == s2[j]) { vec[i + 1][j + 1] = psi(vec[i][j].first + s1[i], vec[i][j].second + 1); } else {
+        if(vec[i + 1][j].second > vec[i][j + 1].second) vec[i + 1][j + 1] = vec[i + 1][j];
+        else vec[i + 1][j + 1] = vec[i][j + 1];
+      }
+    }
   }
-  for(const auto &i:input) {
-    auto it = lower_bound(lis.begin(), lis.end(), i, greater<int>());
-    if(it == lis.end()) lis.push_back(i);
-    else *it = i;
-  }
-  cout << lis.size() << endl;
+  cout << vec[s1.length()][s2.length()].second << endl;
+  cout << vec[s1.length()][s2.length()].first << endl;
   return 0;
 } // main
