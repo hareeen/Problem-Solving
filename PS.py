8e34b500298840f9252340bc98a5369b4dd08c1a
himyu <himyuuuu1@gmail.com>
2019-06-08 10:20:25 +0900

    BOJ workbook 2052 all AC

--- PS.py
+++ PS.py
@@ -1,35 +1,7 @@
-import queue
+import itertools
 
-
-def tup_plus(a: tuple, b: tuple):
-    c = list()
-    for i in range(len(a)):
-        c.append(a[i]+b[i])
-    return tuple(c)
-
-
-def main():
-    N, M = map(int, input().split())
-    mp = []
-    for _ in range(N):
-        mp.append(list(map(bool, list(input()))))
-    que = queue.Queue()
-    que.put(((0, 0), 1))
-    while que.qsize():
-        curtup = que.get()
-        if curtup[0] == (N-1, M-1):
-            print(curtup[1])
-            break
-
-        def check(tup, plus):
-            ret = tup_plus(tup, plus)
-            return (ret[0] >= 0 and ret[0] < N and ret[1] >= 0 and ret[1] < M and mp[ret[0]][ret[1]] == 1)
-        for i in range(4):
-            pl = [(0, -1), (0, 1), (1, 0), (-1, 0)][i]
-            if check(curtup[0], pl):
-                que.put((tup_plus(curtup[0], pl), curtup[1]+1))
-        mp[curtup[0][0]][curtup[0][1]] = 0
-    return
-
-
-main()
+N, M = map(int, input().split())
+l = list(map(int, input().split()))
+res = list(set(itertools.combinations_with_replacement(sorted(l), M)))
+for i in sorted(res):
+    print(' '.join(map(str, i)))
