244654c02ad781c7271940292bc3b16e906971fe
himyu <himyuuuu1@gmail.com>
2019-06-10 17:51:32 +0900

    BOJ 1934 AC

--- PS.py
+++ PS.py
@@ -1,17 +1,7 @@
-l = 'qwertyuiopasdfghjklzxcvbnm'
-s = 0
+def gcd(a, b):
+    return a if b == 0 else gcd(b, a % b)
+
+
 for i in range(int(input())):
-    wd = input()
-    flg = True
-    for c in l:
-        if c in wd:
-            lst = -1
-            for j in range(len(wd)):
-                if wd[j] == c:
-                    if lst != -1 and lst != j-1:
-                        flg = False
-                    else:
-                        lst = j
-    if flg:
-        s += 1
-print(s)
+    a, b = map(int, input().split())
+    print(a*b//gcd(a, b))
