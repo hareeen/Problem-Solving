49cb33d4f67e201e22fcc91971fd87ab319afe55
himyu <himyuuuu1@gmail.com>
2019-06-10 16:17:26 +0900

    BOJ 7576 AC

--- PS.cpp
+++ PS.cpp
@@ -20,12 +20,19 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-void que_append(queue<pi> &que, vector<bool> &check, int appd, int cur_t)
+using pii = pair<pi, int>;
+
+void que_append(pii to_append, int &M, int &N, vector<vector<int>> &mp, vector<vector<bool>> &check, queue<pii> &que)
 {
-  if (appd >= 0 && appd < check.size() && !check[appd])
+  auto appendDeco = to_append.first;
+  auto appendDay = to_append.second;
+  if (appendDeco.first >= 0 && appendDeco.first < N &&
+      appendDeco.second >= 0 && appendDeco.second < M &&
+      !check[appendDeco.first][appendDeco.second])
   {
-    que.push(pi(appd, cur_t + 1));
-    check[appd] = true;
+    que.push(to_append);
+    check[appendDeco.first][appendDeco.second] = true;
+    mp[appendDeco.first][appendDeco.second] = appendDay;
   }
   return;
 }
@@ -36,24 +43,56 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N, K;
-  cin >> N >> K;
-  queue<pi> que;
-  que.push(pi(N, 0));
-  vector<bool> check(200001);
-  check[N] = true;
+  int M, N;
+  cin >> M >> N;
+  vector<vector<int>> mp(N);
+  vector<vector<bool>> check(N);
+  queue<pii> que;
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < M; j++)
+    {
+      int t;
+      cin >> t;
+      mp[i].push_back(t);
+      check[i].push_back((t != 0));
+      if (t == 1)
+        que.push(pii(pi(i, j), 0));
+    }
+  }
+  int dx[4] = {1, -1, 0, 0};
+  int dy[4] = {0, 0, 1, -1};
+  bool che_ini = true;
+  for (const auto &i : check)
+    for (const auto &j : i)
+      che_ini &= j;
+  if (che_ini)
+  {
+    cout << 0 << endl;
+    return 0;
+  }
   while (!que.empty())
   {
-    auto cur = que.front();
+    auto curDeco = que.front().first;
+    auto curDay = que.front().second;
     que.pop();
-    if (cur.first == K)
-    {
-      cout << cur.second << endl;
-      break;
-    }
-    que_append(que, check, cur.first + 1, cur.second);
-    que_append(que, check, cur.first - 1, cur.second);
-    que_append(que, check, cur.first * 2, cur.second);
+    for (int i = 0; i < 4; i++)
+      que_append(pii(pi(curDeco.first + dx[i], curDeco.second + dy[i]), curDay + 1), M, N, mp, check, que);
   }
+  int res = INT_MIN;
+  for (const auto &i : mp)
+  {
+    for (const auto &j : i)
+    {
+      res = max(res, j);
+      if (j == 0)
+      {
+        cout << -1 << endl;
+        return 0;
+      }
+    }
+  }
+
+  cout << res << endl;
   return 0;
 }
