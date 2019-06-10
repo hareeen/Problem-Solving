c7658fb655089fe78dc7a9dbcf62c2b0e4605e11
himyu <himyuuuu1@gmail.com>
2019-06-10 17:05:50 +0900

    BOJ 2667 AC

--- PS.cpp
+++ PS.cpp
@@ -20,92 +20,16 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-using psi = pair<string, int>;
-
-void addZero(string &s, int n)
+void que_append(pi apd_deco, int apd_clr, int &N, vector<vector<int>> &mp, vector<vector<bool>> &check, queue<pi> &que)
 {
-  for (int i = 0; i < n; i++)
-    s += '0';
-}
-
-void addDigit(psi &t, int adder)
-{
-  addZero(t.first, adder);
-  t.second += adder;
-}
-
-char ntoCh(int n)
-{
-  return static_cast<char>(48 + n);
-}
-
-int chtoN(char ch)
-{
-  return static_cast<int>(ch - '0');
-}
-
-psi add_(psi l, psi r)
-{
-  if (l.second > r.second)
-    swap(l, r);
-  addDigit(l, r.second - l.second);
-  if (l.first.length() > r.first.length())
-    swap(l, r);
-  string ret;
-  int carry = 0;
-  string st_l = l.first, st_r = r.first;
-  int lenL = st_l.length(), lenR = st_r.length();
-  reverse(st_l.begin(), st_l.end());
-  reverse(st_r.begin(), st_r.end());
-  for (int i = 0; i < lenR; i++)
+  if (apd_deco.first >= 0 && apd_deco.first < N &&
+      apd_deco.second >= 0 && apd_deco.second < N &&
+      !check[apd_deco.first][apd_deco.second])
   {
-    int plus;
-    if (i < lenL)
-      plus = carry + chtoN(st_l[i]) + chtoN(st_r[i]);
-    else
-      plus = carry + chtoN(st_r[i]);
-    ret += ntoCh(plus % 10);
-    carry = plus / 10;
+    que.push(apd_deco);
+    mp[apd_deco.first][apd_deco.second] = apd_clr;
+    check[apd_deco.first][apd_deco.second] = true;
   }
-  if (carry != 0)
-    ret += ntoCh(carry);
-  reverse(ret.begin(), ret.end());
-  return psi(ret, l.second);
-}
-
-string mul_one(string s, int n)
-{
-  string ret;
-  int carry = 0;
-
-  reverse(s.begin(), s.end());
-  for (const auto &i : s)
-  {
-    int plus = carry + chtoN(i) * n;
-    ret += ntoCh(plus % 10);
-    carry = plus / 10;
-  }
-  if (carry != 0)
-    ret += ntoCh(carry);
-  reverse(ret.begin(), ret.end());
-  return ret;
-}
-
-psi mul_(psi l, psi r)
-{
-  if (l.first.length() > r.first.length())
-    swap(l, r);
-  string st_l = l.first, st_r = r.first;
-  reverse(st_l.begin(), st_l.end());
-  psi ret = psi(string("0"), 0);
-  for (int i = 0; i < st_l.length(); i++)
-  {
-    int ntomul = chtoN(st_l[i]);
-    string toAdd = mul_one(st_r, ntomul);
-    addZero(toAdd, i);
-    ret = add_(ret, psi(toAdd, 0));
-  }
-  return psi(ret.first, l.second + r.second);
 }
 
 int main()
@@ -113,30 +37,54 @@ int main()
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  /*
-  string a;
-  int n;
-  string filtered;
-  int digit;
-  cin >> a >> n;
-  for(int i = 0; i < a.length(); i++) {
-    if(a[i] == '.') digit = a.length() - i - 1;
-    else filtered += a[i];
+
+  int N;
+  cin >> N;
+  vector<vector<int>> mp(N);
+  vector<vector<bool>> check(N);
+  queue<pi> que;
+  for (int i = 0; i < N; i++)
+  {
+    string s;
+    cin >> s;
+    for (auto &j : s)
+    {
+      mp[i].push_back(static_cast<int>(j - '0'));
+      check[i].push_back((j == '0'));
+    }
   }
-  psi toCalc(filtered, digit);
-  psi result("10", 0);
-  while(n > 0) {
-    if(n % 2) result = mul_(result, toCalc);
-    toCalc = mul_(toCalc, toCalc);
-    n     /= 2;
+
+  int apd_clr = 1;
+  int dx[4] = {0, 0, 1, -1};
+  int dy[4] = {1, -1, 0, 0};
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < N; j++)
+    {
+      if (!check[i][j])
+      {
+        que = queue<pi>();
+        que_append(pi(i, j), apd_clr, N, mp, check, que);
+        while (!que.empty())
+        {
+          auto cur = que.front();
+          que.pop();
+          for (int k = 0; k < 4; k++)
+            que_append(pi(cur.first + dx[k], cur.second + dy[k]), apd_clr, N, mp, check, que);
+        }
+        apd_clr++;
+      }
+    }
   }
-  for(int i = 0; i < result.first.length() - 1; i++) {
-    if(i == result.first.length() - result.second - 1) cout << '.';
-    cout << result.first[i];
-  }
-  */
-  string a, b;
-  cin >> a >> b;
-  cout << add_(psi(a, 0), psi(b, 0)).first << endl;
+  vector<int> clr(apd_clr - 1);
+  for (const auto &i : mp)
+    for (const auto &j : i)
+      if (j != 0)
+        clr[j - 1]++;
+
+  sort(clr.begin(), clr.end());
+  cout << clr.size() << endl;
+  for (const auto &i : clr)
+    cout << i << '\n';
   return 0;
 }
