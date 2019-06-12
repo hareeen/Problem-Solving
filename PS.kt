54dbbdceea483f0d36db6f7effa11a721a31eb74
himyu <himyuuuu1@gmail.com>
2019-06-12 19:47:54 +0900

    BOJ 1010 AC

--- PS.kt
+++ PS.kt
@@ -1,17 +1,13 @@
-import java.util.*
-import kotlin.math.max
-
-fun main(args:Array<String>) {
-  val sc=Scanner(System.`in`)
-  val N=sc.nextInt()
-  val arr=Array(N){sc.nextInt()}
-  val dp=Array(N){Array(2){-99999999}}
-  dp[0][0]=arr[0]
-  for(i in 1 until N) {
-    if(i>1) dp[i][0]=max(max(dp[i-2][0], dp[i-2][1]), 0)+arr[i]
-    else dp[i][0]=arr[i]
-    dp[i][1]=dp[i-1][0]+arr[i]
+fun main(args:Array<String>){with(java.util.Scanner(System.`in`)) {
+  for(tc in 1..nextInt()) {
+    val r=nextInt()
+    val n=nextInt()
+    val d=Array(n+1){Array(r+1){1}}
+    for(i in 1..n) {
+      for(j in 1..minOf(i,r)) {
+        d[i][j]=d[i-1][j-1]+(if(j!=i) d[i-1][j] else 0)
+      }
+    }
+    println(d[n][r])
   }
-  println(dp[N-1].max())
-  return
-}
+}}
