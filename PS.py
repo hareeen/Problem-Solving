c5d34cfc4d4ad401bfd2207b04cfbbfb0c815306
himyu <himyuuuu1@gmail.com>
2019-06-15 11:48:01 +0900

    BOJ 1475 AC

--- PS.py
+++ PS.py
@@ -1,2 +1,9 @@
-input();s=set(map(int,input().split()))
-input();print('\n'.join(map(lambda x:'1' if int(x) in s else '0',input().split())))
+from math import ceil
+d = [0]*10
+for i in map(int, input()):
+    if i in (6, 9):
+        d[6] += 0.5
+        d[9] += 0.5
+    else:
+        d[i] += 1
+print(max(map(ceil, d)))
