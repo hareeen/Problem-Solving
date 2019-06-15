1eab2004852d4fc7eacee326c73cff275bdb2b91
himyu <himyuuuu1@gmail.com>
2019-06-16 01:47:20 +0900

    BOJ 11051 AC

--- PS.kt
+++ PS.kt
@@ -4,7 +4,7 @@ fun main(args:Array<String>){with(java.util.Scanner(System.`in`)) {
   val d=Array(n+1){Array(r+1){1}}
   for(i in 1..n) {
     for(j in 1..minOf(i,r)) {
-      d[i][j]=d[i-1][j-1]+(if(j!=i) d[i-1][j] else 0)
+      d[i][j]=(d[i-1][j-1]+(if(j!=i) d[i-1][j] else 0))%10007
     }
   }
   println(d[n][r])
