6f6b659379f8da00eddc3d7276a2eabe64fdf9f6
himyu <himyuuuu1@gmail.com>
2019-06-15 11:30:26 +0900

    BOJ 10989 AC

--- PS.cpp
+++ PS.cpp
@@ -20,68 +20,23 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-void que_append(pi apd_deco, int apd_clr, int &N, int &M, vector<vector<int>> &mp, vector<vector<bool>> &check, queue<pi> &que)
-{
-  if (apd_deco.first >= 0 && apd_deco.first < N &&
-      apd_deco.second >= 0 && apd_deco.second < M &&
-      !check[apd_deco.first][apd_deco.second])
-  {
-    que.push(apd_deco);
-    mp[apd_deco.first][apd_deco.second] = apd_clr;
-    check[apd_deco.first][apd_deco.second] = true;
-  }
-}
-
-void process()
-{
-  int N, M, K;
-  cin >> M >> N >> K;
-  vector<vector<int>> mp(N, vector<int>(M));
-  vector<vector<bool>> check(N, vector<bool>(M, true));
-  queue<pi> que;
-  for (int i = 0; i < K; i++)
-  {
-    int a, b;
-    cin >> b >> a;
-    mp[a][b] = 1;
-    check[a][b] = false;
-  }
-
-  int apd_clr = 1;
-  int dx[4] = {0, 0, 1, -1};
-  int dy[4] = {1, -1, 0, 0};
-  for (int i = 0; i < N; i++)
-  {
-    for (int j = 0; j < M; j++)
-    {
-      if (!check[i][j])
-      {
-        que = queue<pi>();
-        que_append(pi(i, j), apd_clr, N, M, mp, check, que);
-        while (!que.empty())
-        {
-          auto cur = que.front();
-          que.pop();
-          for (int k = 0; k < 4; k++)
-            que_append(pi(cur.first + dx[k], cur.second + dy[k]), apd_clr, N, M, mp, check, que);
-        }
-        apd_clr++;
-      }
-    }
-  }
-  cout << apd_clr - 1 << '\n';
-}
-
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int T;
-  cin >> T;
-  for (int i = 0; i < T; i++)
-    process();
-
+  int N;
+  vector<int> v(10001);
+  cin >> N;
+  for (int i = 0; i < N; i++)
+  {
+    int t;
+    cin >> t;
+    v[t]++;
+  }
+  for (int i = 1; i <= 10000; i++)
+    for (int j = 0; j < v[i]; j++)
+      cout << i << '\n';
   return 0;
 }
