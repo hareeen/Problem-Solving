7ad7ae7674df5de10eb74626982218c591f1882d
himyu <himyuuuu1@gmail.com>
2019-06-16 01:46:14 +0900

    BOJ 11050 AC

--- PS.kt
+++ PS.kt
@@ -1,13 +1,11 @@
 fun main(args:Array<String>){with(java.util.Scanner(System.`in`)) {
-  for(tc in 1..nextInt()) {
-    val r=nextInt()
-    val n=nextInt()
-    val d=Array(n+1){Array(r+1){1}}
-    for(i in 1..n) {
-      for(j in 1..minOf(i,r)) {
-        d[i][j]=d[i-1][j-1]+(if(j!=i) d[i-1][j] else 0)
-      }
+  val n=nextInt()
+  val r=nextInt()
+  val d=Array(n+1){Array(r+1){1}}
+  for(i in 1..n) {
+    for(j in 1..minOf(i,r)) {
+      d[i][j]=d[i-1][j-1]+(if(j!=i) d[i-1][j] else 0)
     }
-    println(d[n][r])
   }
+  println(d[n][r])
 }}
