2f2e393f41602c11800536f83dc7c0bf79ef17dc
himyu <himyuuuu1@gmail.com>
2019-06-23 18:20:24 +0900

    BOJ 1009 AC

--- PS.py
+++ PS.py
@@ -1,8 +1,15 @@
-from itertools import combinations
-while True:
-    l = list(map(int, input().split()))[1:]
-    if len(l) == 0:
-        break
-    for combi in combinations(l, 6):
-        print(*combi)
-    print()
+def pow_fast(n, e, MOD=10):  # O(log e)
+    _mul = n
+    _res = 1
+    for digit in str(bin(e))[2:][::-1]:
+        if digit == '1':
+            _res *= _mul
+            _res %= MOD
+        _mul *= _mul
+        _mul %= MOD
+    return _res
+
+
+for _ in range(int(input())):
+    n, e = map(int, input().split())
+    print((pow_fast(n, e)-1) % 10+1)
