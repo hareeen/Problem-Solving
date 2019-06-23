a1ff299db91157ba35c0865c2076e23c13c64bd9
himyu <himyuuuu1@gmail.com>
2019-06-23 17:10:34 +0900

    BOJ 11724 AC

--- PS.cpp
+++ PS.cpp
@@ -23,31 +23,55 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
+void ufinit(vector<int> &parent)
+{
+  for (int i = 0; i < parent.size(); i++)
+    parent[i] = i;
+  return;
+}
+
+int uffind(vector<int> &parent, int x)
+{
+  if (parent[x] == x)
+    return x;
+  else
+    return parent[x] = uffind(parent, parent[x]);
+}
+
+void ufunion(vector<int> &parent, int x, int y)
+{
+  parent[uffind(parent, y)] = uffind(parent, x);
+  return;
+}
+
+int ufcount(vector<int> &parent)
+{
+  int ret = 0;
+  vector<bool> count(parent.size());
+  for (const auto &el : parent)
+    count[uffind(parent, el)] = true;
+  for (auto el : count)
+    if (el)
+      ret++;
+  return ret;
+}
+
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N;
-  cin >> N;
-  vector<vector<pi>> sche(N + 1);
-  for (int i = 0; i < N; i++)
+  int N, M;
+  cin >> N >> M;
+  vector<int> parent(N);
+  ufinit(parent);
+  for (int i = 0; i < M; i++)
   {
-    int Ti, Pi;
-    cin >> Ti >> Pi;
-    if (Ti + i <= N)
-      sche[Ti + i].push_back(pi(i, Pi));
+    int a, b;
+    cin >> a >> b;
+    ufunion(parent, --a, --b);
   }
-  vector<int> dp;
-  dp.push_back(0);
-  for (int i = 1; i <= N; i++)
-  {
-    int _m = dp.back();
-    for (auto el : sche[i])
-      _m = max(el.second + dp[el.first], _m);
-    dp.push_back(_m);
-  }
-  cout << dp.back() << endl;
+  cout << ufcount(parent) << endl;
   return 0;
 }
