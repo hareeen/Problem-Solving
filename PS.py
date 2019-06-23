d3ca621e7aeb7f1b2c48e6e1939fc5d7161fcfd5
himyu <himyuuuu1@gmail.com>
2019-06-23 18:05:00 +0900

    BOJ 1478 AC

--- PS.py
+++ PS.py
@@ -1,1 +1,10 @@
-l=[];exec('l+=[input()];'*int(input()));print(*sorted(sorted(set(l)),key=len),sep='\n')
+e, s, m = map(int, input().split())
+e %= 15
+s %= 28
+m %= 19
+res = 0
+while(True):
+    res += 1
+    if res % 15 == e and res % 28 == s and res % 19 == m:
+        print(res)
+        break
