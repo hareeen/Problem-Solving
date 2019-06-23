036ac2bba6661ededc7ffd7a5e057b5b20ec9ff0
himyu <himyuuuu1@gmail.com>
2019-06-23 16:54:20 +0900

    BOJ 1181 AC

--- PS.py
+++ PS.py
@@ -1,3 +1,1 @@
-l=[1]*10
-for i in range(int(input())):l=list(map(lambda x:sum(l[:(x+1)]),range(10)))
-print(l[-1]%10007)
+l=[];exec('l+=[input()];'*int(input()));print(*sorted(sorted(set(l)),key=len),sep='\n')
