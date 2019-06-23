f72700dcabe21a78eebacd4eff4f0057ebaa2b3a
himyu <himyuuuu1@gmail.com>
2019-06-23 16:36:26 +0900

    BOJ 10808 AC

--- PS.py
+++ PS.py
@@ -1,5 +1,1 @@
-exec('''n=int(input())+1;k=int(input());l=[[1]*(k+1)]
-for _ in range(n):l+=[[0]*(k+1)]
-for i in range(n):
-    for j in range(k):l[i+1][j+1]=l[i][j+1]+l[i+1][j]
-print(l[-1][-1]);'''*int(input()))
+i=input();print(*map(i.count,map(chr,range(97,123))))
