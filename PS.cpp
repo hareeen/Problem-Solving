031cc4af1ab91090488589d558fcfa3862fb1a9b
himyu <himyuuuu1@gmail.com>
2019-06-27 02:51:42 +0900

    BOJ 1991 AC

--- PS.cpp
+++ PS.cpp
@@ -24,29 +24,66 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
+char toCh(int n)
+{
+  return static_cast<char>(n + 65);
+}
+
+int toInt(char c)
+{
+  if (c == '.')
+    return -1;
+  else
+    return static_cast<int>(c) - 65;
+}
+
+void order(vector<vector<int>> &tree, int node, int mode)
+{
+  if (node == -1)
+    return;
+  switch (mode)
+  {
+  case 0:
+    cout << toCh(node);
+    order(tree, tree[node][0], mode);
+    order(tree, tree[node][1], mode);
+    break;
+  case 1:
+    order(tree, tree[node][0], mode);
+    cout << toCh(node);
+    order(tree, tree[node][1], mode);
+    break;
+  case 2:
+    order(tree, tree[node][0], mode);
+    order(tree, tree[node][1], mode);
+    cout << toCh(node);
+    break;
+
+  default:
+    break;
+  }
+  if (node == 0)
+    cout << endl;
+  return;
+}
+
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  string s;
-  cin >> s;
-  vector<char> que;
-  for (const auto &i : s)
+  int N;
+  cin >> N;
+  vector<vector<int>> tree(N, vector<int>(2, -1));
+  for (int i = 0; i < N; i++)
   {
-    que.push_back(i);
-    if (que.size() >= 4)
-    {
-      bool isMatch = true;
-      char match[4] = {'P', 'A', 'P', 'P'};
-      for (int i = 1; i <= 4; i++)
-        isMatch = isMatch && (match[i - 1] == que[que.size() - i]);
-      if (isMatch)
-        for (int i = 0; i < 3; i++)
-          que.pop_back();
-    }
+    char u, v1, v2;
+    cin >> u >> v1 >> v2;
+    tree[toInt(u)][0] = toInt(v1);
+    tree[toInt(u)][1] = toInt(v2);
   }
-  cout << ((que.size() == 1 && que[0] == 'P') ? "PPAP" : "NP") << endl;
+  for (int i = 0; i < 3; i++)
+    order(tree, 0, i);
   return 0;
 }
