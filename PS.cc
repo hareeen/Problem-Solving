29fa57398e65ba69eed529e56734dc33d3724fda
himyu <himyuuuu1@gmail.com>
2019-07-06 22:06:41 +0900

    BOJ 10815 AC

--- PS.cc
+++ PS.cc
@@ -9,13 +9,14 @@
 #include <functional>
 #include <numeric>
 #include <unordered_map>
+#include <unordered_set>
 
 #include <cmath>
 #include <cstdio>
 #include <climits>
 #include <cstring>
 
-#define M_iterai64(container) container.begin(), container.end()
+#define M_iterall(container) container.begin(), container.end()
 
 using namespace std;
 using i64 = long long;
@@ -30,47 +31,24 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N, r1, r2;
-  cin >> N >> r1 >> r2;
-  r1--;
-  r2--;
-  vector<bool> visited(N);
-  vector<vector<pi>> adjL(N);
-  visited[r1] = true;
-  for (int i = 0; i < N - 1; i++)
+  int N;
+  int M;
+  unordered_set<int> s;
+  cin>>N;
+  for (int i = 0; i < N; i++)
   {
-    int u, v, w;
-    cin >> u >> v >> w;
-    u--;
-    v--;
-    adjL[u].push_back(pi(v, w));
-    adjL[v].push_back(pi(u, w));
+    int a;
+    cin >> a;
+    s.insert(a);
   }
-  queue<ti> que;
-  que.push(ti(r1, 0, 0));
-  while (true)
+  cin>>M;
+  for (int i = 0; i < M; i++)
   {
-    auto cur = que.front();
-    int cur_vertex = get<0>(cur);
-    int cur_weight = get<1>(cur);
-    int cur_weight_max = get<2>(cur);
-
-    que.pop();
-
-    if (cur_vertex == r2)
-    {
-      cout << cur_weight - cur_weight_max << endl;
-      break;
-    }
-
-    for (auto [vertex, weight] : adjL[cur_vertex])
-    {
-      if (!visited[vertex])
-      {
-        visited[vertex] = true;
-        que.push(ti(vertex, cur_weight + weight, max(cur_weight_max, weight)));
-      }
-    }
+    int a;
+    cin >> a;
+    cout << (s.find(a) != s.end()) << " ";
   }
+  cout << endl;
+
   return 0;
 }
