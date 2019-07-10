0e4400d8aa18c4c6720939a2a9537addce47fb03
himyu <himyuuuu1@gmail.com>
2019-07-10 19:52:57 +0900

    BOJ 13304 AC

--- PS.py
+++ PS.py
@@ -1,20 +1,11 @@
-box1 = list(map(int, input().split()))
-box2 = list(map(int, input().split()))
-b1 = ((box1[0], box1[2]), (box1[1], box1[3]))
-b2 = ((box2[0], box2[2]), (box2[1], box2[3]))
-
-
-def mtch(l1, l2):
-    if l1[0] > l2[0]:
-        l1, l2 = l2, l1
-    if l2[0] > l1[1]:
-        return 0
-    elif l2[0] == l1[1]:
-        return 1
-    else:
-        return 2
-
-
-print([['NULL', 'NULL', 'NULL'],
-       ['NULL', 'POINT', 'LINE'],
-       ['NULL', 'LINE', 'FACE']][mtch(b1[0], b2[0])][mtch(b1[1], b2[1])])
+from math import ceil
+arr = [[0, 0, 0], [0, 0, 0]]
+N, K = map(int, input().split())
+for i in range(N):
+    s, y = map(int, input().split())
+    arr[s][(y-1)//2] += 1
+s = ceil((arr[0][0]+arr[1][0])/K)
+for i in range(2):
+    for j in range(1, 3):
+        s += ceil(arr[i][j]/K)
+print(s)
