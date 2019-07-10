216be222b9ed6f49a5cd1e67d271fc7ef1b09bba
himyu <himyuuuu1@gmail.com>
2019-07-10 21:00:02 +0900

    BOJ 13306 AC

--- PS.cc
+++ PS.cc
@@ -17,7 +17,7 @@
 #include <climits>
 #include <cstring>
 
-#define M_iterall(container) container.begin(), container.e()
+#define iterall(container) container.begin(), container.end()
 
 using namespace std;
 using i64 = long long;
@@ -26,67 +26,18 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-i64 init(vector<i64> &arr, vector<pli> &tree, int node, int s, int e)
+int ufFind(vector<int> &parent, int n)
 {
-  int mid = (s + e) / 2;
-  if (s == e)
-    return tree[node].first = arr[s];
+  if (parent[n] == n)
+    return n;
   else
-    return tree[node].first = init(arr, tree, node * 2, s, mid) + init(arr, tree, node * 2 + 1, mid + 1, e);
+    return parent[n] = ufFind(parent, parent[n]);
 }
 
-void update_range(vector<pli> &tree, int node, int s, int e, int left, int right, i64 diff)
+void ufUnion(vector<int> &parent, int u, int v)
 {
-  if (tree[node].second != 0)
-  {
-    tree[node].first += tree[node].second * (e - s + 1);
-    if (s != e)
-    {
-      tree[node * 2].second += tree[node].second;
-      tree[node * 2 + 1].second += tree[node].second;
-    }
-    tree[node].second = 0;
-  }
-  if (e < left || s > right)
-    return;
-  if (left <= s && e <= right)
-  {
-    tree[node].first += (e - s + 1) * diff;
-    if (s != e)
-    {
-      tree[node * 2].second += diff;
-      tree[node * 2 + 1].second += diff;
-    }
-    return;
-  }
-
-  int mid = (s + e) / 2;
-  update_range(tree, node * 2, s, mid, left, right, diff);
-  update_range(tree, node * 2 + 1, mid + 1, e, left, right, diff);
-
-  tree[node].first = tree[node * 2].first + tree[node * 2 + 1].first;
+  parent[ufFind(parent, u)] = ufFind(parent, v);
   return;
-}
-
-i64 sum(vector<pli> &tree, int node, int s, int e, int left, int right)
-{
-  if (tree[node].second != 0)
-  {
-    tree[node].first += tree[node].second * (e - s + 1);
-    if (s != e)
-    {
-      tree[node * 2].second += tree[node].second;
-      tree[node * 2 + 1].second += tree[node].second;
-    }
-    tree[node].second = 0;
-  }
-  if (e < left || s > right)
-    return 0;
-  if (left <= s && e <= right)
-    return tree[node].first;
-
-  int mid = (s + e) / 2;
-  return sum(tree, node * 2, s, mid, left, right) + sum(tree, node * 2 + 1, mid + 1, e, left, right);
 }
 
 int main()
@@ -95,38 +46,62 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N, M, K;
-  cin >> N >> M >> K;
+  int N, Q;
+  cin >> N >> Q;
 
-  vector<pli> tree(static_cast<int>(pow(2, static_cast<int>(log2(N) + 1) + 1)));
-  vector<i64> arr(1);
-
-  for (int i = 0; i < N; i++)
+  vector<int> parent_final(1), parent(1);
+  for (int i = 0; i < N - 1; i++)
   {
     int t;
     cin >> t;
-    arr.push_back(t);
+    parent_final.push_back(--t);
+    parent.push_back(i + 1);
   }
 
-  init(arr, tree, 1, 1, N);
-
-  for (int i = 0; i < M + K; i++)
+  vector<pi> query1;
+  vector<pair<int, pi>> query2;
+  for (int i = 0; i < N + Q - 1; i++)
   {
-    int mode;
-    cin >> mode;
-    if (mode == 1)
+    int x;
+    cin >> x;
+    if (x == 0)
     {
-      int l, r, d;
-      cin >> l >> r >> d;
-      update_range(tree, 1, 1, N, l, r, d);
+      int b;
+      cin >> b;
+      query1.push_back(pi(i, --b));
     }
     else
     {
-      int l, r;
-      cin >> l >> r;
-      cout << sum(tree, 1, 1, N, l, r) << "\n";
+      int c, d;
+      cin >> c >> d;
+      query2.push_back(make_pair(i, pi(--c, --d)));
     }
   }
+
+  reverse(iterall(query1));
+  reverse(iterall(query2));
+
+  vector<bool> result;
+  auto iter1 = query1.begin();
+  auto iter2 = query2.begin();
+  for (int i = N + Q - 2; i >= 0; i--)
+  {
+    if (iter1 != query1.end() && (*iter1).first == i)
+    {
+      ufUnion(parent, (*iter1).second, parent_final[(*iter1).second]);
+      iter1++;
+    }
+    else
+    {
+      result.push_back(ufFind(parent, (*iter2).second.first) == ufFind(parent, (*iter2).second.second));
+      iter2++;
+    }
+  }
+
+  reverse(iterall(result));
+
+  for (const auto &el : result)
+    cout << (el ? "YES" : "NO") << '\n';
 
   return 0;
 }
