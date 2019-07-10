7f56b5c3c7b2b4d04aa9dfea8ae93ed37f082137
himyu <himyuuuu1@gmail.com>
2019-07-10 21:08:11 +0900

    BOJ 13301 AC

--- PS.py
+++ PS.py
@@ -1,11 +1,1 @@
-from math import ceil
-arr = [[0, 0, 0], [0, 0, 0]]
-N, K = map(int, input().split())
-for i in range(N):
-    s, y = map(int, input().split())
-    arr[s][(y-1)//2] += 1
-s = ceil((arr[0][0]+arr[1][0])/K)
-for i in range(2):
-    for j in range(1, 3):
-        s += ceil(arr[i][j]/K)
-print(s)
+l=[1,1];exec('l.append(l[-1]+l[-2]);'*80);n=int(input());print((l[n]+l[n-1])*2)
