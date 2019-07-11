f27707b85fc9b02e5c3d8fdf4ce947d449131ed7
himyu <himyuuuu1@gmail.com>
2019-07-11 21:53:21 +0900

    BOJ 2436 AC

--- PS.py
+++ PS.py
@@ -1,1 +1,14 @@
-l=[1,1];exec('l.append(l[-1]+l[-2]);'*80);n=int(input());print((l[n]+l[n-1])*2)
+from math import sqrt
+
+
+def gcd(a, b):
+    return a if b == 0 else gcd(b, a % b)
+
+
+g, l = map(int, input().split())
+c = int(sqrt(l//g))
+while True:
+    if (l//g) % c == 0 and gcd(c, l//c//g) == 1:
+        print(c*g, l//c)
+        break
+    c -= 1
