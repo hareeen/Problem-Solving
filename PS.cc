b7b11d11231d57a48e92260126acba254550deb9
himyu <himyuuuu1@gmail.com>
2019-07-11 19:30:12 +0900

    BOJ 11438 AC

--- PS.cc
+++ PS.cc
@@ -26,33 +26,75 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
+void configure_tree(vector<vector<int>> &inc, vector<pair<vector<int>, int>> &tree, int node, int parent)
+{
+  tree[node].second = (parent == -1 ? 1 : (tree[parent].second + 1));
+  tree[node].first.push_back(parent);
+
+  for (int cur = 2; cur < tree[node].second; cur *= 2)
+    tree[node].first.push_back(tree[tree[node].first.back()].first[tree[node].first.size() - 1]);
+
+  for (const auto &el : inc[node])
+    if (tree[el].first.empty())
+      configure_tree(inc, tree, el, node);
+
+  return;
+}
+
+int LCA(vector<pair<vector<int>, int>> &tree, int u, int v)
+{
+  if (tree[u].second == tree[v].second)
+  {
+    if (u == v)
+      return u;
+    if (tree[u].first[0] == tree[v].first[0])
+      return tree[u].first[0];
+    for (int i = 1; i < tree[u].first.size(); i++)
+      if (tree[u].first[i] == tree[v].first[i])
+        return LCA(tree, tree[u].first[i - 1], tree[v].first[i - 1]);
+    return LCA(tree, tree[u].first.back(), tree[v].first.back());
+  }
+  else
+  {
+    if (tree[u].second < tree[v].second)
+      swap(u, v);
+    return LCA(tree, tree[u].first[static_cast<int>(log2(static_cast<double>(tree[u].second - tree[v].second)))], v);
+  }
+}
+
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N, K;
-  cin >> N >> K;
-  vector<int> arr;
-  for (int i = 0; i < N; i++)
+  int N;
+  cin >> N;
+
+  vector<vector<int>> inc(N);
+  for (int i = 0; i < N - 1; i++)
   {
-    int t;
-    cin >> t;
-    arr.push_back(t);
+    int u, v;
+    cin >> u >> v;
+    u--;
+    v--;
+    inc[u].push_back(v);
+    inc[v].push_back(u);
   }
 
-  vector<int> dp(K + 1, INT_MAX / 2);
-  dp[0] = 0;
-  for (int i = 0; i < K; i++)
-    for (int j = 0; j < N; j++)
-      for (int k = i + arr[j]; k <= K; k += arr[j])
-        dp[k] = min(dp[k], dp[i] + (k - i) / arr[j]);
+  vector<pair<vector<int>, int>> tree(N);
+  configure_tree(inc, tree, 0, -1);
 
-  if (dp.back() >= INT_MAX / 2)
-    cout << -1 << endl;
-  else
-    cout << dp.back() << endl;
+  int M;
+  cin >> M;
+  for (int i = 0; i < M; i++)
+  {
+    int u, v;
+    cin >> u >> v;
+    u--;
+    v--;
+    cout << LCA(tree, u, v) + 1 << '\n';
+  }
 
   return 0;
 }
