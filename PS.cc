e28368cf8fd597781d412f1525f45b436c5661a1
himyu <himyuuuu1@gmail.com>
2019-07-13 08:27:05 +0900

    BOJ 1937 AC

--- PS.cc
+++ PS.cc
@@ -26,12 +26,19 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-i64 calc_treeh(vector<int> &arr, int n)
+int dx[4] = {1, 0, 0, -1}, dy[4] = {0, -1, 1, 0};
+
+int dfs(const int &N, vector<vector<int>> &arr, vector<vector<int>> &check, int corr1, int corr2)
 {
-  i64 s = 0;
-  for (const auto &el : arr)
-    s += max(0, el - n);
-  return s;
+  if (check[corr1][corr2] != 1)
+    return check[corr1][corr2];
+  for (int i = 0; i < 4; i++)
+  {
+    int c1_new = corr1 + dx[i], c2_new = corr2 + dy[i];
+    if (0 <= c1_new && c1_new < N && 0 <= c2_new && c2_new < N && arr[c1_new][c2_new] > arr[corr1][corr2])
+      check[corr1][corr2] = max(check[corr1][corr2], dfs(N, arr, check, c1_new, c2_new) + 1);
+  }
+  return check[corr1][corr2];
 }
 
 int main()
@@ -40,34 +47,35 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  i64 N, M;
-  cin >> N >> M;
+  int N;
+  cin >> N;
 
-  vector<int> arr;
+  vector<vector<int>> arr(N);
+  vector<vector<int>> check(N, vector<int>(N, 1));
+  vector<pair<int, pi>> cor;
   for (int i = 0; i < N; i++)
   {
-    int t;
-    cin >> t;
-    arr.push_back(t);
-  }
-
-  int l = 0, r = INT_MAX;
-  while (l < r)
-  {
-    int mid = (l + r) / 2;
-    i64 res = calc_treeh(arr, mid);
-    if (res <= M)
-      r = mid;
-    else
-      l = mid + 1;
-  }
-
-  for(int i=l+1;i>=l-1;i--) {
-    if(calc_treeh(arr, i)>=M) {
-      cout<<i<<endl;
-      break;
+    for (int j = 0; j < N; j++)
+    {
+      int t;
+      cin >> t;
+      arr[i].push_back(t);
+      cor.push_back(make_pair(t, pi(i, j)));
     }
   }
 
+  sort(iterall(cor));
+  reverse(iterall(cor));
+
+  for (const auto &[height, corr] : cor)
+    if (check[corr.first][corr.second] == 1)
+      dfs(N, arr, check, corr.first, corr.second);
+
+  int ret = INT_MIN;
+  for (const auto &subarr : check)
+    for (const auto &el : subarr)
+      ret = max(ret, el);
+
+  cout << ret << '\n';
   return 0;
 }
