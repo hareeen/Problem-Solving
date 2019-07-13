34f9181065393cee22a825861e982f992eac3244
himyu <himyuuuu1@gmail.com>
2019-07-13 09:16:24 +0900

    BOJ 2011 AC

--- PS.py
+++ PS.py
@@ -1,3 +1,6 @@
-from math import ceil
-a, b, v = map(int, input().split())
-print(ceil((v-b)/(a-b)))
+l = [1, 1]
+g = list(map(int, list(input())))
+for i in range(len(g)):
+    l.append(((l[-1] if g[i] != 0 else 0) +
+              (l[-2] if i > 0 and g[i-1] != 0 and g[i-1]*10+g[i] <= 26 else 0)) % 1000000)
+print(l[-1])
