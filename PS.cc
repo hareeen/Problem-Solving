addc62745c7f2082735dd28c36643d4d582a1954
himyu <himyuuuu1@gmail.com>
2019-07-13 13:34:57 +0900

    BOJ 1976 AC

--- PS.cc
+++ PS.cc
@@ -26,19 +26,29 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-int dx[4] = {1, 0, 0, -1}, dy[4] = {0, -1, 1, 0};
-
-int dfs(const int &N, vector<vector<int>> &arr, vector<vector<int>> &check, int corr1, int corr2)
+void ufInit(vector<int> &par)
 {
-  if (check[corr1][corr2] != 1)
-    return check[corr1][corr2];
-  for (int i = 0; i < 4; i++)
-  {
-    int c1_new = corr1 + dx[i], c2_new = corr2 + dy[i];
-    if (0 <= c1_new && c1_new < N && 0 <= c2_new && c2_new < N && arr[c1_new][c2_new] > arr[corr1][corr2])
-      check[corr1][corr2] = max(check[corr1][corr2], dfs(N, arr, check, c1_new, c2_new) + 1);
-  }
-  return check[corr1][corr2];
+  for (int i = 0; i < par.size(); i++)
+    par[i] = i;
+  return;
+}
+
+int ufFind(vector<int> &par, int vtx)
+{
+  if (par[vtx] == vtx)
+    return vtx;
+  else
+    return par[vtx] = ufFind(par, par[vtx]);
+}
+
+void ufMerge(vector<int> &par, int u, int v)
+{
+  par[ufFind(par, u)] = ufFind(par, v);
+}
+
+bool ufSameset(vector<int> &par, int u, int v)
+{
+  return ufFind(par, u) == ufFind(par, v);
 }
 
 int main()
@@ -47,35 +57,36 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N;
-  cin >> N;
+  int N, M;
+  cin >> N >> M;
 
-  vector<vector<int>> arr(N);
-  vector<vector<int>> check(N, vector<int>(N, 1));
-  vector<pair<int, pi>> cor;
+  vector<int> parent(N);
+  ufInit(parent);
+
   for (int i = 0; i < N; i++)
   {
     for (int j = 0; j < N; j++)
     {
       int t;
       cin >> t;
-      arr[i].push_back(t);
-      cor.push_back(make_pair(t, pi(i, j)));
+      if (i > j && t == 1)
+        ufMerge(parent, i, j);
     }
   }
 
-  sort(iterall(cor));
-  reverse(iterall(cor));
+  bool ret = true;
+  int lst, cur;
+  cin >> lst;
+  lst--;
+  for (int i = 1; i < M; i++)
+  {
+    cin >> cur;
+    cur--;
+    ret &= ufSameset(parent, lst, cur);
+    lst = cur;
+  }
 
-  for (const auto &[height, corr] : cor)
-    if (check[corr.first][corr.second] == 1)
-      dfs(N, arr, check, corr.first, corr.second);
+  cout << (ret ? "YES" : "NO") << endl;
 
-  int ret = INT_MIN;
-  for (const auto &subarr : check)
-    for (const auto &el : subarr)
-      ret = max(ret, el);
-
-  cout << ret << '\n';
   return 0;
 }
