c82281b2058f9db6ea2f17a82d545150b118b5fc
himyu <himyuuuu1@gmail.com>
2019-07-14 17:37:18 +0900

    BOJ 15972 AC

--- PS.cc
+++ PS.cc
@@ -26,10 +26,7 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-bool comp(const pi &left, const pi &right)
-{
-  return left.first <= right.first && left.second <= right.second;
-}
+using pii = pair<int, pi>;
 
 int main()
 {
@@ -37,33 +34,82 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N;
-  cin >> N;
+  int N, M, H;
+  int dx[4] = {1, -1, 0, 0}, dy[4] = {0, 0, 1, -1};
+  cin >> N >> M >> H;
+  vector<vector<vector<int>>> holes(N, vector<vector<int>>(M, vector<int>(4, -1)));
 
-  vector<pi> arr;
+  // 0: down, 1: up, 2:right, 3:left
+  for (int i = 0; i < N + 1; i++)
+  {
+    for (int j = 0; j < M; j++)
+    {
+      int h;
+      cin >> h;
+      if (i != N)
+        holes[i][j][1] = h;
+      if (i != 0)
+        holes[i - 1][j][0] = h;
+    }
+  }
   for (int i = 0; i < N; i++)
   {
-    int a, b;
-    cin >> a >> b;
-    if (a < b)
-      swap(a, b);
-    arr.push_back(pi(a, b));
+    for (int j = 0; j < M + 1; j++)
+    {
+      int h;
+      cin >> h;
+      if (j != M)
+        holes[i][j][3] = h;
+      if (j != 0)
+        holes[i][j - 1][2] = h;
+    }
   }
 
-  sort(iterall(arr));
-
-  vector<int> dp(1, 1);
-
-  for (int i = 1; i < N; i++)
+  priority_queue<pii, deque<pii>, greater<pii>> pq;
+  vector<vector<int>> water(N, vector<int>(M, H));
+  for (int i = 0; i < M; i++)
   {
-    int ret = 1;
-    for (int j = 0; j < i; j++)
-      if (arr[j].second <= arr[i].second)
-        ret = max(ret, dp[j] + 1);
-    dp.push_back(ret);
+    if (holes[0][i][1] != -1 && holes[0][i][1] < water[0][i])
+      pq.push(pii(holes[0][i][1], pi(0, i))), water[0][i] = holes[0][i][1];
+    if (holes[N - 1][i][0] != -1 && holes[N - 1][i][0] < water[N - 1][i])
+      pq.push(pii(holes[N - 1][i][0], pi(N - 1, i))), water[N - 1][i] = holes[N - 1][i][0];
+  }
+  for (int i = 0; i < N; i++)
+  {
+    if (holes[i][0][3] != -1 && holes[i][0][3] < water[i][0])
+      pq.push(pii(holes[i][0][3], pi(i, 0))), water[i][0] = holes[i][0][3];
+    if (holes[i][M - 1][2] != -1 && holes[i][M - 1][2] < water[i][M - 1])
+      pq.push(pii(holes[i][M - 1][2], pi(i, M - 1))), water[i][M - 1] = holes[i][M - 1][2];
   }
 
-  cout << *max_element(iterall(dp)) << endl;
+  while (!pq.empty())
+  {
+    auto cur = pq.top();
+    auto curH = cur.first, curX = cur.second.first, curY = cur.second.second;
+    pq.pop();
+    if (water[curX][curY] < curH)
+      continue;
+    for (int i = 0; i < 4; i++)
+    {
+      if (holes[curX][curY][i] != -1)
+      {
+        int x = curX + dx[i], y = curY + dy[i];
+        if (x < 0 || x >= N || y < 0 || y >= M)
+          continue;
+        if (water[x][y] > max(curH, holes[curX][curY][i]))
+        {
+          water[x][y] = max(curH, holes[curX][curY][i]);
+          pq.push(pii(max(curH, holes[curX][curY][i]), pi(x, y)));
+        }
+      }
+    }
+  }
+
+  int ans = 0;
+  for (const auto &vec : water)
+    for (const auto &el : vec)
+      ans += el;
+  cout << ans << endl;
 
   return 0;
 }
