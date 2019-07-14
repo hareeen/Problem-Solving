6cef79496d75c6a0370e5ed1605b9eccd851ad6d
himyu <himyuuuu1@gmail.com>
2019-07-14 22:48:16 +0900

    BOJ 11404 AC

--- PS.cc
+++ PS.cc
@@ -26,97 +26,39 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-class disjointSet
-{
-private:
-  vector<int> uf;
-
-public:
-  disjointSet(int N)
-  {
-    uf.clear();
-    for (int i = 0; i < N; i++)
-      uf.push_back(i);
-  }
-  int find(int n)
-  {
-    if (n == uf[n])
-      return n;
-    return uf[n] = find(uf[n]);
-  }
-  void merge(int u, int v)
-  {
-    uf[find(u)] = uf[find(v)];
-    return;
-  }
-  bool isSameset(int u, int v)
-  {
-    return find(u) == find(v);
-  }
-};
-
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int T;
-  cin >> T;
+  int N, M;
+  cin >> N;
+  cin >> M;
 
-  for (int tc = 0; tc < T; tc++)
+  vector<vector<int>> inc(N, vector<int>(N, INT_MAX / 2));
+  for (int i = 0; i < N; i++)
+    inc[i][i] = 0;
+
+  for (int i = 0; i < M; i++)
   {
-    int N, K;
-    cin >> N >> K;
+    int u, v, w;
+    cin >> u >> v >> w;
+    u--;
+    v--;
+    inc[u][v] = min(inc[u][v], w);
+  }
 
-    vector<int> weight;
-    for (int i = 0; i < N; i++)
-    {
-      int t;
-      cin >> t;
-      weight.push_back(t);
-    }
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      for (int k = 0; k < N; k++)
+        inc[k][j] = min(inc[k][j], inc[k][i] + inc[i][j]);
 
-    vector<vector<int>> inc(N);
-    vector<int> remain(N);
-    auto ds = disjointSet(N);
-    for (int i = 0; i < K; i++)
-    {
-      int u, v;
-      cin >> u >> v;
-      u--;
-      v--;
-      inc[u].push_back(v);
-      ds.merge(u, v);
-      remain[v]++;
-    }
-
-    int target;
-    cin >> target;
-    target--;
-
-    queue<int> topo;
-    for (int i = 0; i < N; i++)
-      if (remain[i] == 0 && ds.isSameset(i, target))
-        topo.push(i);
-
-    vector<int> minTime(N, 0);
-    while (!topo.empty())
-    {
-      auto cur = topo.front();
-      topo.pop();
-
-      minTime[cur] += weight[cur];
-      for (const auto &i : inc[cur])
-      {
-        minTime[i] = max(minTime[i], minTime[cur]);
-        remain[i]--;
-        if (remain[i] == 0)
-          topo.push(i);
-      }
-    }
-
-    cout << minTime[target] << '\n';
+  for (const auto &vec : inc)
+  {
+    for (const auto &el : vec)
+      cout << (el >= INT_MAX / 2 ? 0 : el) << " ";
+    cout << '\n';
   }
 
   return 0;
