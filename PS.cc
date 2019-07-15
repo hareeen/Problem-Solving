0dcf9b4481268bc40a6e4bd5d61136098d290393
himyu <himyuuuu1@gmail.com>
2019-07-16 07:25:35 +0900

    BOJ 14938 AC

--- PS.cc
+++ PS.cc
@@ -26,93 +26,52 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-class SegmentTree
-{
-private:
-  vector<int> tree;
-
-public:
-  SegmentTree(int N)
-  {
-    tree.clear();
-    tree.resize(static_cast<int>(pow(2, floor(log2(N) + 1) + 1)) + 1);
-  }
-  int init(vector<int> &arr, int s, int e, int node)
-  {
-    if (s == e)
-      return tree[node] = arr[s];
-    else
-      return tree[node] = init(arr, s, (s + e) / 2, node * 2) + init(arr, (s + e) / 2 + 1, e, node * 2 + 1);
-  }
-  void update(int target, int s, int e, int node, int diff)
-  {
-    if (target < s || e < target)
-      return;
-    tree[node] += diff;
-    if (s != e)
-    {
-      update(target, s, (s + e) / 2, node * 2, diff);
-      update(target, (s + e) / 2 + 1, e, node * 2 + 1, diff);
-    }
-    return;
-  }
-  int query(int l, int r, int s, int e, int node) // SUM
-  {
-    if (r < s || e < l)
-      return 0;
-    else if (l <= s && e <= r)
-      return tree[node];
-    else
-      return query(l, r, s, (s + e) / 2, node * 2) + query(l, r, (s + e) / 2 + 1, e, node * 2 + 1);
-  }
-};
-
-i64 process()
-{
-  int N;
-  cin >> N;
-
-  vector<pi> corY, corr;
-  for (int i = 0; i < N; i++)
-  {
-    int x, y;
-    cin >> x >> y;
-    corY.push_back(pi(y, i));
-    corr.push_back(pi(-x, y));
-  }
-
-  sort(iterall(corY));
-
-  int cur = 0, last = INT_MAX;
-  for (auto &[num, idx] : corY)
-  {
-    if (num != last)
-      cur++, last = num;
-    corr[idx].second = cur;
-  }
-  int maxY = cur + 1;
-
-  sort(iterall(corr));
-
-  i64 ret = 0;
-  auto segt = SegmentTree(maxY);
-  for (const auto &[x, y] : corr)
-  {
-    ret += segt.query(1, y, 1, maxY, 1);
-    segt.update(y, 1, maxY, 1, 1);
-  }
-
-  return ret;
-}
-
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int T;
-  cin >> T;
-  for (int i = 0; i < T; i++)
-    cout << process() << '\n';
+  int N, M, R;
+  cin >> N >> M >> R;
+
+  vector<int> item;
+  for (int i = 0; i < N; i++)
+  {
+    int t;
+    cin >> t;
+    item.push_back(t);
+  }
+
+  vector<vector<int>> floyd(N, vector<int>(N, INT_MAX / 2));
+  for (int i = 0; i < N; i++)
+    floyd[i][i] = 0;
+
+  for (int i = 0; i < R; i++)
+  {
+    int a, b, l;
+    cin >> a >> b >> l;
+    a--;
+    b--;
+    floyd[a][b] = l;
+    floyd[b][a] = l;
+  }
+
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      for (int k = 0; k < N; k++)
+        floyd[k][j] = min(floyd[k][j], floyd[k][i] + floyd[i][j]);
+        
+  int ans = 0;
+  for (int i = 0; i < N; i++)
+  {
+    int cp = 0;
+    for (int j = 0; j < N; j++)
+      if (floyd[i][j] <= M)
+        cp += item[j];
+    ans = max(cp, ans);
+  }
+  cout << ans << endl;
+
+  return 0;
 }
