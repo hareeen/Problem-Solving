5daf98a02a1812ff856c64320fd233eb99cd54da
himyu <himyuuuu1@gmail.com>
2019-07-19 22:06:18 +0900

    BOJ 10163 AC

--- PS.cc
+++ PS.cc
@@ -26,112 +26,32 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-vector<int> addVector(vector<int> &lhs, vector<int> &rhs)
-{
-  vector<int> result;
-  for (int i = 0; i < lhs.size(); i++)
-    result.push_back(lhs[i] + rhs[i]);
-  return result;
-}
-
-class segmentTree
-{
-private:
-  int treeSize;
-  int elementSize;
-  vector<vector<int>> tree;
-
-public:
-  segmentTree(const int N)
-  {
-    treeSize = (1 << static_cast<int>(ceil(log2(N)) + 1)) + 1;
-    tree.clear();
-    tree.resize(treeSize, vector<int>(N));
-  }
-  vector<int> init(const vector<vector<int>> &arr, const int s, const int e, const int node)
-  {
-    if (s == e)
-      return tree[node] = arr[s];
-    else
-    {
-      auto leftRes = init(arr, s, (s + e) / 2, node * 2);
-      auto rightRes = init(arr, (s + e) / 2 + 1, e, node * 2 + 1);
-      return tree[node] = addVector(leftRes, rightRes);
-    }
-  }
-  void update(const int t, const int s, const int e, const int node, const int yCorr, const int diff)
-  {
-    if (t < s || e < t)
-      return;
-    tree[node][yCorr] += diff;
-    if (s != e)
-    {
-      update(t, s, (s + e) / 2, node * 2, yCorr, diff);
-      update(t, (s + e) / 2 + 1, e, node * 2 + 1, yCorr, diff);
-    }
-  }
-  int query(const int s, const int e, const int node, const int lx, const int ly, const int rx, const int ry)
-  {
-    if (e < lx || rx < s)
-      return 0;
-    if (lx <= s && e <= rx)
-    {
-      int ret = 0;
-      for (int i = ly; i <= ry; i++)
-        ret += tree[node][i];
-      return ret;
-    }
-    return query(s, (s + e) / 2, node * 2, lx, ly, rx, ry) + query((s + e) / 2 + 1, e, node * 2 + 1, lx, ly, rx, ry);
-  }
-};
-
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  int N, M;
-  cin >> N >> M;
+  int N;
+  cin >> N;
 
-  vector<vector<int>> mp(1, vector<int>(N));
-  for (int i = 0; i < N; i++)
+  vector<vector<int>> map(101, vector<int>(101));
+  for (int i = 1; i <= N; i++)
   {
-    vector<int> pb;
-    for (int j = 0; j < N; j++)
-    {
-      int t;
-      cin >> t;
-      pb.push_back(t);
-    }
-    mp.push_back(pb);
+    int sx, sy, w, h;
+    cin >> sx >> sy >> w >> h;
+    for (int x = 0; x < w; x++)
+      for (int y = 0; y < h; y++)
+        map[sx + x][sy + y] = i;
   }
 
-  auto sT = segmentTree(N);
-  sT.init(mp, 1, N, 1);
-  for (int i = 0; i < M; i++)
-  {
-    int mode;
-    cin >> mode;
-    if (mode == 0)
-    {
-      int x, y, c;
-      cin >> x >> y >> c;
-      y--;
-      sT.update(x, 1, N, 1, y, c - mp[x][y]);
-      mp[x][y] = c;
-    }
-    else
-    {
-      int sx, sy, ex, ey;
-      cin >> sx >> sy >> ex >> ey;
-      if (sx > ex)
-        swap(sx, ex);
-      if (sy > ey)
-        swap(sy, ey);
-      cout << sT.query(1, N, 1, sx, sy - 1, ex, ey - 1) << '\n';
-    }
-  }
+  vector<int> ans(N + 1);
+  for (int i = 0; i <= 100; i++)
+    for (int j = 0; j <= 100; j++)
+      ans[map[i][j]]++;
+
+  for (int i = 1; i <= N; i++)
+    cout << ans[i] << '\n';
 
   return 0;
 }
