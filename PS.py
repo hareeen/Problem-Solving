a67056a2e894194c7b39b136484bcd2abb0fb4fa
himyu <himyuuuu1@gmail.com>
2019-08-25 20:26:34 +0900

    BOJ 2669 AC

--- PS.py
+++ PS.py
@@ -1,9 +1,12 @@
-a = int(input())
-b = list(map(int, input()))
-x = a*b[2]
-y = a*b[1]
-z = a*b[0]
-print(x)
-print(y)
-print(z)
-print(x+y*10+z*100)
+mp = []
+for i in range(100):
+    mp.append([0]*100)
+for i in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            mp[x][y] = 1
+ans = 0
+for i in mp:
+    ans += sum(i)
+print(ans)
