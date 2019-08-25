698f924fd1060e866cbd961cd414b9bbe9e54c75
himyu <himyuuuu1@gmail.com>
2019-08-25 21:41:23 +0900

    BOJ 1572 && 9426 AC

--- PS.cc
+++ PS.cc
@@ -30,35 +30,73 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-void manachers(const string &s, vector<int> &res) {
-  int N = s.length();
-  res.clear();
-  res.resize(N);
+class segTree {
+ private:
+  vector<i64> tree;
 
-  int r = 0, p = 0;
-  for (int i = 0; i < N; i++) {
-    if (i <= r) res[i] = min(res[2 * p - i], r - i);
-    while (i - res[i] - 1 >= 0 && i + res[i] + 1 < N &&
-           s[i - res[i] - 1] == s[i + res[i] + 1])
-      res[i]++;
-    if (r < i + res[i]) r = i + res[i], p = i;
+ public:
+  segTree() {
+    tree.clear();
+    tree.resize(131072 + 128);
   }
-}
+  i64 init(int s, int e, int node, vector<i64> &arr) {
+    if (s == e)
+      return tree[node] = arr[s];
+    else
+      return tree[node] = init(s, (s + e) / 2, node * 2, arr) +
+                          init((s + e) / 2, e, node * 2 + 1, arr);
+  }
+  void update(int s, int e, int tar, int node, i64 diff) {
+    if (s > tar || e < tar) return;
+    if (s != e) {
+      update(s, (s + e) / 2, tar, node * 2, diff);
+      update((s + e) / 2 + 1, e, tar, node * 2 + 1, diff);
+    }
+    tree[node] += diff;
+    return;
+  }
+  int query(int s, int e, int node, int rank) {
+    if (s == e) return s;
+    auto lft = tree[node * 2];
+    if (rank <= lft)
+      return query(s, (s + e) / 2, node * 2, rank);
+    else
+      return query((s + e) / 2 + 1, e, node * 2 + 1, rank - lft);
+  }
+};
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  string tmp;
-  cin >> tmp;
+  int N, K;
+  cin >> N >> K;
 
-  string st = "#";
-  for (auto c : tmp) st.push_back(c), st.push_back('#');
+  auto target_rank = (K + 1) / 2;
+  auto sT = segTree();
 
-  vector<int> mana;
-  manachers(st, mana);
+  vector<int> arr;
+  for (int i = 0; i < K; i++) {
+    int t;
+    cin >> t;
+    arr.push_back(t);
+    sT.update(1, 65536, t + 1, 1, 1);
+  }
 
-  cout << *max_element(iterall(mana)) << endl;
+  i64 ans = 0;
+  for (int i = K + 1; i <= N; i++) {
+    ans += (static_cast<i64>(sT.query(1, 65536, 1, target_rank)) - 1);
+    // cout << sT.query(1, 65536, 1, target_rank) << endl;
+
+    int t;
+    cin >> t;
+    arr.push_back(t);
+    sT.update(1, 65536, t + 1, 1, 1);
+    sT.update(1, 65536, arr[i - K - 1] + 1, 1, -1);
+  }
+
+  cout << ans + static_cast<i64>(sT.query(1, 65536, 1, target_rank)) - 1
+       << endl;
   return 0;
 }
