0eb8174cdcb47cdd9d550b3acc1c654400bd9dd0
himyu <himyuuuu1@gmail.com>
2019-09-18 00:00:23 +0900

    BOJ 5000 AC

--- PS.cc
+++ PS.cc
@@ -24,59 +24,78 @@
 using namespace std;
 
 using i64 = long long;
-using i128 = __int128;
 using pi = pair<int, int>;
 using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-bool dfs(int here, vector<bool> &vst, vector<int> &match,
-         vector<vector<int>> &grp) {
-  if (vst[here]) return false;
-  vst[here] = true;
-  for (auto there : grp[here]) {
-    if (match[there] == -1 || dfs(match[there], vst, match, grp)) {
-      match[there] = here;
-      return true;
+class segTree {
+ private:
+  vector<int> tree;
+
+ public:
+  segTree(int N) {
+    tree.clear();
+    tree.resize(N);
+  }
+  int init(int s, int e, int node, const vector<int> &arr) {
+    if (s == e)
+      return tree[node] = arr[s];
+    else
+      return tree[node] = init(s, (s + e) / 2, node * 2, arr) +
+                          init((s + e) / 2 + 1, e, node * 2 + 1, arr);
+  }
+  void update(int s, int e, int node, const int tar, const int diff) {
+    if (tar < s || e < tar) return;
+    tree[node] -= diff;
+    if (s != e) {
+      update(s, (s + e) / 2, node * 2, tar, diff);
+      update((s + e) / 2 + 1, e, node * 2 + 1, tar, diff);
     }
   }
-  return false;
-}
+  int query(int s, int e, int node, const int l, const int r) {
+    if (e < l || r < s)
+      return 0;
+    else if (l <= s && e <= r)
+      return tree[node];
+    else
+      return query(s, (s + e) / 2, node * 2, l, r) +
+             query((s + e) / 2 + 1, e, node * 2 + 1, l, r);
+  }
+};
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int N, M;
-  cin >> N >> M;
+  int N;
+  cin >> N;
 
-  vector<vector<int>> grp(N + M);
+  N++;
 
-  for (int i = 0; i < N; i++) {
-    int sz;
-    cin >> sz;
-    for (int j = 0; j < sz; j++) {
-      int k;
-      cin >> k;
-      k += (N - 1);
-      grp[i].push_back(k);
-      grp[k].push_back(i);
-    }
-  }
+  vector<int> st(N), fi(N);
+  for (int i = 1; i < N; i++) cin >> st[i];
+  for (int i = 1; i < N; i++) cin >> fi[i];
+
+  for (int i = 1; i < N; i++) st[i]--, fi[i]--;
+
+  vector<int> mp(N);
+  for (int i = 1; i < N; i++) mp[st[i]] = i;
+  for (int i = 1; i < N; i++) fi[i] = mp[fi[i]];
+
+  vector<int> mp2(N);
+  for (int i = 1; i < N; i++) mp2[fi[i]] = i;
 
   int ans = 0;
-  vector<int> match(N + M, -1);
-  for (int i = 0; i < N; i++) {
-    vector<bool> vst(N);
-    if (dfs(i, vst, match, grp)) ans++;
-  }
-  for (int i = 0; i < N; i++) {
-    vector<bool> vst(N);
-    if (dfs(i, vst, match, grp)) ans++;
+  auto sT = segTree(262144);
+  for (int i = 1; i < N; i++) {
+    ans += (mp2[i] - sT.query(1, N, 1, 1, mp2[i]) - 1);
+    sT.update(1, N, 1, mp2[i], 1);
+    ans %= 2;
   }
 
-  cout << ans << endl;
+  cout << (ans ? "Impossible" : "Possible") << endl;
 
   return 0;
 }
