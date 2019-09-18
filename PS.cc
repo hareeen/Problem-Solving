04198c68c990e9764e033798172559e87ea4b1b0
himyu <himyuuuu1@gmail.com>
2019-09-19 01:59:04 +0900

    BOJ 12728 AC

--- PS.cc
+++ PS.cc
@@ -29,73 +29,135 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-class segTree {
- private:
-  vector<int> tree;
-
+template <class T>
+class Matrix : public vector<vector<T>> {
  public:
-  segTree(int N) {
-    tree.clear();
-    tree.resize(N);
+  Matrix() { this->clear(); }
+
+  Matrix(const int M, const int N) {
+    this->clear();
+    this->resize(M);
+    for (auto it = this->begin(); it != this->end(); it++) it->resize(N);
   }
-  int init(int s, int e, int node, const vector<int> &arr) {
-    if (s == e)
-      return tree[node] = arr[s];
-    else
-      return tree[node] = init(s, (s + e) / 2, node * 2, arr) +
-                          init((s + e) / 2 + 1, e, node * 2 + 1, arr);
-  }
-  void update(int s, int e, int node, const int tar, const int diff) {
-    if (tar < s || e < tar) return;
-    tree[node] -= diff;
-    if (s != e) {
-      update(s, (s + e) / 2, node * 2, tar, diff);
-      update((s + e) / 2 + 1, e, node * 2 + 1, tar, diff);
+
+  Matrix(const vector<vector<T>>& arr) {
+    const int M = arr.size();
+    const int N = arr[0].size();
+
+    this->clear();
+    this->resize(M);
+    for (int i = 0; i < M; i++) {
+      this->at(i).resize(N);
+      for (int j = 0; j < N; j++) {
+        this->at(i).at(j) = arr[i][j];
+      }
     }
   }
-  int query(int s, int e, int node, const int l, const int r) {
-    if (e < l || r < s)
-      return 0;
-    else if (l <= s && e <= r)
-      return tree[node];
-    else
-      return query(s, (s + e) / 2, node * 2, l, r) +
-             query((s + e) / 2 + 1, e, node * 2 + 1, l, r);
+
+  Matrix(initializer_list<initializer_list<T>> arr) {
+    const int M = arr.size();
+
+    this->clear();
+    this->resize(M);
+
+    int i = 0;
+    for (auto el : arr) {
+      this->at(i) = vector<T>(el);
+      i++;
+    }
+  }
+
+  T& at2(const int m, const int n) { return this->at(m).at(n); }
+
+  const T& at2(const int m, const int n) const { return this->at(m).at(n); }
+
+  pi size2() const noexcept {
+    return {static_cast<int>(this->size()),
+            static_cast<int>(this->at(0).size())};
+  }
+
+  void modulo(const T mod) {
+    for (auto it = this->begin(); it != this->end(); it++) {
+      for (auto& el : (*it)) el %= mod;
+    }
+  }
+
+  Matrix<T> operator*(const Matrix<T>& rhs) {
+    auto lsz = this->size2();
+    auto rsz = rhs.size2();
+
+    if (lsz.second != rsz.first) throw;
+
+    Matrix<T> newMatrix(lsz.first, rsz.second);
+
+    for (int i = 0; i < lsz.first; i++) {
+      for (int j = 0; j < rsz.second; j++) {
+        T _s{};
+        for (int k = 0; k < lsz.second; k++)
+          _s += (this->at2(i, k) * rhs.at2(k, j));
+        newMatrix.at2(i, j) = _s;
+      }
+    }
+
+    return newMatrix;
   }
 };
+
+const i64 mod = 1000LL;
+
+vector<Matrix<i64>> tf;
+const auto transform_base = Matrix<i64>({{6, -4}, {1, 0}});
+
+void initialize(const int n) {
+  tf.push_back(transform_base);
+  for (int i = 0; i < n; i++) {
+    tf.push_back(tf.back() * tf.back());
+    tf.back().modulo(mod);
+  }
+}
+
+int get_nth(int n) {
+  int cur = 0, mul = 1;
+  Matrix<i64> base = {{6}, {2}};
+
+  n--;
+  while (n) {
+    if (n % 2) base = tf[cur] * base;
+    base.modulo(mod);
+    cur++, mul *= 2;
+    n /= 2;
+  }
+
+  return base.at2(0, 0);
+}
+
+void print(const int n, const int r) {
+  int res = (r + mod) % mod;
+
+  string atFront;
+  if (res < 10)
+    atFront = "00";
+  else if (res < 100)
+    atFront = "0";
+
+  cout << "Case #" << n << ": " << atFront << res << '\n';
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int N;
-  cin >> N;
+  initialize(31);
 
-  N++;
+  int tc;
+  cin >> tc;
 
-  vector<int> st(N), fi(N);
-  for (int i = 1; i < N; i++) cin >> st[i];
-  for (int i = 1; i < N; i++) cin >> fi[i];
-
-  for (int i = 1; i < N; i++) st[i]--, fi[i]--;
-
-  vector<int> mp(N);
-  for (int i = 1; i < N; i++) mp[st[i]] = i;
-  for (int i = 1; i < N; i++) fi[i] = mp[fi[i]];
-
-  vector<int> mp2(N);
-  for (int i = 1; i < N; i++) mp2[fi[i]] = i;
-
-  int ans = 0;
-  auto sT = segTree(262144);
-  for (int i = 1; i < N; i++) {
-    ans += (mp2[i] - sT.query(1, N, 1, 1, mp2[i]) - 1);
-    sT.update(1, N, 1, mp2[i], 1);
-    ans %= 2;
+  for (int i = 0; i < tc; i++) {
+    int t;
+    cin >> t;
+    print(i + 1, get_nth(t) - 1);
   }
-
-  cout << (ans ? "Impossible" : "Possible") << endl;
 
   return 0;
 }
