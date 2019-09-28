d236ed0c627b6dc5d7ad0f65114aa6f3517c7aff
himyu <himyuuuu1@gmail.com>
2019-09-28 09:30:51 +0000

    upd

--- PS.py
+++ PS.py
@@ -1,12 +1,20 @@
-mp = []
-for i in range(100):
-    mp.append([0]*100)
-for i in range(4):
-    x1, y1, x2, y2 = map(int, input().split())
-    for x in range(x1, x2):
-        for y in range(y1, y2):
-            mp[x][y] = 1
-ans = 0
-for i in mp:
-    ans += sum(i)
-print(ans)
+n,l=map(int,input().split())
+
+flag=False
+for i in range(l,101):
+	if i%2==1 and n%i==0:
+		st=(n//i)-(i//2)
+		if st>=0:
+			print(' '.join(map(str,range(st,st+i))))
+			flag=True
+			break
+	elif i%2==0 and (2*n)%i==0:
+		t=(2*n)//i
+		if t%2==0: continue
+		st=(t//2)-(i//2)+1
+		if st>=0:
+			print(' ' .join(map(str,range(st,st+i))))
+			flag=True
+			break
+
+if not flag: print(-1)
