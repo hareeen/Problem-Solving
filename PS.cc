4fa1d8fae51b597ebab98b49cb1b854698714c32
himyu <himyuuuu1@gmail.com>
2019-11-06 18:37:46 +0900

    BOJ 1006 AC

--- PS.cc
+++ PS.cc
@@ -29,29 +29,66 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-void process() {
-  int N;
-  cin >> N;
+void update_ans(const vector<pi> &tagon, vector<vector<int>> &dp, const int N,
+                const int W, int &ans, int finPos, int finPhase) {
+  for (int i = 2; i <= N; i++) {
+    bool fi_pos = tagon[i].first + tagon[i - 1].first <= W;
+    bool se_pos = tagon[i].second + tagon[i - 1].second <= W;
+    bool cur_pos = tagon[i].first + tagon[i].second <= W;
 
-  vector<pi> edge(N);
-  for (int i = 0; i < N; i++) {
-    for (int j = i + 1; j < N; j++) {
-      int color;
-      cin >> color;
+    if (cur_pos) dp[i][0] = min(dp[i][0], dp[i - 1][0] + 1);
+    if (fi_pos && se_pos) dp[i][0] = min(dp[i][0], dp[i - 2][0] + 2);
+    if (fi_pos) dp[i][1] = min(dp[i][1], dp[i - 1][2] + 1);
+    if (se_pos) dp[i][2] = min(dp[i][2], dp[i - 1][1] + 1);
 
-      if (color)
-        edge[i].first++, edge[j].first++;
-      else
-        edge[i].second++, edge[j].second++;
-    }
+    dp[i][1] = min(dp[i][1], dp[i - 1][0] + 1);
+    dp[i][2] = min(dp[i][2], dp[i - 1][0] + 1);
+    dp[i][0] = min({dp[i][0], dp[i][1] + 1, dp[i][2] + 1, dp[i - 1][0] + 2});
   }
 
-  cout << accumulate(
-              edge.begin(), edge.end(), N * (N - 1) * (N - 2) / 3,
-              [](int x, pi p) -> int { return x - p.first * p.second; }) /
-              2
-       << '\n';
+  ans = min(ans, dp[N - finPos][finPhase]);
   return;
+}
+
+void process() {
+  int N, W;
+  cin >> N >> W;
+
+  vector<pi> tagon(N + 1);
+  for (int i = 1; i <= N; i++) cin >> tagon[i].first;
+  for (int i = 1; i <= N; i++) cin >> tagon[i].second;
+
+  int ans = numeric_limits<int>::max();
+  bool match1 = tagon[1].first + tagon[N].first <= W;
+  bool match2 = tagon[1].second + tagon[N].second <= W;
+
+  {
+    vector<vector<int>> dp(N + 1, vector<int>(3, 1e9));
+    dp[0][0] = 0;
+    dp[1][0] = tagon[1].first + tagon[1].second <= W ? 1 : 2;
+    dp[1][1] = dp[1][2] = 1;
+    update_ans(tagon, dp, N, W, ans, 0, 0);
+  }
+
+  if (match1) {
+    vector<vector<int>> dp(N + 1, vector<int>(3, 1e9));
+    dp[1][0] = 2, dp[1][1] = 1;
+    update_ans(tagon, dp, N, W, ans, 0, 2);
+  }
+
+  if (match2) {
+    vector<vector<int>> dp(N + 1, vector<int>(3, 1e9));
+    dp[1][0] = 2, dp[1][2] = 1;
+    update_ans(tagon, dp, N, W, ans, 0, 1);
+  }
+
+  if (match1 && match2) {
+    vector<vector<int>> dp(N + 1, vector<int>(3, 1e9));
+    dp[1][0] = 2;
+    update_ans(tagon, dp, N, W, ans, 1, 0);
+  }
+
+  cout << ans << '\n';
 }
 
 int main() {
@@ -62,7 +99,7 @@ int main() {
   int T;
   cin >> T;
 
-  for (int i = 0; i < T; i++) process();
+  for (int tc = 0; tc < T; tc++) process();
 
   return 0;
 }
