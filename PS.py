601874f4b0f18f5e0bc8ebb1605d4c0d33b0b186
himyu <himyuuuu1@gmail.com>
2019-11-13 17:22:10 +0900

    BOJ 1033 AC

--- PS.py
+++ PS.py
@@ -1,20 +1,43 @@
-n,l=map(int,input().split())
+from math import gcd
+from fractions import Fraction
 
-flag=False
-for i in range(l,101):
-	if i%2==1 and n%i==0:
-		st=(n//i)-(i//2)
-		if st>=0:
-			print(' '.join(map(str,range(st,st+i))))
-			flag=True
-			break
-	elif i%2==0 and (2*n)%i==0:
-		t=(2*n)//i
-		if t%2==0: continue
-		st=(t//2)-(i//2)+1
-		if st>=0:
-			print(' ' .join(map(str,range(st,st+i))))
-			flag=True
-			break
+N = int(input())
 
-if not flag: print(-1)
+l = []
+l.append(Fraction(1, 1))
+for i in range(N-1):
+    l.append(Fraction(0, 1))
+
+grp = []
+for i in range(N):
+    grp.append([])
+
+for i in range(N-1):
+    a, b, p, q = map(int, input().split())
+    grp[a].append((b, Fraction(q, p)))
+    grp[b].append((a, Fraction(p, q)))
+
+
+def dfs(u: int):
+    global l, grp
+    for v, w in grp[u]:
+        if l[v] == 0:
+            l[v] = l[u]*w
+            dfs(v)
+
+
+dfs(0)
+
+
+def lcm(a: int, b: int):
+    return a//gcd(a, b)*b
+
+
+lc = 1
+for rat in l:
+    lc = lcm(rat.denominator, lc)
+
+for i in range(N):
+    l[i] *= lc
+
+print(' '.join(map(lambda x: str(x.numerator), l)))
