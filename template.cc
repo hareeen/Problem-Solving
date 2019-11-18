025b2965b28fb5875c2b5a26c956da0bfa5203e4
himyu <himyuuuu1@gmail.com>
2019-11-18 12:35:10 +0900

    Create Template File

---
+++ template.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = long long;
+using d64 = long double;
+using pi = pair<int, int>;
+using pli = pair<i64, i64>;
+using ti = tuple<int, int, int>;
+using tli = tuple<i64, i64, i64>;
+
+#define iterall(cont) cont.begin(), cont.end()
+#define prec(n) setprecision(n) << fixed
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  return 0;
+}
