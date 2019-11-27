bbb3a9edd2a4dc25885928b0996cbc3215b9067f
himyu <himyuuuu1@gmail.com>
2019-11-27 21:45:01 +0900

    BOJ 13544 AC

--- PS.cc
+++ PS.cc
@@ -12,50 +12,62 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-d64 solve(int bit, int select, int idx, const int N, const vector<pi> &vecs) {
-  if (idx == N) {
-    if (select != N / 2) return numeric_limits<d64>::max();
+class mergeTree {
+ private:
+  vector<vector<int>> tree;
 
-    pi vecSum = {0, 0};
-    auto it = vecs.begin();
-    while (bit > 0) {
-      vecSum.first += (((bit % 2) * 2 - 1) * it->first);
-      vecSum.second += (((bit % 2) * 2 - 1) * it->second);
-      bit /= 2;
-      it++;
-    }
-    for (; it != vecs.end(); it++) {
-      vecSum.first -= it->first;
-      vecSum.second -= it->second;
-    }
-
-    auto x = static_cast<d64>(vecSum.first);
-    auto y = static_cast<d64>(vecSum.second);
-    return sqrt(x * x + y * y);
+ public:
+  mergeTree() {
+    tree.clear();
+    tree.resize(262144 + 1);
   }
-
-  return min(solve(bit * 2, select, idx + 1, N, vecs),
-             solve(bit * 2 + 1, select + 1, idx + 1, N, vecs));
-}
-
-void process() {
-  int N;
-  cin >> N;
-
-  vector<pi> arr(N);
-  for (int i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
-
-  cout << prec(12) << solve(0, 0, 0, N, arr) << '\n';
-}
+  void init(int s, int e, int node, vector<int> &arr) {
+    for (int i = s; i <= e; i++) tree[node].push_back(arr[i]);
+    sort(iterall(tree[node]));
+    if (s != e) {
+      init(s, (s + e) / 2, node * 2, arr);
+      init((s + e) / 2 + 1, e, node * 2 + 1, arr);
+    }
+  }
+  int query(int s, int e, int node, const int l, const int r, const int k) {
+    if (r < s || e < l)
+      return 0;
+    else if (l <= s && e <= r)
+      return distance(upper_bound(iterall(tree[node]), k), tree[node].end());
+    else
+      return query(s, (s + e) / 2, node * 2, l, r, k) +
+             query((s + e) / 2 + 1, e, node * 2 + 1, l, r, k);
+  }
+};
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int tc;
-  cin >> tc;
-  for (int i = 0; i < tc; i++) process();
+  int N;
+  cin >> N;
+
+  vector<int> arr(N + 1);
+  for (int i = 1; i <= N; i++) cin >> arr[i];
+
+  int Q;
+  cin >> Q;
+
+  auto mT = mergeTree();
+  mT.init(1, N, 1, arr);
+
+  int l, r, k;
+  cin >> l >> r >> k;
+
+  auto last_ans = mT.query(1, N, 1, l, r, k);
+  cout << last_ans << '\n';
+  for (int i = 1; i < Q; i++) {
+    cin >> l >> r >> k;
+    l ^= last_ans, r ^= last_ans, k ^= last_ans;
+    last_ans = mT.query(1, N, 1, l, r, k);
+    cout << last_ans << '\n';
+  }
 
   return 0;
 }
