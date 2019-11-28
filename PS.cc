734cb3ae806d515aaad312ec8521046a11f61e74
himyu <himyuuuu1@gmail.com>
2019-11-29 02:58:30 +0900

    BOJ 16978 AC

--- PS.cc
+++ PS.cc
@@ -12,38 +12,44 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-class segTree {
+pli psum(const pli l, const pli r) { return {0, l.second + r.second}; }
+
+class perSegTree {
  private:
-  vector<i64> tree;
+  vector<vector<pli>> tree;
 
  public:
-  segTree() {
+  perSegTree() {
     tree.clear();
     tree.resize(262144 + 1);
   }
-  i64 init(int s, int e, int node, const vector<i64> &arr) {
+  pli init(int s, int e, int node, const vector<i64> &arr) {
     if (s == e)
-      return tree[node] = arr[s];
+      tree[node].push_back({0, arr[s]});
     else
-      return tree[node] = init(s, (s + e) / 2, node * 2, arr) +
-                          init((s + e) / 2 + 1, e, node * 2 + 1, arr);
+      tree[node].push_back(psum(init(s, (s + e) / 2, node * 2, arr),
+                                init((s + e) / 2 + 1, e, node * 2 + 1, arr)));
+    return tree[node].front();
   }
-  void update(int s, int e, int node, const int t, const int d) {
+  void update(int s, int e, int node, const int t, const int d, const i64 cnt) {
     if (e < t || t < s) return;
     if (s != e) {
-      update(s, (s + e) / 2, node * 2, t, d);
-      update((s + e) / 2 + 1, e, node * 2 + 1, t, d);
+      update(s, (s + e) / 2, node * 2, t, d, cnt);
+      update((s + e) / 2 + 1, e, node * 2 + 1, t, d, cnt);
     }
-    tree[node] += d;
+    tree[node].push_back({cnt, tree[node].back().second + d});
   }
-  i64 query(int s, int e, int node, const int l, const int r) {
+  i64 query(int s, int e, int node, const int l, const int r, const i64 cnt) {
     if (e < l || r < s)
       return 0;
-    else if (l <= s && e <= r)
-      return tree[node];
-    else
-      return query(s, (s + e) / 2, node * 2, l, r) +
-             query((s + e) / 2 + 1, e, node * 2 + 1, l, r);
+    else if (l <= s && e <= r) {
+      auto iter = lower_bound(iterall(tree[node]),
+                              pli(cnt, numeric_limits<i64>::max()));
+      iter--;
+      return iter->second;
+    } else
+      return query(s, (s + e) / 2, node * 2, l, r, cnt) +
+             query((s + e) / 2 + 1, e, node * 2 + 1, l, r, cnt);
   }
 };
 
@@ -58,27 +64,25 @@ int main() {
   vector<i64> arr(N + 1);
   for (int i = 1; i <= N; i++) cin >> arr[i];
 
-  vector<i64> diffarr(N + 1);
-  for (int i = 1; i <= N; i++) diffarr[i] = arr[i] - arr[i - 1];
+  auto psT = perSegTree();
+  psT.init(1, N, 1, arr);
 
-  auto sT = segTree();
-  sT.init(1, N, 1, diffarr);
-
-  int Q;
+  int Q, qCount = 0;
   cin >> Q;
 
   for (int i = 0; i < Q; i++) {
-    int qtype;
-    cin >> qtype;
-    if (qtype == 1) {
-      int s, e, d;
-      cin >> s >> e >> d;
-      sT.update(1, N, 1, s, d);
-      if (e != N) sT.update(1, N, 1, e + 1, -d);
+    int qType;
+    cin >> qType;
+    if (qType == 1) {
+      int t, n;
+      cin >> t >> n;
+      qCount++;
+      // cout << t << " " << psT.query(1, N, 1, t, t, qCount) << endl;
+      psT.update(1, N, 1, t, n - psT.query(1, N, 1, t, t, qCount), qCount);
     } else {
-      int x;
-      cin >> x;
-      cout << sT.query(1, N, 1, 1, x) << '\n';
+      int k, s, e;
+      cin >> k >> s >> e;
+      cout << psT.query(1, N, 1, s, e, k) << '\n';
     }
   }
 
