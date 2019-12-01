baf460235cc935e381e281718b3f86084e5947be
himyu <himyuuuu1@gmail.com>
2019-12-01 14:20:32 +0900

    BOJ 2306 AC

--- PS.cc
+++ PS.cc
@@ -12,79 +12,31 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-pli psum(const pli l, const pli r) { return {0, l.second + r.second}; }
-
-class perSegTree {
- private:
-  vector<vector<pli>> tree;
-
- public:
-  perSegTree() {
-    tree.clear();
-    tree.resize(262144 + 1);
-  }
-  pli init(int s, int e, int node, const vector<i64> &arr) {
-    if (s == e)
-      tree[node].push_back({0, arr[s]});
-    else
-      tree[node].push_back(psum(init(s, (s + e) / 2, node * 2, arr),
-                                init((s + e) / 2 + 1, e, node * 2 + 1, arr)));
-    return tree[node].front();
-  }
-  void update(int s, int e, int node, const int t, const int d, const i64 cnt) {
-    if (e < t || t < s) return;
-    if (s != e) {
-      update(s, (s + e) / 2, node * 2, t, d, cnt);
-      update((s + e) / 2 + 1, e, node * 2 + 1, t, d, cnt);
-    }
-    tree[node].push_back({cnt, tree[node].back().second + d});
-  }
-  i64 query(int s, int e, int node, const int l, const int r, const i64 cnt) {
-    if (e < l || r < s)
-      return 0;
-    else if (l <= s && e <= r) {
-      auto iter = lower_bound(iterall(tree[node]),
-                              pli(cnt, numeric_limits<i64>::max()));
-      iter--;
-      return iter->second;
-    } else
-      return query(s, (s + e) / 2, node * 2, l, r, cnt) +
-             query((s + e) / 2 + 1, e, node * 2 + 1, l, r, cnt);
-  }
-};
-
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int N;
-  cin >> N;
+  string s;
+  cin >> s;
 
-  vector<i64> arr(N + 1);
-  for (int i = 1; i <= N; i++) cin >> arr[i];
+  int N = s.size();
+  vector<vector<int>> dp(N + 1, vector<int>(N + 1, 0));
 
-  auto psT = perSegTree();
-  psT.init(1, N, 1, arr);
-
-  int Q, qCount = 0;
-  cin >> Q;
-
-  for (int i = 0; i < Q; i++) {
-    int qType;
-    cin >> qType;
-    if (qType == 1) {
-      int t, n;
-      cin >> t >> n;
-      qCount++;
-      // cout << t << " " << psT.query(1, N, 1, t, t, qCount) << endl;
-      psT.update(1, N, 1, t, n - psT.query(1, N, 1, t, t, qCount), qCount);
-    } else {
-      int k, s, e;
-      cin >> k >> s >> e;
-      cout << psT.query(1, N, 1, s, e, k) << '\n';
+  for (int i = 2; i <= N; i++) {
+    for (int j = 0; i + j <= N; j++) {
+      if ((s[j] == 'a' && s[i + j - 1] == 't') ||
+          (s[j] == 'g' && s[i + j - 1] == 'c'))
+        dp[j][i + j] = dp[j + 1][i + j - 1] + 2;
+      for (int k = j; k <= i + j; k++)
+        dp[j][i + j] = max(dp[j][i + j], dp[j][k] + dp[k][i + j]);
     }
   }
+
+  int ans = 0;
+  for (const auto &vec : dp)
+    for (auto el : vec) ans = max(ans, el);
+  cout << ans << endl;
 
   return 0;
 }
