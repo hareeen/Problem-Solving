62a9b0b1bc74b5f350127dec6a9d9b763897f991
himyu <himyuuuu1@gmail.com>
2019-12-03 03:25:37 +0900

    BOJ 17408 AC

--- PS.cc
+++ PS.cc
@@ -12,32 +12,52 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
+const int inf = numeric_limits<int>::min() / 3;
+
+pi merge(const pi a, const pi b) {
+  vector<int> v;
+  v.push_back(a.first);
+  v.push_back(a.second);
+  v.push_back(b.first);
+  v.push_back(b.second);
+  sort(iterall(v), greater<int>());
+  return {v[0], v[1]};
+}
+
 class segTree {
  private:
-  vector<int> tree;
+  vector<pi> tree;
 
  public:
-  segTree() { tree.resize(1048576, numeric_limits<int>::max()); }
+  segTree() { tree.resize(262144, {inf, inf}); }
+
+  pi init(int s, int e, int nd, const vector<int> &arr) {
+    if (s == e)
+      return tree[nd] = {arr[s], inf};
+    else
+      return tree[nd] = merge(init(s, (s + e) / 2, nd * 2, arr),
+                              init((s + e) / 2 + 1, e, nd * 2 + 1, arr));
+  }
 
   void update(int s, int e, int nd, const int t, const int nb) {
     if (t < s || e < t)
       return;
     else if (s == e)
-      tree[nd] = nb;
+      tree[nd] = {nb, inf};
     else {
       update(s, (s + e) / 2, nd * 2, t, nb);
       update((s + e) / 2 + 1, e, nd * 2 + 1, t, nb);
-      tree[nd] = min(tree[nd * 2], tree[nd * 2 + 1]);
+      tree[nd] = merge(tree[nd * 2], tree[nd * 2 + 1]);
     }
   }
 
-  int query(int s, int e, int nd, const int l, const int r) {
-    if (r < s || e < l) return numeric_limits<int>::max();
+  pi query(int s, int e, int nd, const int l, const int r) {
+    if (r < s || e < l) return {inf, inf};
     if (l <= s && e <= r)
       return tree[nd];
     else
-      return min(query(s, (s + e) / 2, nd * 2, l, r),
-                 query((s + e) / 2 + 1, e, nd * 2 + 1, l, r));
+      return merge(query(s, (s + e) / 2, nd * 2, l, r),
+                   query((s + e) / 2 + 1, e, nd * 2 + 1, l, r));
   }
 };
 
@@ -49,34 +69,24 @@ int main() {
   int N;
   cin >> N;
 
-  vector<ti> stud(N);
-  for (int i = 1; i <= N; i++) {
-    int t;
-    cin >> t;
-    get<0>(stud[--t]) = i;
-  }
-  for (int i = 1; i <= N; i++) {
-    int t;
-    cin >> t;
-    get<1>(stud[--t]) = i;
-  }
-  for (int i = 1; i <= N; i++) {
-    int t;
-    cin >> t;
-    get<2>(stud[--t]) = i;
-  }
+  vector<int> arr(N + 1);
+  for (int i = 1; i <= N; i++) cin >> arr[i];
 
-  sort(iterall(stud));
-
-  int ans = 0;
   auto sT = segTree();
-  for (int i = 0; i < N; i++) {
-    // cout << get<1>(stud[i]) << " " << get<2>(stud[i]) << endl;
-    if (sT.query(1, N, 1, 1, get<1>(stud[i])) > get<2>(stud[i])) ans++;
-    sT.update(1, N, 1, get<1>(stud[i]), get<2>(stud[i]));
-  }
+  sT.init(1, N, 1, arr);
 
-  cout << ans << endl;
+  int Q;
+  cin >> Q;
+
+  auto ss = [](pi p) -> int { return p.first + p.second; };
+  for (int i = 0; i < Q; i++) {
+    int t, a, b;
+    cin >> t >> a >> b;
+    if (t == 1)
+      sT.update(1, N, 1, a, b);
+    else
+      cout << ss(sT.query(1, N, 1, a, b)) << '\n';
+  }
 
   return 0;
 }
