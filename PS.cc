f9a8e1e6469c345dc061f99e25b74938439e5585
himyu <himyuuuu1@gmail.com>
2019-12-10 00:51:08 +0900

    BOJ 17353 AC

--- PS.cc
+++ PS.cc
@@ -12,35 +12,89 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-class disjointSet {
+i64 su(i64 l, i64 r) {
+  if (l > r) return 0;
+  if ((r - l + 1) % 2)
+    return (l + r) / 2 * (r - l + 1);
+  else
+    return (r - l + 1) / 2 * (l + r);
+}
+i64 su(pli p) { return su(p.first, p.second); }
+
+pli split(pli p, int len) {
+  // i64 len = static_cast<i64>(le);
+  i64 f = (p.first - p.second * su(1, len - 1)) / len;
+  i64 left = f * ((len + 1) / 2) + su(1, (len + 1) / 2 - 1) * p.second;
+  // cout << f << " " << left << endl;
+  return {left, p.first - left};
+}
+
+bool intersect(int s, int e, int l, int r) { return !(e < l || r < s); }
+
+i64 tot(int s, int e, int l, int r) {
+  if (!intersect(s, e, l, r))
+    return 0;
+  else
+    return su(max(1, s - l + 1), min(e - l + 1, r - l + 1));
+}
+
+class segTree {
  private:
-  vector<int> uf, ng;
+  vector<i64> tree;
+  vector<pli> lazy;
 
  public:
-  disjointSet(int N) {
-    uf.clear();
-    uf.resize(N + 1);
-    for (int i = 0; i <= N; i++) uf[i] = i;
-
-    ng.clear();
-    ng.resize(N + 1, 1);
+  segTree() {
+    tree.clear();
+    tree.resize(262144 + 1);
+    lazy.clear();
+    lazy.resize(262144 + 1);
   }
-  int find(int x) {
-    if (x == uf[x])
-      return x;
+  i64 init(int s, int e, int nd, const vector<i64> &arr) {
+    if (s == e)
+      return tree[nd] = arr[s];
     else
-      return uf[x] = find(uf[x]);
+      return tree[nd] = init(s, (s + e) / 2, nd * 2, arr) +
+                        init((s + e) / 2 + 1, e, nd * 2 + 1, arr);
   }
-  bool sameset(int u, int v) { return find(u) == find(v); }
-  void merge(int u, int v) {
-    if (sameset(u, v)) return;
-    ng[find(v)] += ng[find(u)];
-    uf[find(u)] = uf[find(v)];
+  void propagate(int s, int e, int nd) {
+    if (lazy[nd] == pli(0, 0)) return;
+    tree[nd] += lazy[nd].first;
+
+    if (s != e) {
+      auto [pl, pr] = split(lazy[nd], e - s + 1);
+      // cout << lazy[nd].first << " " << lazy[nd].second << " " << e - s + 1
+      //      << " " << pl << " " << pr << endl;
+      lazy[nd * 2].first += pl;
+      lazy[nd * 2].second += lazy[nd].second;
+      lazy[nd * 2 + 1].first += pr;
+      lazy[nd * 2 + 1].second += lazy[nd].second;
+    }
+
+    lazy[nd] = {0, 0};
   }
-  bool commit(int u) {
-    bool ret = ng[find(u)];
-    if (ret) ng[find(u)]--;
-    return ret;
+  void update(int s, int e, int nd, const int l, const int r) {
+    propagate(s, e, nd);
+    if (!intersect(s, e, l, r)) return;
+    if (l <= s && e <= r) {
+      lazy[nd].first += tot(s, e, l, r);
+      lazy[nd].second++;
+      propagate(s, e, nd);
+      return;
+    }
+    update(s, (s + e) / 2, nd * 2, l, r);
+    update((s + e) / 2 + 1, e, nd * 2 + 1, l, r);
+    tree[nd] = tree[nd * 2] + tree[nd * 2 + 1];
+  }
+  i64 query(int s, int e, int nd, const int l, const int r) {
+    propagate(s, e, nd);
+    if (!intersect(s, e, l, r))
+      return 0;
+    else if (l <= s && e <= r)
+      return tree[nd];
+    else
+      return query(s, (s + e) / 2, nd * 2, l, r) +
+             query((s + e) / 2 + 1, e, nd * 2 + 1, l, r);
   }
 };
 
@@ -49,17 +103,32 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int N, L;
-  cin >> N >> L;
+  int N;
+  cin >> N;
 
-  vector<pi> arr(N);
-  for (int i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+  vector<i64> arr(N + 1);
+  for (int i = 1; i <= N; i++) cin >> arr[i];
 
-  auto dS = disjointSet(L);
-  for (auto [a, b] : arr) {
-    dS.merge(a, b);
-    cout << ((dS.commit(a) || dS.commit(b)) ? "LADICA" : "SMECE") << '\n';
+  auto sT = segTree();
+  sT.init(1, N, 1, arr);
+
+  int Q;
+  cin >> Q;
+
+  for (int i = 0; i < Q; i++) {
+    int t;
+    cin >> t;
+
+    if (t == 1) {
+      int l, r;
+      cin >> l >> r;
+      sT.update(1, N, 1, l, r);
+    } else {
+      int x;
+      cin >> x;
+      cout << sT.query(1, N, 1, x, x) << '\n';
+    }
   }
-  
+
   return 0;
 }
