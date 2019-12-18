747810639e81fa189250e228376c42efb20b55ad
himyu <himyuuuu1@gmail.com>
2019-12-19 07:09:38 +0900

    BOJ 17363 AC

--- PS.py
+++ PS.py
@@ -1,3 +1,2 @@
-s='.O-|/\\^<v>';S='.O|-\\/<v>^';v=[]
-exec('v+=[[S[s.index(e)]for e in input()[::-1]]];'*int(input().split()[0]))
+v=[];exec("v+=[['.O|-\\/<v>^'['.O-|/\\^<v>'.index(e)]for e in input()[::-1]]];"*int(input().split()[0]))
 for i in zip(*v):print(*i,sep='')
