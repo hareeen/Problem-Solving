fc61eb537548236b465df7da10f5e8b8a585d89d
himyu <himyuuuu1@gmail.com>
2019-12-18 10:20:54 +0900

    BOJ 17363 AC

--- PS.py
+++ PS.py
@@ -1,17 +1,3 @@
-from math import sqrt
-from decimal import *
-
-getcontext().prec = 50
-
-a, b, c = map(lambda x: Decimal(float(x)), input().split())
-s = (a+b+c)/2
-S = (s*(s-a)*(s-b)*(s-c)).sqrt()
-R = a*b*c/4/S
-r = 2*S/(a+b+c)
-d = max(Decimal(0), (R*(R-2*r))).sqrt()
-k = (R*R-a*a/4).sqrt()+(R*R-b*b/4).sqrt()+(R*R-c*c/4).sqrt()
-print(S)
-print(R)
-print(r)
-print(d)
-print(k)
+s='.O-|/\\^<v>';S='.O|-\\/<v>^';v=[]
+exec('v+=[[S[s.index(e)]for e in input()[::-1]]];'*int(input().split()[0]))
+for i in zip(*v):print(*i,sep='')
