c5d77b0b90dccf4a3bf08711b575ebf7cef2ca49
himyu <himyuuuu1@gmail.com>
2019-12-24 23:05:36 +0900

    BOJ 1280 AC

--- PS.cc
+++ PS.cc
@@ -12,89 +12,37 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-i64 su(i64 l, i64 r) {
-  if (l > r) return 0;
-  if ((r - l + 1) % 2)
-    return (l + r) / 2 * (r - l + 1);
-  else
-    return (r - l + 1) / 2 * (l + r);
-}
-i64 su(pli p) { return su(p.first, p.second); }
-
-pli split(pli p, int len) {
-  // i64 len = static_cast<i64>(le);
-  i64 f = (p.first - p.second * su(1, len - 1)) / len;
-  i64 left = f * ((len + 1) / 2) + su(1, (len + 1) / 2 - 1) * p.second;
-  // cout << f << " " << left << endl;
-  return {left, p.first - left};
-}
-
-bool intersect(int s, int e, int l, int r) { return !(e < l || r < s); }
-
-i64 tot(int s, int e, int l, int r) {
-  if (!intersect(s, e, l, r))
-    return 0;
-  else
-    return su(max(1, s - l + 1), min(e - l + 1, r - l + 1));
-}
+const i64 mod = 1e9 + 7;
+const int ed = 200001;
 
 class segTree {
  private:
   vector<i64> tree;
-  vector<pli> lazy;
 
  public:
   segTree() {
     tree.clear();
-    tree.resize(262144 + 1);
-    lazy.clear();
-    lazy.resize(262144 + 1);
+    tree.resize(524288 + 1);
   }
-  i64 init(int s, int e, int nd, const vector<i64> &arr) {
-    if (s == e)
-      return tree[nd] = arr[s];
-    else
-      return tree[nd] = init(s, (s + e) / 2, nd * 2, arr) +
-                        init((s + e) / 2 + 1, e, nd * 2 + 1, arr);
-  }
-  void propagate(int s, int e, int nd) {
-    if (lazy[nd] == pli(0, 0)) return;
-    tree[nd] += lazy[nd].first;
 
+  void update(int s, int e, int nd, const int t, const i64 d) {
+    if (t < s || e < t) return;
+    if (s <= t && t <= e) tree[nd] += d, tree[nd] %= mod;
     if (s != e) {
-      auto [pl, pr] = split(lazy[nd], e - s + 1);
-      // cout << lazy[nd].first << " " << lazy[nd].second << " " << e - s + 1
-      //      << " " << pl << " " << pr << endl;
-      lazy[nd * 2].first += pl;
-      lazy[nd * 2].second += lazy[nd].second;
-      lazy[nd * 2 + 1].first += pr;
-      lazy[nd * 2 + 1].second += lazy[nd].second;
+      update(s, (s + e) / 2, nd * 2, t, d);
+      update((s + e) / 2 + 1, e, nd * 2 + 1, t, d);
     }
+  }
 
-    lazy[nd] = {0, 0};
-  }
-  void update(int s, int e, int nd, const int l, const int r) {
-    propagate(s, e, nd);
-    if (!intersect(s, e, l, r)) return;
-    if (l <= s && e <= r) {
-      lazy[nd].first += tot(s, e, l, r);
-      lazy[nd].second++;
-      propagate(s, e, nd);
-      return;
-    }
-    update(s, (s + e) / 2, nd * 2, l, r);
-    update((s + e) / 2 + 1, e, nd * 2 + 1, l, r);
-    tree[nd] = tree[nd * 2] + tree[nd * 2 + 1];
-  }
   i64 query(int s, int e, int nd, const int l, const int r) {
-    propagate(s, e, nd);
-    if (!intersect(s, e, l, r))
+    if (l <= s && e <= r)
+      return tree[nd] % mod;
+    else if (e < l || r < s)
       return 0;
-    else if (l <= s && e <= r)
-      return tree[nd];
     else
-      return query(s, (s + e) / 2, nd * 2, l, r) +
-             query((s + e) / 2 + 1, e, nd * 2 + 1, l, r);
+      return (query(s, (s + e) / 2, nd * 2, l, r) +
+              query((s + e) / 2 + 1, e, nd * 2 + 1, l, r)) %
+             mod;
   }
 };
 
@@ -106,29 +54,24 @@ int main() {
   int N;
   cin >> N;
 
-  vector<i64> arr(N + 1);
-  for (int i = 1; i <= N; i++) cin >> arr[i];
+  auto sT1 = segTree(), sT2 = segTree();
+  i64 su = 0, ans = 1;
+  for (i64 i = 0; i < N; i++) {
+    i64 k;
+    cin >> k, k++;
 
-  auto sT = segTree();
-  sT.init(1, N, 1, arr);
+    if (i != 0)
+      ans *= ((2 * sT1.query(1, ed, 1, k, ed) - su -
+               (2 * sT2.query(1, ed, 1, k, ed) - i) * k) %
+              mod);
+    su += k;
+    su %= mod;
+    ans %= mod;
 
-  int Q;
-  cin >> Q;
-
-  for (int i = 0; i < Q; i++) {
-    int t;
-    cin >> t;
-
-    if (t == 1) {
-      int l, r;
-      cin >> l >> r;
-      sT.update(1, N, 1, l, r);
-    } else {
-      int x;
-      cin >> x;
-      cout << sT.query(1, N, 1, x, x) << '\n';
-    }
+    sT1.update(1, ed, 1, k, k);
+    sT2.update(1, ed, 1, k, 1);
   }
 
+  cout << (ans + mod) % mod << endl;
   return 0;
 }
