3679c6533fa70e2ebf9dc56091351da281c5ddb6
himyu <himyuuuu1@gmail.com>
2019-12-27 22:09:06 +0900

    BOJ 14565 AC

--- PS.cc
+++ PS.cc
@@ -12,54 +12,30 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-const i64 mod = 1e9 + 7;
+inline i64 smod(i64 a, i64 m) { return ((a % m) + m) % m; }
+inline __int128 smod(__int128 a, __int128 m) { return ((a % m) + m) % m; }
 
-void solve(i64 nd, i64 par, vector<pli> &sol,
-           const vector<vector<pli>> &graph) {
-  if (par != -1 && graph[nd].size() == 1) {
-    sol[nd] = {0, 0};
-    return;
-  }
-  for (auto [th, we] : graph[nd]) {
-    if (th == par) continue;
-    solve(th, nd, sol, graph);
-    sol[nd].first += (sol[th].first + 1) * we;
-    sol[nd].first %= mod;
-    sol[nd].second +=
-        (((sol[th].first + 1) * (sol[th].first + 1)) % mod) * we * we;
-    sol[nd].second %= mod;
-  }
+i64 gcd(i64 a, i64 b) { return a ? gcd(b % a, a) : b; }
+
+__int128 getInv(__int128 a, __int128 b) {
+  if (a <= 1)
+    return a;
+  else if (a > b)
+    return getInv(smod(a, b), b);
+  else
+    return smod((-getInv(smod(b, a), a) * b + 1) / a, b);
 }
+
+i64 inv(i64 a, i64 b) { return gcd(a, b) == 1 ? getInv(a, b) : -1; }
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  i64 N;
-  cin >> N;
+  i64 a, b;
+  cin >> a >> b;
+  cout << a - b << " " << inv(b, a) << endl;
 
-  vector<vector<pli>> graph(N);
-  for (int i = 0; i < N - 1; i++) {
-    i64 u, v, w;
-    cin >> u >> v >> w;
-    u--, v--;
-    graph[u].push_back({v, w});
-    graph[v].push_back({u, w});
-  }
-
-  vector<pli> sol(N);
-  solve(0, -1, sol, graph);
-
-  i64 ans = 0;
-  for (auto [as, bs] : sol) {
-    // cout << as << " " << bs << endl;
-    i64 tmp = ((as * as) % mod - bs);
-    if (tmp % 2) tmp += mod;
-    ans += (tmp / 2 + as);
-    ans %= mod;
-  }
-
-  cout << (ans + mod) % mod << endl;
   return 0;
 }
