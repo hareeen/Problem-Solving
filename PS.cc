36162547e66c34072d2632649ed119d95dac9add
himyu <himyuuuu1@gmail.com>
2020-01-02 02:25:19 +0900

    BOJ 1067 AC

--- PS.cc
+++ PS.cc
@@ -14,90 +14,50 @@ using tli = tuple<i64, i64, i64>;
 
 using cpx = complex<d64>;
 
-const size_t SIMPLE_LIMIT = 8;
-
-void FFT(vector<cpx> &v, cpx w) {
+void FFT(vector<cpx> &v, cpx z) {
     const auto N = v.size();
+    if (N == 1) return;
 
-    if (N <= SIMPLE_LIMIT) {
-        vector<cpx> ret(N);
-        cpx cur(1);
-        for (int i = 0; i < N; i++) {
-            cpx su(0), mul(1);
-            for (int j = 0; j < N; j++) {
-                su += (mul * v[j]);
-                mul *= cur;
-            }
-            ret[i] = su;
-            cur *= w;
-        }
-        v = ret;
-        return;
-    }
-
-    vector<cpx> even(N / 2), odd(N / 2);
+    vector<cpx> even, odd;
     for (int i = 0; i < N; i++) {
-        if (i % 2) odd[i / 2] = v[i];
-        else even[i / 2] = v[i];
+        if (i % 2) odd.push_back(v[i]);
+        else even.push_back(v[i]);
     }
 
-    FFT(even, w * w);
-    FFT(odd, w * w);
+    FFT(even, z * z), FFT(odd, z * z);
 
     cpx mul(1);
     for (int i = 0; i < N / 2; i++) {
         v[i] = even[i] + mul * odd[i];
         v[i + N / 2] = even[i] - mul * odd[i];
-        mul *= w;
+        mul *= z;
     }
 }
 
 const d64 PI = acos(-1);
 
-vector<cpx> convolution(vector<cpx> lhs, vector<cpx> rhs) {
-    i64 N = 1;
-    while (N <= lhs.size() || N <= rhs.size()) N *= 2;
+vector<cpx> convolution(vector<cpx> a, vector<cpx> b) {
+    int N = 1;
+    while (N < a.size() || N < b.size()) N *= 2;
     N *= 2;
 
-    lhs.resize(N), rhs.resize(N);
+    const auto originalSize = b.size();
+    a.resize(N), b.resize(N);
+    for (int i = 0; i < originalSize; i++) b[N - originalSize + i] = b[i];
 
-    cpx w(cos(2 * PI / N), sin(2 * PI / N));
-    FFT(lhs, w), FFT(rhs, w);
+    cpx z(cos(2 * PI / N), sin(2 * PI / N));
+    FFT(a, z), FFT(b, z);
 
-    vector<cpx> ret(N);
-    generate(iterall(ret), [&, i = -1]() mutable {
+    vector<cpx> c(N);
+    generate(iterall(c), [&, i = -1]() mutable {
         i++;
-        return lhs[i] * rhs[i];
+        return a[i] * b[i];
     });
 
-    FFT(ret, cpx(1) / w);
-    for (auto &el:ret) el /= cpx(N);
+    FFT(c, cpx(1) / z);
+    for (auto &el:c) el /= cpx(N);
 
-    return ret;
-}
-
-const int digit = 4;
-
-void cpx_slice(string &s, vector<cpx> &v) {
-    for (int i = 0; i < s.size(); i += digit) {
-        i64 su = 0, mul = 1;
-        for (int j = i; j < min(static_cast<int>(s.size()), i + digit); j++) {
-            su += (static_cast<i64>(s[j] - '0') * mul);
-            mul *= 10;
-        }
-        v.emplace_back(cpx(su));
-    }
-}
-
-string tidify(i64 x) {
-    vector<i64> v;
-    while (x) v.push_back(x % 10), x /= 10;
-    while (v.size() < digit) v.push_back(0);
-
-    string s;
-    for (auto el:v) s.push_back('0' + el);
-
-    return s;
+    return c;
 }
 
 int main() {
@@ -105,37 +65,22 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    string s1, s2;
-    cin >> s1 >> s2;
+    int N;
+    cin >> N;
 
-    reverse(iterall(s1));
-    reverse(iterall(s2));
+    vector<i64> a(N), b(N);
+    for (int i = 0; i < N; i++) cin >> a[i];
+    for (int i = 0; i < N; i++) cin >> b[i];
+    reverse(iterall(b));
 
-    bool minus = false;
-    if (s1.back() == '-') minus = !minus, s1.pop_back();
-    if (s2.back() == '-') minus = !minus, s2.pop_back();
+    vector<cpx> ac, bc;
+    transform(iterall(a), back_inserter(ac), [](i64 x) -> cpx { return cpx(x); });
+    transform(iterall(b), back_inserter(bc), [](i64 x) -> cpx { return cpx(x); });
 
-    vector<cpx> cpx1, cpx2;
-    cpx_slice(s1, cpx1);
-    cpx_slice(s2, cpx2);
+    auto cc = convolution(ac, bc);
+    vector<i64> c;
+    for (int i = 0; i < N; i++) c.push_back(round(cc[i].real()));
 
-    auto ret = convolution(cpx1, cpx2);
-    vector<i64> res(ret.size());
-    for (int i = 0; i < ret.size(); i++) res[i] = static_cast<i64>(round(ret[i].real()));
-
-    i64 mod = 1;
-    for (int i = 0; i < digit; i++) mod *= 10;
-    for (int i = 0; i < res.size() - 1; i++) {
-        res[i + 1] += (res[i] / mod), res[i] %= mod;
-        if (res[i] < 0) res[i + 1] -= 1, res[i] += mod;
-    }
-
-    string prt;
-    for (auto el:res) prt += tidify(el);
-    while (prt.back() == '0' && prt.size() > 1) prt.pop_back();
-    reverse(iterall(prt));
-
-    if (minus && prt.front() != '0') cout << '-';
-    cout << prt << endl;
+    cout << *max_element(iterall(c)) << endl;
     return 0;
 }
