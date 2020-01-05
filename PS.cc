43a6470e90e4e412ebf066a5fe6a0744edbf3fc8
himyu <himyuuuu1@gmail.com>
2020-01-05 20:14:03 +0900

    BOJ 14245 AC

--- PS.cc
+++ PS.cc
@@ -12,36 +12,88 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-inline i64 smod(i64 x, i64 m) {
-    return (x % m + m) % m;
-}
+class segTree {
+private:
+    vector<int> vl, lz;
+public:
+    segTree() {
+        vl.clear();
+        lz.clear();
+        vl.resize(1048576 + 1);
+        lz.resize(1048576 + 1);
+    }
 
-tli ext_euc(i64 a, i64 b) {
-    if (b == 0) return {a, 1, 0};
-    auto[g, x, y] = ext_euc(b, a % b);
-    return {g, y, x - (a / b) * y};
-}
+    int init(int s, int e, int nd, const vector<int> &arr) {
+        if (s == e) return vl[nd] = arr[s];
+        else return vl[nd] = init(s, (s + e) / 2, nd * 2, arr) ^ init((s + e) / 2 + 1, e, nd * 2 + 1, arr);
+    }
 
-const i64 MOD = 1e9 + 7;
+    void propagate(int s, int e, int nd) {
+        if (!lz[nd]) return;
+        vl[nd] ^= lz[nd];
+
+        if (s != e) {
+            lz[nd * 2] ^= lz[nd];
+            lz[nd * 2 + 1] ^= lz[nd];
+        }
+
+        lz[nd] = 0;
+    }
+
+    void update(int s, int e, int nd, const int l, const int r, const int d) {
+        propagate(s, e, nd);
+        if (e < l || r < s) return;
+        if (l <= s && e <= r) {
+            lz[nd] ^= d;
+            propagate(s, e, nd);
+            return;
+        }
+        update(s, (s + e) / 2, nd * 2, l, r, d);
+        update((s + e) / 2 + 1, e, nd * 2 + 1, l, r, d);
+        vl[nd] = vl[nd * 2] ^ vl[nd * 2 + 1];
+    }
+
+    int query(int s, int e, int nd, const int l, const int r) {
+        propagate(s, e, nd);
+        if (e < l || r < s) return 0;
+        if (l <= s && e <= r) return vl[nd];
+        return query(s, (s + e) / 2, nd * 2, l, r) ^ query((s + e) / 2 + 1, e, nd * 2 + 1, l, r);
+    }
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    i64 N, K;
-    cin >> N >> K;
+    int N;
+    cin >> N;
 
-    vector<i64> fact(1, 1);
-    for (int i = 1; i <= N; i++) fact.push_back(fact.back() * i), fact.back() %= MOD;
+    vector<int> arr(N + 1);
+    for (int i = 1; i <= N; i++) cin >> arr[i];
 
-    i64 ans = fact.back();
-    ans *= get<1>(ext_euc(fact[K], MOD));
-    ans = smod(ans, MOD);
-    ans *= get<1>(ext_euc(fact[N - K], MOD));
-    ans = smod(ans, MOD);
+    auto sT = segTree();
+    sT.init(1, N, 1, arr);
 
-    cout << ans << endl;
+    int Q;
+    cin >> Q;
+
+    for (int i = 0; i < Q; i++) {
+        int q;
+        cin >> q;
+
+        if (q == 1) {
+            int a, b, c;
+            cin >> a >> b >> c;
+            a++, b++;
+            sT.update(1, N, 1, a, b, c);
+        } else {
+            int a;
+            cin >> a;
+            a++;
+            cout << sT.query(1, N, 1, a, a) << '\n';
+        }
+    }
 
     return 0;
 }
