ddb168479278c93b95093cbb7e3f56482e09ba8b
himyu <himyuuuu1@gmail.com>
2020-01-24 10:44:15 +0900

    BOJ 12795 AC

--- PS.cc
+++ PS.cc
@@ -19,26 +19,103 @@ using ordered_set =
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
+const i64 inf = numeric_limits<i64>::max() / 3;
+
+class Line {
+   public:
+    i64 a, b;
+
+    Line() { a = 0, b = -inf; }
+
+    Line(i64 _a, i64 _b) { a = _a, b = _b; }
+
+    inline i64 get(i64 x) { return a * x + b; }
+};
+
+class Node {
+   public:
+    i64 s, e;
+    Line v;
+    Node *l, *r;
+
+    Node() {
+        s = e = 0;
+        v = Line();
+        l = r = nullptr;
+    }
+
+    Node(i64 _s, i64 _e) {
+        s = _s, e = _e;
+        v = Line();
+        l = r = nullptr;
+    }
+
+    Node(i64 _s, i64 _e, i64 _a, i64 _b) {
+        s = _s, e = _e;
+        v = Line(_a, _b);
+        l = r = nullptr;
+    }
+};
+
+namespace LiChao {
+Node* init(i64 s, i64 e) { return new Node(s, e); }
+
+void update(Node* here, const Line d) {
+    i64 s = here->s, e = here->e;
+    i64 m = (s + e) / 2;
+
+    auto lo = here->v, hi = d;
+    if (lo.get(s) > hi.get(s)) swap(lo, hi);
+
+    if (lo.get(e) < hi.get(e)) {
+        here->v = hi;
+        return;
+    }
+
+    if (lo.get(m) < hi.get(m)) {
+        here->v = hi;
+        if (!here->r) here->r = new Node(m + 1, e);
+        update(here->r, lo);
+    } else {
+        here->v = lo;
+        if (!here->l) here->l = new Node(s, m);
+        update(here->l, hi);
+    }
+}
+
+i64 query(Node* here, const i64 x) {
+    if (!here) return -inf;
+
+    i64 s = here->s, e = here->e;
+    i64 m = (s + e) / 2;
+
+    return max(here->v.get(x), x <= m ? query(here->l, x) : query(here->r, x));
+}
+};  // namespace LiChao
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    i64 N;
+    int N;
     cin >> N;
 
-    vector<i64> fibo(2, 1);
-    int fn = 2;
-    for (; fibo[fn - 1] < 1e15;)
-        fibo.push_back(fibo[fn - 1] + fibo[fn - 2]), ++fn;
+    auto root = LiChao::init(-1e12, 1e12);
+    for (int i = 0; i < N; i++) {
+        int t;
+        cin >> t;
 
-    auto n = N;
-    auto it = prev(fibo.end());
-    while (n > 0) {
-        while (*it > n) --it;
-        n -= *it;
+        if (t == 1) {
+            i64 a, b;
+            cin >> a >> b;
+            LiChao::update(root, Line(a, b));
+        } else {
+            i64 x;
+            cin >> x;
+            cout << LiChao::query(root, x) << '\n';
+        }
     }
 
-    cout << (*it == N ? -1 : *it) << endl;
     return 0;
 }
