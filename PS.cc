bc21902bded709605a02ce899dd910fbe71e050d
himyu <himyuuuu1@gmail.com>
2020-01-26 00:30:04 +0900

    BOJ 2519 AC

--- PS.cc
+++ PS.cc
@@ -18,75 +18,6 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-<<<<<<< HEAD
-const i64 inf = numeric_limits<i64>::max() / 3;
-
-class Line {
-   public:
-    i64 a, b;
-
-    Line() { a = 1, b = -inf; }
-
-    Line(i64 _a, i64 _b) { a = _a, b = _b; }
-
-    inline i64 get(i64 x) { return a * x + b; }
-};
-
-class Node {
-   public:
-    i64 s, e;
-    Line v;
-    Node *l, *r;
-
-    Node() {
-        s = e = 0;
-        v = Line();
-        l = r = nullptr;
-    }
-
-    Node(i64 _s, i64 _e) {
-        s = _s, e = _e;
-        v = Line();
-        l = r = nullptr;
-    }
-};
-
-namespace LiChao {
-Node* init(i64 s, i64 e) { return new Node(s, e); }
-
-void update(Node* hr, const Line l) {
-    i64 s = hr->s, e = hr->e;
-    i64 m = (s + e) / 2;
-
-    auto lo = hr->v, hi = l;
-    if (lo.get(s) > hi.get(s)) swap(lo, hi);
-
-    if (lo.get(e) <= hi.get(e)) {
-        hr->v = hi;
-        return;
-    }
-
-    if (lo.get(m) < hi.get(m)) {
-        hr->v = hi;
-        if (!hr->r) hr->r = new Node(m + 1, e);
-        update(hr->r, lo);
-    } else {
-        hr->v = lo;
-        if (!hr->l) hr->l = new Node(s, m);
-        update(hr->l, hi);
-    }
-}
-
-i64 query(Node* hr, const i64 x) {
-    if (!hr) return -inf;
-
-    i64 s = hr->s, e = hr->e;
-    i64 m = (s + e) / 2;
-
-    return max(hr->v.get(x), x <= m ? query(hr->l, x) : query(hr->r, x));
-}
-};  // namespace LiChao
-=======
 #define x first
 #define y second
 
@@ -161,7 +92,6 @@ vector<vector<int>> SCC(const vector<vector<int>> &graph1,
 
     return ret;
 };
->>>>>>> 2942a67c145d1230c901d1e8b281a0d9f5675738
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -171,21 +101,6 @@ int main() {
     int N;
     cin >> N;
 
-<<<<<<< HEAD
-    vector<i64> v(N);
-    for (int i = 0; i < N; i++) cin >> v[i];
-
-    i64 _su = 0;
-    i64 _ssu = 0;
-    auto root = LiChao::init(-2e12, 2e12);
-
-    vector<i64> d(N);
-    for (int i = 0; i < N; i++) {
-        LiChao::update(root, Line(-i, i * _su - _ssu));
-        _su += v[i];
-        _ssu += (i + 1) * v[i];
-        d[i] = max(d[i], LiChao::query(root, _su) + _ssu);
-=======
     vector<pair<pli, pli>> segs(3 * N);
     for (int i = 0; i < 3 * N; i++)
         cin >> segs[i].first.x >> segs[i].first.y >> segs[i].second.x >>
@@ -288,9 +203,7 @@ int main() {
     if (res.size()) {
         copy(iterall(res), ostream_iterator<int>(cout, " "));
         cout << endl;
->>>>>>> 2942a67c145d1230c901d1e8b281a0d9f5675738
     }
 
-    cout << *max_element(iterall(d)) << endl;
     return 0;
 }
