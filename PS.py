8327c9ef7d33ab5abddadecc082dc695af0ff5c2
himyu <himyuuuu1@gmail.com>
2020-01-27 13:11:55 +0900

    BOJ 13949 AC

--- PS.py
+++ PS.py
@@ -1,11 +1,42 @@
-N=int(input())
-s=input()
-p=1
-res=0
-for i in range(N):
-    res+=(p*(ord(s[i])-ord('a')+1))
-    res%=1234567891
-    p*=31
-    p%=1234567891
+k, n = map(int, input().split())
+q = [[1, k, k*(k+1)]]
+qa = [True]
+s = {1, k, k*(k+1)}
+p = 0
+t = 0
+lim = 10**100
 
-print(res)  
+
+def qapp(tup):
+    global q, t
+    tup.sort()
+    if tup[-1] >= lim or tup[0] <= -lim:
+        return
+    if tup in q:
+        return
+
+    q.append(tup)
+    qa.append(False)
+
+    if tup[0] > 0 and not (tup[0] in s or tup[1] in s or tup[2] in s):
+        t += 1
+        qa[-1] = True
+        s.add(tup[0])
+        s.add(tup[1])
+        s.add(tup[2])
+
+
+while t < n:
+    cur = q[p]
+    qapp([k*(cur[1]+cur[2])-cur[0], cur[1], cur[2]])
+    qapp([cur[0], k*(cur[0]+cur[2])-cur[1], cur[2]])
+    qapp([cur[0], cur[1], k*(cur[0]+cur[1])-cur[2]])
+    p += 1
+
+m = 0
+for i in range(len(q)):
+    if m == n:
+        break
+    if qa[i]:
+        m += 1
+        print(*q[i])
