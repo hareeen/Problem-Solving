76f9946c23d2ce0969606a2fd1a97574cfd8214d
himyu <himyuuuu1@gmail.com>
2020-01-27 12:44:15 +0900

    BOJ 13510 AC

--- PS.cc
+++ PS.cc
@@ -12,86 +12,166 @@ using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
 
-template<class T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-#define x first
-#define y second
+class segTree {
+   private:
+    vector<int> vl;
 
-pli operator-(const pli &lhs, const pli &rhs) {
-    return {lhs.x - rhs.x, lhs.y - rhs.y};
-}
+   public:
+    segTree() { vl.resize(262144 + 1); }
 
-inline i64 ccw(pli p1, pli p2, pli p3) {
-    auto[x1, y1] = p2 - p1;
-    auto[x2, y2] = p3 - p1;
-
-    return x1 * y2 - x2 * y1;
-}
-
-i64 dir(pli p1, pli p2, pli p3) {
-    auto c = ccw(p1, p2, p3);
-
-    if (c > 0)
-        return 1;
-    else if (c == 0)
-        return 0;
-    else
-        return -1;
-}
-
-i64 isIntersect(pli p1, pli p2, pli q1, pli q2) {
-    return dir(p1, p2, q1) * dir(p1, p2, q2) < 0 &&
-           dir(q1, q2, p1) * dir(q1, q2, p2) < 0;
-}
-
-void dfs1(int here, vector<bool> &vst, stack<int> &trace,
-          const vector<vector<int>> &graph) {
-    for (auto there : graph[here]) {
-        if (!vst[there]) vst[there] = true, dfs1(there, vst, trace, graph);
-    }
-
-    trace.push(here);
-}
-
-void dfs2(int here, vector<bool> &vst, vector<int> &group,
-          const vector<vector<int>> &graph) {
-    for (auto there : graph[here]) {
-        if (!vst[there]) vst[there] = true, dfs2(there, vst, group, graph);
-    }
-
-    group.push_back(here);
-}
-
-vector<vector<int>> SCC(const vector<vector<int>> &graph1,
-                        const vector<vector<int>> &graph2) {
-    const int V = graph1.size();
-
-    vector<bool> vst(V);
-    stack<int> trace;
-    for (int i = 0; i < V; i++)
-        if (!vst[i]) {
-            vst[i] = true, dfs1(i, vst, trace, graph1);
+    void update(int s, int e, int nd, const int t, const int d) {
+        if (t < s || e < t) return;
+        if (s == e) {
+            vl[nd] = d;
+            return;
         }
 
-    vector<vector<int>> ret;
-    vst.clear(), vst.resize(V);
-    while (!trace.empty()) {
-        if (!vst[trace.top()]) {
-            vector<int> group;
-            vst[trace.top()] = true;
-            dfs2(trace.top(), vst, group, graph2);
-            sort(iterall(group));
-            ret.push_back(group);
-        }
-        trace.pop();
+        update(s, (s + e) / 2, nd * 2, t, d);
+        update((s + e) / 2 + 1, e, nd * 2 + 1, t, d);
+        vl[nd] = max(vl[nd * 2], vl[nd * 2 + 1]);
     }
 
-    return ret;
+    int query(int s, int e, int nd, const int l, const int r) {
+        if (r < s || e < l) return -1;
+        if (l <= s && e <= r) return vl[nd];
+        return max(query(s, (s + e) / 2, nd * 2, l, r),
+                   query((s + e) / 2 + 1, e, nd * 2 + 1, l, r));
+    }
 };
+
+namespace HLD {
+
+int N;
+vector<vector<int>> adj, tr, spt;
+vector<int> dep, sz;
+vector<int> hld, hc, hld_tp;
+vector<int> in, out;
+segTree sT;
+
+int hld_sz = 0, order = 0;
+
+void init(const int _N) {
+    N = _N;
+
+    adj.resize(N + 1), tr.resize(N + 1), spt.resize(N + 1);
+    dep.resize(N + 1), sz.resize(N + 1);
+
+    hld.resize(N + 1), hc.resize(N + 1);
+    in.resize(N + 1), out.resize(N + 1);
+
+    sT = segTree();
+}
+
+void emplace_edge(int u, int v) {
+    adj[u].emplace_back(v);
+    adj[v].emplace_back(u);
+}
+
+void dfs1(int here, int par) {
+    sz[here] = 1;
+    for (auto there : adj[here]) {
+        if (par == there) continue;
+
+        tr[here].emplace_back(there);
+        dfs1(there, here);
+        sz[here] += sz[there];
+    }
+
+    if (par == -1) adj.clear();
+}
+
+void dfs2(int here, int par) {
+    if (par == -1) {
+        dep[here] = 1;
+    } else {
+        dep[here] = dep[par] + 1;
+        spt[here].emplace_back(par);
+    }
+
+    for (int i = 1; (1 << i) < dep[here]; i++)
+        spt[here].emplace_back(spt[spt[here][i - 1]][i - 1]);
+
+    for (auto there : tr[here]) dfs2(there, here);
+}
+
+void dfs3(int here, bool _init = false) {
+    if (_init) {
+        order = 0;
+        hld_sz = 0;
+        hld_tp.emplace_back(here);
+    }
+
+    in[here] = ++order;
+
+    for (auto there : tr[here]) {
+        if (sz[there] * 2 >= sz[here]) {
+            hld[there] = hld[here];
+            hc[here] = there;
+            dfs3(there);
+            break;
+        }
+    }
+
+    for (auto there : tr[here]) {
+        if (sz[there] * 2 < sz[here]) {
+            hld[there] = ++hld_sz;
+            hld_tp.emplace_back(there);
+            dfs3(there);
+        }
+    }
+
+    out[here] = order;
+}
+
+int lca(int u, int v) {
+    if (dep[u] > dep[v]) swap(u, v);
+
+    int pos = spt[v].size() - 1;
+    while (dep[u] < dep[v]) {
+        while ((1 << pos) > dep[v] - dep[u]) --pos;
+        v = spt[v][pos];
+    }
+
+    while (true) {
+        if (u == v) return u;
+        if (spt[u][0] == spt[v][0]) return spt[u][0];
+
+        pos = spt[u].size() - 1;
+        while (spt[u][pos] == spt[v][pos]) --pos;
+
+        u = spt[u][pos];
+        v = spt[v][pos];
+    }
+}
+
+void update(int u, int v, const int d) {
+    if (dep[u] > dep[v]) swap(u, v);
+    sT.update(1, N, 1, in[v], d);
+}
+
+int query(int u, int v, bool disjoint = false) {
+    if (disjoint) {
+        auto L = lca(u, v);
+        return max(query(u, L), query(v, L));
+    }
+
+    if (u == v) return 0;
+    if (dep[u] > dep[v]) swap(u, v);
+
+    if (hld[hc[u]] == hld[v]) return sT.query(1, N, 1, in[hc[u]], in[v]);
+
+    return max(query(u, spt[hld_tp[hld[v]]][0]),
+               sT.query(1, N, 1, in[hld_tp[hld[v]]], in[v]));
+}
+
+};  // namespace HLD
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -101,108 +181,36 @@ int main() {
     int N;
     cin >> N;
 
-    vector<pair<pli, pli>> segs(3 * N);
-    for (int i = 0; i < 3 * N; i++)
-        cin >> segs[i].first.x >> segs[i].first.y >> segs[i].second.x >>
-            segs[i].second.y;
+    vector<pi> edg(N);
+    vector<int> wei(N);
+    for (int i = 1; i < N; i++) cin >> edg[i].first >> edg[i].second >> wei[i];
 
-    vector<vector<int>> graph1(6 * N), graph2(6 * N);
-    const int _not = 3 * N;
+    HLD::init(N);
+    for (auto [u, v] : edg) HLD::emplace_edge(u, v);
 
-    auto tsat_append = [&graph1, &graph2, _not](int u, int v, bool dau = true) {
-        graph1[u].push_back(v);
-        if (dau)
-            graph1[(_not + v) % (2 * _not)].push_back((_not + u) % (2 * _not));
+    HLD::dfs1(1, -1);
+    HLD::dfs2(1, -1);
+    HLD::dfs3(1, true);
 
-        graph2[v].push_back(u);
-        if (dau)
-            graph2[(_not + u) % (2 * _not)].push_back((_not + v) % (2 * _not));
-    };
+    for (int i = 1; i < N; i++)
+        HLD::update(edg[i].first, edg[i].second, wei[i]);
 
-    for (int i = 0; i < N; i++) {
-        tsat_append(_not + 3 * i, 3 * i + 1);
-        tsat_append(_not + 3 * i + 1, 3 * i + 2);
-        tsat_append(_not + 3 * i + 2, 3 * i);
-    }
+    int Q;
+    cin >> Q;
 
-    for (int i = 0; i < 3 * N; i++) {
-        for (int j = i + 1; j < 3 * N; j++) {
-            if (isIntersect(segs[i].first, segs[i].second,
-                            segs[j].first, segs[j].second))
-                tsat_append(i, _not + j);
+    for (int i = 0; i < Q; i++) {
+        int t;
+        cin >> t;
+
+        if (t == 1) {
+            int j, c;
+            cin >> j >> c;
+            HLD::update(edg[j].first, edg[j].second, c);
+        } else {
+            int u, v;
+            cin >> u >> v;
+            cout << HLD::query(u, v, true) << '\n';
         }
-    }
-
-    auto scc = SCC(graph1, graph2);
-    const int S = scc.size();
-
-    vector<int> con(2 * _not);
-    for (int i = 0; i < S; i++) {
-        for (auto v : scc[i]) con[v] = i;
-    }
-
-    for (int i = 0; i < _not; i++) {
-        if (con[i] == con[i + _not]) {
-            cout << -1 << endl;
-            return 0;
-        }
-    }
-
-    vector<vector<bool>> graph(S, vector<bool>(S));
-    for (int u = 0; u < 2 * _not; u++) {
-        for (auto v : graph1[u]) graph[con[u]][con[v]] = true;
-    }
-
-    for (int i = 0; i < S; i++) graph[i][i] = false;
-
-    vector<int> indeg(S);
-    for (int i = 0; i < S; i++) {
-        for (int j = 0; j < S; j++) {
-            if (graph[i][j]) indeg[j]++;
-        }
-    }
-
-    vector<bool> vst(S), assigned(2 * _not), ans(2 * _not);
-
-    queue<int> que;
-    for (int i = 0; i < S; i++) {
-        if (indeg[i] == 0) {
-            que.push(i);
-            vst[i] = true;
-        }
-    }
-
-    while (!que.empty()) {
-        auto cur = que.front();
-        que.pop();
-
-        for (auto v:scc[cur]) {
-            if (!assigned[v]) {
-                assigned[v] = true;
-                assigned[(_not + v) % (2 * _not)] = true;
-
-                ans[v] = false;
-                ans[(_not + v) % (2 * _not)] = true;
-            }
-        }
-
-        for (int i = 0; i < S; i++) {
-            if (!graph[cur][i]) continue;
-            if (--indeg[i] == 0 && !vst[i]) {
-                que.push(i);
-                vst[i] = true;
-            }
-        }
-    }
-
-    vector<int> res;
-    for (int i = _not; i < 2 * _not; i++)
-        if (ans[i]) res.push_back(i - _not + 1);
-
-    cout << res.size() << endl;
-    if (res.size()) {
-        copy(iterall(res), ostream_iterator<int>(cout, " "));
-        cout << endl;
     }
 
     return 0;
