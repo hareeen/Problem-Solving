dc6a04a500d03e0fdd0b99da93ac41cf3d9e5f42
himyu <himyuuuu1@gmail.com>
2020-02-18 23:26:04 +0900

    BOJ 10641 AC

--- PS.cc
+++ PS.cc
@@ -19,80 +19,59 @@ using ordered_set =
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-using qli = tuple<i64, i64, i64, i64>;
-#define pack(n) su[n], ls[n], rs[n], vl[n]
-
-const i64 inf = numeric_limits<i64>::max() / 4;
+vector<i64> pzip;
 
 class segTree {
    public:
-    vector<i64> su;
-    vector<i64> ls;
-    vector<i64> rs;
-    vector<i64> vl;
-    const i64 ini = -100001;
-    size_t SZ = 0;
+    vector<i64> vl, lz;
+    size_t sz;
 
-    segTree() {
-        const size_t s = (1 << 18) + 1;
-        su.resize(s);
-        ls.resize(s);
-        rs.resize(s);
-        vl.resize(s);
+    segTree(size_t _sz) {
+        vl.resize((1 << 20) + 1);
+        lz.resize((1 << 20) + 1);
+        sz = _sz;
     }
 
-    qli mer(qli l, qli r) {
-        auto [lsu, lls, lrs, lvl] = l;
-        auto [rsu, rls, rrs, rvl] = r;
+    void propagate(int s, int e, int n) {
+        if (!lz[n]) return;
+        vl[n] += (pzip[e] - pzip[s - 1]) * lz[n];
 
-        i64 tsu = lsu + rsu;
-        i64 tls = max(lls, lsu + rls);
-        i64 trs = max(rrs, rsu + lrs);
-        i64 tvl = max({tls, trs, lrs + rls, lvl, rvl});
+        if (s != e) {
+            lz[n * 2] += lz[n];
+            lz[n * 2 + 1] += lz[n];
+        }
 
-        return {tsu, tls, trs, tvl};
+        lz[n] = 0;
     }
 
-    void init(int s, int e, int n) {
-        if (n == 1) SZ = e;
+    void update(int s, int e, int n, const int l, const int r) {
+        propagate(s, e, n);
 
-        if (s == e) {
-            vl[n] = ls[n] = rs[n] = 0;
-            su[n] = ini;
+        if (r < s || e < l) return;
+        if (l <= s && e <= r) {
+            lz[n]++;
+            propagate(s, e, n);
             return;
         }
 
-        init(s, (s + e) / 2, n * 2);
-        init((s + e) / 2 + 1, e, n * 2 + 1);
-        tie(pack(n)) = mer({pack(n * 2)}, {pack(n * 2 + 1)});
+        update(s, (s + e) / 2, n * 2, l, r);
+        update((s + e) / 2 + 1, e, n * 2 + 1, l, r);
+
+        vl[n] = vl[n * 2] + vl[n * 2 + 1];
     }
 
-    void update(int s, int e, int n, const int t, const i64 u = 1LL) {
-        if (t < s || e < t) return;
+    void update(const int l, const int r) { update(1, sz, 1, l, r); }
 
-        if (s == e) {
-            vl[n] = ls[n] = rs[n] = max(u, 0LL);
-            su[n] = u;
-            return;
-        }
+    i64 query(int s, int e, int n, const int l, const int r) {
+        propagate(s, e, n);
 
-        update(s, (s + e) / 2, n * 2, t, u);
-        update((s + e) / 2 + 1, e, n * 2 + 1, t, u);
-        tie(pack(n)) = mer({pack(n * 2)}, {pack(n * 2 + 1)});
+        if (r < s || e < l) return 0;
+        if (l <= s && e <= r) return vl[n];
+        return query(s, (s + e) / 2, n * 2, l, r) +
+               query((s + e) / 2 + 1, e, n * 2 + 1, l, r);
     }
 
-    void update(const int t) { update(1, SZ, 1, t); }
-
-    qli query(int s, int e, int n, const int l, const int r) {
-        if (r < s || e < l) return {0, -inf, -inf, -inf};
-        if (l <= s && e <= r) return {pack(n)};
-        return mer(query(s, (s + e) / 2, n * 2, l, r),
-                   query((s + e) / 2 + 1, e, n * 2 + 1, l, r));
-    }
-
-    i64 query(const int l, const int r) {
-        return get<3>(query(1, SZ, 1, l, r));
-    }
+    i64 query(const int l, const int r) { return query(1, sz, 1, l, r); }
 };
 
 int main() {
@@ -100,69 +79,83 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N;
-    cin >> N;
+    i64 N, M, Q;
+    cin >> N >> M >> Q;
 
-    vector<pli> hist(N);
-    for (int i = 0; i < N; i++) {
-        cin >> hist[i].first;
-        hist[i].second = i + 1;
+    vector<tli> upds;
+    for (int i = 0; i < M; i++) {
+        i64 l, r, v;
+        cin >> l >> r >> v;
+        upds.emplace_back(v, l, r + 1);
+        pzip.emplace_back(l);
+        pzip.emplace_back(r + 1);
     }
+    sort(iterall(upds));
 
-    sort(iterall(hist), greater<>());
+    vector<i64> zip;
+    for (auto [v, l, r] : upds) zip.emplace_back(v);
+    zip.erase(unique(iterall(zip)), zip.end());
 
-    vector<i64> comp(N);
-    for (int i = 0; i < N; i++) comp[i] = hist[i].first;
-    reverse(iterall(comp));
-    comp.erase(unique(iterall(comp)), comp.end());
+    for (auto& [v, l, r] : upds) v = lower_bound(iterall(zip), v) - zip.begin();
 
-    for (auto& [h, _] : hist) h = lower_bound(iterall(comp), h) - comp.begin();
-
-    int H = comp.size();
-
-    int Q;
-    cin >> Q;
-
-    vector<tli> queries(Q);
+    vector<tli> queries;
     for (int i = 0; i < Q; i++) {
-        i64 l, r, w;
-        cin >> l >> r >> w;
-        queries[i] = {l, r, w};
+        i64 l, r, j;
+        cin >> l >> r >> j;
+
+        queries.emplace_back(l, r + 1, j);
+        pzip.emplace_back(l);
+        pzip.emplace_back(r + 1);
     }
 
-    vector<int> lo(Q), hi(Q, H);
+    sort(iterall(pzip));
+    pzip.erase(unique(iterall(pzip)), pzip.end());
+
+    for (auto& [v, l, r] : upds) {
+        l = lower_bound(iterall(pzip), l) - pzip.begin() + 1;
+        r = lower_bound(iterall(pzip), r) - pzip.begin();
+    }
+
+    for (auto& [l, r, j] : queries) {
+        l = lower_bound(iterall(pzip), l) - pzip.begin() + 1;
+        r = lower_bound(iterall(pzip), r) - pzip.begin();
+    }
+
+    const auto nu = zip.size();
+    vector<int> lo(Q), hi(Q, nu - 1);
+
     while (true) {
         bool flag = true;
-        vector<vector<int>> proc(H + 1);
-
+        vector<vector<int>> proc(nu);
         for (int i = 0; i < Q; i++) {
             if (lo[i] < hi[i]) {
                 flag = false;
-                proc[(lo[i] + hi[i] + 1) / 2].emplace_back(i);
+                proc[(lo[i] + hi[i]) / 2].emplace_back(i);
             }
         }
 
         if (flag) break;
 
-        auto sT = segTree();
-        sT.init(1, N, 1);
-
         int p = 0;
-        for (int h = H; h >= 0; --h) {
-            for (; p < N && hist[p].first == h; ++p) {
-                sT.update(hist[p].second);
+        auto sT = segTree(pzip.size() - 1);
+
+        for (int n = 0; n < nu; n++) {
+            while (p < M && n == get<0>(upds[p])) {
+                sT.update(get<1>(upds[p]), get<2>(upds[p]));
+                ++p;
             }
-            for (auto i : proc[h]) {
-                auto [l, r, w] = queries[i];
-                if (sT.query(l, r) >= w)
-                    lo[i] = h;
+
+            for (auto i : proc[n]) {
+                auto [l, r, j] = queries[i];
+                if (sT.query(l, r) < j)
+                    lo[i] = n + 1;
                 else
-                    hi[i] = h - 1;
+                    hi[i] = n;
             }
         }
     }
 
-    for (int i = 0; i < Q; i++) cout << comp[lo[i]] << '\n';
+    for (int i = 0; i < Q; i++) cout << zip[lo[i]] << '\n';
 
     return 0;
 }
