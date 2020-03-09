cf94d87976b1488d887d1eaf333669fbf6f84fea
himyu <himyuuuu1@gmail.com>
2020-03-09 22:31:33 +0900

    BOJ 1150 AC

--- PS.cc
+++ PS.cc
@@ -19,83 +19,70 @@ using ordered_set =
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-const i64 inf = numeric_limits<i64>::max() / 3;
-
-vector<i64> dijkstra(int sv, const vector<vector<pli>> &adj) {
-    vector<i64> dist(adj.size(), inf);
-    priority_queue<pli, vector<pli>, greater<>> pq;
-    dist[sv] = 0;
-    pq.emplace(0, sv);
-
-    while (!pq.empty()) {
-        auto [W, u] = pq.top();
-        pq.pop();
-
-        if (W > dist[u]) continue;
-        for (auto [v, w] : adj[u]) {
-            if (dist[v] > W + w) {
-                pq.emplace(W + w, v);
-                dist[v] = W + w;
-            }
-        }
-    }
-
-    return dist;
-}
-
-pair<vector<i64>, vector<i64>> adv_dijkstra(int sv,
-                                            const vector<vector<pli>> &adj,
-                                            const vector<i64> &td) {
-    vector<i64> dist(adj.size() + 1, inf), tm(adj.size() + 1, inf);
-    priority_queue<tli, vector<tli>, greater<>> pq;
-    dist[sv] = 0, tm[sv] = td[sv];
-    pq.emplace(0, td[sv], sv);
-
-    while (!pq.empty()) {
-        auto [W, M, u] = pq.top();
-        pq.pop();
-
-        if (W > dist[u] || M > tm[u]) continue;
-        for (auto [v, w] : adj[u]) {
-            if (dist[v] > W + w ||
-                (dist[v] == W + w && tm[v] > min(M, td[v]))) {
-                pq.emplace(W + w, min(M, td[v]), v);
-                dist[v] = W + w;
-                tm[v] = min(M, td[v]);
-            }
-        }
-    }
-
-    return {dist, tm};
-}
+set<pi> st;
+list<pi> ls;
+vector<decltype(ls)::iterator> vt;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N, M, S, T, U, V;
-    cin >> N >> M >> S >> T >> U >> V;
+    int N, K;
+    cin >> N >> K;
 
-    vector<vector<pli>> adj(N + 1);
-    for (int i = 0; i < M; i++) {
-        i64 u, v, w;
-        cin >> u >> v >> w;
-        adj[u].emplace_back(v, w);
-        adj[v].emplace_back(u, w);
+    vector<int> _arr(N);
+    for (int i = 0; i < N; i++) cin >> _arr[i];
+
+    vector<int> arr(N - 1);
+    for (int i = 0; i < N - 1; i++) arr[i] = _arr[i + 1] - _arr[i];
+
+    _arr.clear();
+
+    for (int i = 0; i < N - 1; i++) {
+        st.emplace(arr[i], i);
+        ls.emplace_back(arr[i], i);
+        vt.emplace_back(prev(ls.end()));
     }
 
-    auto ud = dijkstra(U, adj);
-    auto vd = dijkstra(V, adj);
-    auto [sd, svm] = adv_dijkstra(S, adj, vd);
-    auto [td, tvm] = adv_dijkstra(T, adj, vd);
+    arr.clear();
 
-    auto minDis = sd[T];
-    auto ans = ud[V];
+    i64 ans = 0;
+    for (int i = 0; i < K; i++) {
+        auto be = st.begin();
+        ans += be->first;
 
-    for (int i = 1; i <= N; i++) {
-        if (minDis != sd[i] + td[i]) continue;
-        ans = min(ans, ud[i] + min(svm[i], tvm[i]));
+        if (i == K - 1) break;
+
+        auto it = vt[be->second];
+        if (it == ls.begin()) {
+            st.erase(st.lower_bound(ls.front()));
+            ls.pop_front();
+            st.erase(st.lower_bound(ls.front()));
+            ls.pop_front();
+        } else if (it == prev(ls.end())) {
+            st.erase(st.lower_bound(ls.back()));
+            ls.pop_back();
+            st.erase(st.lower_bound(ls.back()));
+            ls.pop_back();
+        } else {
+            i64 toap = -be->first;
+            auto idx = be->second;
+
+            st.erase(st.lower_bound(*it));
+            it = ls.erase(it);
+            toap += it->first;
+            st.erase(st.lower_bound(*it));
+            it = prev(ls.erase(it));
+            toap += it->first;
+            st.erase(st.lower_bound(*it));
+            it = ls.erase(it);
+
+            toap = min(toap, static_cast<i64>(numeric_limits<int>::max()));
+            ls.insert(it, {toap, idx});
+            st.emplace(toap, idx);
+            vt[idx] = prev(it);
+        }
     }
 
     cout << ans << endl;
