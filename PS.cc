1932c8395073ef519fab6a0479a9eb2f53c43d31
himyu <himyuuuu1@gmail.com>
2020-03-10 22:42:14 +0900

    BOJ 4002 AC

--- PS.cc
+++ PS.cc
@@ -19,72 +19,106 @@ using ordered_set =
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-set<pi> st;
-list<pi> ls;
-vector<decltype(ls)::iterator> vt;
+class group {
+   public:
+    priority_queue<i64> pq;
+    i64 lim, su;
+
+    group(i64 _lim) {
+        lim = _lim;
+        su = 0;
+    }
+
+    void validate() {
+        while (!pq.empty() && su > lim) {
+            su -= pq.top();
+            pq.pop();
+        }
+    }
+
+    void add(i64 x) {
+        pq.emplace(x);
+        su += x;
+        validate();
+    }
+
+    i64 get() { return pq.size(); }
+
+    void merge(group* rhs) {
+        while (!rhs->pq.empty()) {
+            pq.emplace(rhs->pq.top());
+            rhs->pq.pop();
+        }
+        su += rhs->su;
+
+        validate();
+        delete rhs;
+    }
+};
+
+vector<int> sz;
+vector<i64> cost, ld;
+vector<vector<int>> tr;
+
+void alignTree(int h) {
+    sz[h] = 1;
+
+    for (auto t : tr[h]) {
+        alignTree(t);
+        sz[h] += sz[t];
+    }
+
+    sort(iterall(tr[h]), [](int l, int r) { return sz[l] > sz[r]; });
+}
+
+vector<i64> res;
+group* solve(int h, const i64 LIM) {
+    if (tr[h].empty()) {
+        auto ret = new group(LIM);
+        ret->add(cost[h]);
+        res[h] = ret->get();
+        return ret;
+    }
+
+    auto ret = solve(tr[h][0], LIM);
+    for (int i = 1; i < tr[h].size(); i++) ret->merge(solve(tr[h][i], LIM));
+    ret->add(cost[h]);
+    res[h] = ret->get();
+    return ret;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N, K;
-    cin >> N >> K;
+    int N;
+    i64 M;
+    cin >> N >> M;
 
-    vector<int> _arr(N);
-    for (int i = 0; i < N; i++) cin >> _arr[i];
+    res.resize(N + 1);
+    sz.resize(N + 1);
+    tr.resize(N + 1);
+    cost.resize(N + 1);
+    ld.resize(N + 1);
 
-    vector<int> arr(N - 1);
-    for (int i = 0; i < N - 1; i++) arr[i] = _arr[i + 1] - _arr[i];
+    int root = -1;
+    for (int i = 1; i <= N; i++) {
+        i64 p, c, l;
+        cin >> p >> c >> l;
 
-    _arr.clear();
-
-    for (int i = 0; i < N - 1; i++) {
-        st.emplace(arr[i], i);
-        ls.emplace_back(arr[i], i);
-        vt.emplace_back(prev(ls.end()));
+        if (p == 0) root = i;
+        tr[p].emplace_back(i);
+        cost[i] = c;
+        ld[i] = l;
     }
 
-    arr.clear();
+    alignTree(root);
+    solve(root, M);
 
-    i64 ans = 0;
-    for (int i = 0; i < K; i++) {
-        auto be = st.begin();
-        ans += be->first;
-
-        if (i == K - 1) break;
-
-        auto it = vt[be->second];
-        if (it == ls.begin()) {
-            st.erase(st.lower_bound(ls.front()));
-            ls.pop_front();
-            st.erase(st.lower_bound(ls.front()));
-            ls.pop_front();
-        } else if (it == prev(ls.end())) {
-            st.erase(st.lower_bound(ls.back()));
-            ls.pop_back();
-            st.erase(st.lower_bound(ls.back()));
-            ls.pop_back();
-        } else {
-            i64 toap = -be->first;
-            auto idx = be->second;
-
-            st.erase(st.lower_bound(*it));
-            it = ls.erase(it);
-            toap += it->first;
-            st.erase(st.lower_bound(*it));
-            it = prev(ls.erase(it));
-            toap += it->first;
-            st.erase(st.lower_bound(*it));
-            it = ls.erase(it);
-
-            toap = min(toap, static_cast<i64>(numeric_limits<int>::max()));
-            ls.insert(it, {toap, idx});
-            st.emplace(toap, idx);
-            vt[idx] = prev(it);
-        }
-    }
-
+    i64 ans = numeric_limits<i64>::min();
+    for (int i = 1; i <= N; i++) ans = max(ans, res[i] * ld[i]);
     cout << ans << endl;
+
     return 0;
 }
