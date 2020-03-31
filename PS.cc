cea14acd23e35bb99069b6839c79e6284ec60762
himyu <himyuuuu1@gmail.com>
2020-04-01 01:03:16 +0900

    BOJ 13350 AC

--- PS.cc
+++ PS.cc
@@ -21,150 +21,119 @@ using ordered_set =
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-const int sz = 2000000;
-vector<int> graph1[2000000], graph2[2000000];
+class sqrtDecomp {
+   public:
+    int N, sq;
+    // sd[i] : [sq*i, sq*(i+1))
+    vector<int> vl, sd;
 
-void dfs1(int here, vector<bool> &vst, stack<int> &trace) {
-    for (auto there : graph1[here]) {
-        if (!vst[there]) vst[there] = true, dfs1(there, vst, trace);
+    sqrtDecomp(int _N) {
+        N = _N;
+        sq = sqrt(N);
+
+        vl.resize(N + 1);
+        sd.resize(N / sq + 1);
     }
 
-    trace.push(here);
-}
-
-void dfs2(int here, vector<bool> &vst, vector<int> &group) {
-    for (auto there : graph2[here]) {
-        if (!vst[there]) vst[there] = true, dfs2(there, vst, group);
+    void update(int t, int d) {
+        vl[t] += d;
+        sd[t / sq] += d;
     }
 
-    group.push_back(here);
-}
-
-bool SCC(const int V) {
-    vector<bool> vst(V);
-    stack<int> trace;
-    for (int i = 0; i < V; i++) {
-        if (!vst[i]) vst[i] = true, dfs1(i, vst, trace);
-    }
-
-    for (int i = 0; i < V; i++) {
-        for (auto el : graph1[i]) graph2[el].emplace_back(i);
-        graph1[i].clear();
-    }
-
-    vector<int> sccn(V);
-    vst.clear(), vst.resize(V);
-    while (!trace.empty()) {
-        if (!vst[trace.top()]) {
-            vector<int> group;
-            vst[trace.top()] = true;
-            dfs2(trace.top(), vst, group);
-            for (auto el : group) sccn[el] = trace.size();
+    int query() {
+        for (int i = N / sq; i >= 0; i--) {
+            if (!sd[i]) continue;
+            for (int j = min(i * sq + sq - 1, N); j >= i * sq; j--)
+                if (vl[j]) return j;
         }
-        trace.pop();
-    }
 
-    for (int i = 0; i < V / 2; i++)
-        if (sccn[i] == sccn[i + V / 2]) return false;
-    return true;
+        return 0;
+    }
 };
-
-void process() {
-    int N, M;
-    cin >> N >> M;
-
-    vector<string> vec(N);
-    int bn = 0, wn = 0;
-    for (int i = 0; i < N; i++) {
-        cin >> vec[i];
-        for (auto el : vec[i]) {
-            if (el == 'B') bn++;
-            if (el == 'W') wn++;
-        }
-    }
-
-    if (wn != bn * 2) {
-        cout << "NO" << endl;
-        return;
-    }
-
-    const int nt = 4 * N * M;
-    fill(graph2, graph2 + sz, vector<int>());
-    auto sat_append = [&](int u, int v, bool b = true) {
-        graph1[u].emplace_back(v);
-        if (b) graph1[(nt + v) % (2 * nt)].emplace_back((nt + u) % (2 * nt));
-    };
-
-    const int t = 0, l = 1, r = 2, b = 3;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            auto ch = (i * M + j) * 4;
-            if (i != 0) {
-                sat_append(ch + t, ch - M * 4 + b);
-                sat_append(ch - M * 4 + b, ch + t);
-            }
-            if (j != 0) {
-                sat_append(ch + l, ch - 4 + r);
-                sat_append(ch - 4 + r, ch + l);
-            }
-            if (j != M - 1) {
-                sat_append(ch + r, ch + 4 + l);
-                sat_append(ch + 4 + l, ch + r);
-            }
-            if (i != N - 1) {
-                sat_append(ch + b, ch + M * 4 + t);
-                sat_append(ch + M * 4 + t, ch + b);
-            }
-
-            if (vec[i][j] == '.') {
-                if (i != 0) sat_append(ch + t, nt + ch + t, false);
-                if (j != 0) sat_append(ch + l, nt + ch + l, false);
-                if (j != M - 1) sat_append(ch + r, nt + ch + r, false);
-                if (i != N - 1) sat_append(ch + b, nt + ch + b, false);
-                continue;
-            }
-
-            auto tc = i != 0 ? vec[i - 1][j] : '.';
-            auto lc = j != 0 ? vec[i][j - 1] : '.';
-            auto rc = j != M - 1 ? vec[i][j + 1] : '.';
-            auto bc = i != N - 1 ? vec[i + 1][j] : '.';
-
-            if (vec[i][j] == 'B') {
-                if (tc != 'W') sat_append(ch + t, nt + ch + t, false);
-                if (lc != 'W') sat_append(ch + l, nt + ch + l, false);
-                if (rc != 'W') sat_append(ch + r, nt + ch + r, false);
-                if (bc != 'W') sat_append(ch + b, nt + ch + b, false);
-                sat_append(ch + t, nt + ch + b);
-                sat_append(ch + l, nt + ch + r);
-                sat_append(nt + ch + r, ch + l);
-                sat_append(nt + ch + b, ch + t);
-            }
-            if (vec[i][j] == 'W') {
-                if (tc != 'B') sat_append(ch + t, nt + ch + t, false);
-                if (lc != 'B') sat_append(ch + l, nt + ch + l, false);
-                if (rc != 'B') sat_append(ch + r, nt + ch + r, false);
-                if (bc != 'B') sat_append(ch + b, nt + ch + b, false);
-                sat_append(ch + t, nt + ch + l);
-                sat_append(ch + t, nt + ch + r);
-                sat_append(ch + t, nt + ch + b);
-                sat_append(ch + l, nt + ch + r);
-                sat_append(ch + l, nt + ch + b);
-                sat_append(ch + r, nt + ch + b);
-            }
-        }
-    }
-    vec.clear();
-
-    cout << (SCC(2 * nt) ? "YES" : "NO") << endl;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int T;
-    cin >> T;
+    int N, K;
+    cin >> N >> K;
 
-    for (int i = 0; i < T; i++) process();
+    vector<list<int>> v(K);
+    vector<int> arr(N + 1);
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+        arr[i] += arr[i - 1];
+        arr[i] %= K;
+    }
+
+    int Q;
+    cin >> Q;
+
+    vector<ti> queries;
+    for (int i = 0; i < Q; i++) {
+        int s, e;
+        cin >> s >> e;
+        queries.emplace_back(--s, e, i);
+    }
+
+    int sqN = sqrt(N);
+    sort(iterall(queries), [sqN](const ti &l, const ti &r) {
+        auto [s1, e1, i1] = l;
+        auto [s2, e2, i2] = r;
+        s1 /= sqN;
+        s2 /= sqN;
+        return s1 == s2 ? e1 < e2 : s1 < s2;
+    });
+
+    vector<int> ret(Q);
+    ti lq = {-1, -1, -1};
+    auto sD = sqrtDecomp(N);
+
+    for (auto [s, e, id] : queries) {
+        if (get<0>(lq) == -1) {
+            for (int i = s; i <= e; i++) v[arr[i]].emplace_back(i);
+            for (const auto &l : v) {
+                if (l.empty()) continue;
+                sD.update(l.back() - l.front(), 1);
+            }
+        } else {
+            auto [ls, le, _] = lq;
+            if (s < ls)
+                for (int i = ls - 1; i >= s; i--) {
+                    if (!v[arr[i]].empty())
+                        sD.update(v[arr[i]].back() - v[arr[i]].front(), -1);
+                    v[arr[i]].emplace_front(i);
+                    sD.update(v[arr[i]].back() - v[arr[i]].front(), 1);
+                }
+            if (le < e)
+                for (int i = le + 1; i <= e; i++) {
+                    if (!v[arr[i]].empty())
+                        sD.update(v[arr[i]].back() - v[arr[i]].front(), -1);
+                    v[arr[i]].emplace_back(i);
+                    sD.update(v[arr[i]].back() - v[arr[i]].front(), 1);
+                }
+            if (s > ls)
+                for (int i = ls; i < s; i++) {
+                    sD.update(v[arr[i]].back() - v[arr[i]].front(), -1);
+                    v[arr[i]].pop_front();
+                    if (!v[arr[i]].empty())
+                        sD.update(v[arr[i]].back() - v[arr[i]].front(), 1);
+                }
+            if (le > e)
+                for (int i = le; i > e; i--) {
+                    sD.update(v[arr[i]].back() - v[arr[i]].front(), -1);
+                    v[arr[i]].pop_back();
+                    if (!v[arr[i]].empty())
+                        sD.update(v[arr[i]].back() - v[arr[i]].front(), 1);
+                }
+        }
+
+        lq = {s, e, id};
+        ret[id] = sD.query();
+    }
+
+    copy(iterall(ret), ostream_iterator<decltype(ret)::value_type>(cout, "\n"));
+
+    return 0;
 }
--- PS.py
+++ PS.py
@@ -1,13 +1,1 @@
-
-
-def solve1D(l: list):
-    N = len(l)
-    p = 0
-    ret = []
-    
-    while p<N-3:
-        if l[p+1] != l[p+2]:
-            ret.append([p+1, p+2])
-            l[p+1] = l[p+2] = (l[p+1]+l[p+2])%3
-        
-
+x=int(input());print(*['*'*x]+['*'+' '*(x-2)+'*']*(x-2)+['*'*x],sep='\n')
