5dcfcdbccfc72f026f4749310c3423ffe4f8e01b
himyu <himyu@himyu-iMac.local>
2020-05-31 19:27:02 +0900

    template update

--- PS.cc
+++ PS.cc
@@ -1,12 +1,9 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include "bits/stdc++.h"
 
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("unroll-loops")
 
 using namespace std;
-using namespace __gnu_pbds;
 
 using i64 = long long;
 using d64 = long double;
@@ -14,9 +11,6 @@ using pi = pair<int, int>;
 using pli = pair<i64, i64>;
 using ti = tuple<int, int, int>;
 using tli = tuple<i64, i64, i64>;
-
-template<class T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
