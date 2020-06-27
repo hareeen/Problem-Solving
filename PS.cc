3d99171f979dcb1c38b998923f26c380d22a3b8c
himyu <himyuuuu1@gmail.com>
2020-06-27 13:06:47 +0900

    BOJ 10124 AC

--- PS.cc
+++ PS.cc
@@ -15,58 +15,46 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-class disjointSet {
-   private:
-    vector<int> par;
-    int N;
-
-   public:
-    explicit disjointSet(int _N) {
-        N = _N;
-        par.resize(N + 1);
-        for (int i = 0; i <= N; i++) par[i] = i;
-    }
-
-    int find(int u) { return par[u] = (u == par[u] ? u : find(par[u])); }
-
-    bool sset(int u, int v) { return find(u) == find(v); }
-
-    void merge(int u, int v) { par[find(u)] = find(v); }
-};
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    random_device rd;
+    mt19937 rnd(rd());
+
     int N, M;
     cin >> N >> M;
 
-    auto dS = disjointSet(N);
-    vector<int> mask(N);
+    vector<int> v(N);
+    for (int i = 0; i < N; i++) cin >> v[i];
+
+    vector<vector<int>> idx(N + 1);
+    for (int i = 0; i < N; i++) idx[v[i]].emplace_back(i);
+
     for (int i = 0; i < M; i++) {
-        int u, v;
-        cin >> u >> v;
-        --u, --v;
-        dS.merge(u, v);
-        mask[u] = 1 - mask[u];
-        mask[v] = 1 - mask[v];
+        int a, b;
+        cin >> a >> b;
+        --a, --b;
+
+        vector<int> s(25);
+        uniform_int_distribution<int> dist(a, b);
+        for (int j = 0; j < 25; j++) s[j] = v[dist(rnd)];
+        sort(iterall(s));
+        s.erase(unique(iterall(s)), s.end());
+
+        for (auto el:s) {
+            int hm = upper_bound(iterall(idx[el]), b) - lower_bound(iterall(idx[el]), a);
+            if (hm * 2 > b - a + 1) {
+                cout << el << '\n';
+                goto next;
+            }
+        }
+
+        cout << 0 << '\n';
+        next:;
     }
 
-    vector<vector<int>> grp(N);
-    for (int i = 0; i < N; i++) grp[dS.find(i)].emplace_back(i);
 
-    int ans = 0;
-    for (const auto &vec : grp) {
-        if(vec.size() < 2) continue;
-        // copy(iterall(vec), ostream_iterator<int>(cout, " "));
-        // cout<<endl;
-
-        int s = 0;
-        for (auto el : vec) s += mask[el];
-        ans += max(1, s >> 1);
-    }
-
-    cout << ans << endl;
     return 0;
 }
