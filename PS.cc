87b22506508392f454ac0b227921f96fba082488
himyu <himyuuuu1@gmail.com>
2020-06-28 22:57:21 +0900

    BOJ 12963 AC

--- PS.cc
+++ PS.cc
@@ -15,46 +15,61 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
+class disjointSet {
+private:
+    int N;
+    vector<int> par;
+public:
+    explicit disjointSet(int _N) {
+        N = _N;
+        par.resize(N + 1);
+        for (int i = 0; i <= N; i++) par[i] = i;
+    }
+
+    int find(int x) {
+        return par[x] = (par[x] == x ? x : find(par[x]));
+    }
+
+    void merge(int x, int y) {
+        par[find(x)] = find(y);
+    }
+
+    bool sset(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    random_device rd;
-    mt19937 rnd(rd());
+    const i64 MOD = 1e9 + 7;
 
     int N, M;
     cin >> N >> M;
 
-    vector<int> v(N);
-    for (int i = 0; i < N; i++) cin >> v[i];
+    vector<i64> exp3(M, 1);
+    for (int i = 1; i < M; i++) exp3[i] = exp3[i - 1] * 3, exp3[i] %= MOD;
 
-    vector<vector<int>> idx(N + 1);
-    for (int i = 0; i < N; i++) idx[v[i]].emplace_back(i);
-
+    vector<pi> queries;
     for (int i = 0; i < M; i++) {
-        int a, b;
-        cin >> a >> b;
-        --a, --b;
-
-        vector<int> s(25);
-        uniform_int_distribution<int> dist(a, b);
-        for (int j = 0; j < 25; j++) s[j] = v[dist(rnd)];
-        sort(iterall(s));
-        s.erase(unique(iterall(s)), s.end());
-
-        for (auto el:s) {
-            int hm = upper_bound(iterall(idx[el]), b) - lower_bound(iterall(idx[el]), a);
-            if (hm * 2 > b - a + 1) {
-                cout << el << '\n';
-                goto next;
-            }
-        }
-
-        cout << 0 << '\n';
-        next:;
+        int u, v;
+        cin >> u >> v;
+        queries.emplace_back(u, v);
     }
 
+    i64 ans = 0;
+    auto dS = disjointSet(N);
+    for (int i = M - 1; i >= 0; i--) {
+        auto[u, v] = queries[i];
+        if (dS.sset(u, 0) && dS.sset(v, N - 1)) ans += exp3[i];
+        else if (dS.sset(v, 0) && dS.sset(u, N - 1)) ans += exp3[i];
+        else dS.merge(u, v);
 
+        ans %= MOD;
+    }
+
+    cout << ans << endl;
     return 0;
 }
