6346096ae9d658cbd91eb6fed0a4cad06b07b9fc
hamerin <hamerin04@gmail.com>
2021-07-19 22:43:52 +0900

    BOJ 10908 AC

--- PS.py
+++ PS.py
@@ -1,24 +1,45 @@
+M = int(1e9 + 7)
+mod = M**16
+
+
 def ext_euc(a, b):
     if b == 0:
         return (a, 1, 0)
-
     g, x, y = ext_euc(b, a % b)
     return (g, y, x-(a//b)*y)
 
 
-def powmod(b, e, m):
-    b %= m
-    ret: int = 1
-    while e > 0:
-        if e & 1:
-            ret = (ret * b) % m
-        b = (b * b) % m
-        e >>= 1
-
-    return ret
+def matmul(l, r):
+    return [[(l[0][0]*r[0][0]+l[0][1]*r[1][0]) % mod, (l[0][0]*r[0][1]+l[0][1]*r[1][1]) % mod],
+            [(l[1][0]*r[0][0]+l[1][1]*r[1][0]) % mod, (l[1][0]*r[0][1]+l[1][1]*r[1][1]) % mod]]
 
 
-m = int(1e9 + 7)
-for i in range(int(input())):
-    r, g, b, k = map(int, input().split())
-    print((r*(1-powmod(b*ext_euc(b+1, m)[1],k,m))+k*(b+g)*ext_euc(b, m)[1]) % m)
+def fibo(n):
+    if n == -1:
+        return 1
+
+    base = [[1, 1], [1, 0]]
+    target = [[1, 0], [0, 1]]
+    while n:
+        if n % 2:
+            target = matmul(target, base)
+        base = matmul(base, base)
+        n //= 2
+
+    return target[0][1]
+
+
+N, K = map(int, input().split())
+
+Fnk = fibo(N*K)
+Fk = fibo(K)
+Fnkm1 = fibo(N*K-1)
+Fkm1 = fibo(K-1)
+
+while Fk % M == 0 and Fnk % M == 0:
+    Fk //= M
+    Fnk //= M
+
+A = (Fnk * ext_euc(Fk, mod)[1]) % M
+B = (Fnkm1 - A * Fkm1) % M
+print(A, B)
