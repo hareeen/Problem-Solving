09ff548f3d38646b137019e40cf6eaf071197d15
hamerin <hamerin04@gmail.com>
2021-07-20 16:41:13 +0900

    BOJ 17476 AC

--- PS.cc
+++ PS.cc
@@ -15,61 +15,190 @@ using tli = tuple<i64, i64, i64>;
 #define iterall(cont) cont.begin(), cont.end()
 #define prec(n) setprecision(n) << fixed
 
-constexpr int pinf = numeric_limits<int>::max() / 2;
-constexpr int ninf = numeric_limits<int>::min() / 2;
-
-vector<int> dijkstra(const vector<vector<pi>>& G, const vector<int>& ini) {
-    const int V = G.size();
-    vector<int> dist(V, pinf);
-
-    priority_queue<pi, vector<pi>, greater<pi>> pq;
-    for (auto el : ini) {
-        pq.emplace(0, el);
-        dist[el] = 0;
+inline i64 fsqrt(i64 x) {
+    i64 s = 1, e = min(x, static_cast<i64>(numeric_limits<int>::max()));
+    while (s < e) {
+        i64 m = ((s + e) >> 1) + 1;
+        if (x >= m * m)
+            s = m;
+        else
+            e = m - 1;
     }
 
-    while (!pq.empty()) {
-        auto [W, h] = pq.top();
-        pq.pop();
-
-        if (dist[h] < W) continue;
-
-        for (auto [t, w] : G[h]) {
-            if (dist[t] > W + w) {
-                dist[t] = W + w;
-                pq.emplace(W + w, t);
-            }
-        }
-    }
-
-    return dist;
+    return s;
 }
 
-class DisjointSet {
+class Node {
+   public:
+    i64 mx, mi, lsq, su, lz;
+    int mxct;
+    bool lsqt;
+
+    Node() = default;
+    Node(i64 _mx, i64 _mi, int _mxct, i64 _su, i64 _lsq = 0, bool _lsqt = false, i64 _lz = 0) {
+        tie(mx, mi, mxct, su, lsq, lsqt, lz) = {_mx, _mi, _mxct, _su, _lsq, _lsqt, _lz};
+    }
+};
+
+Node operator+(const Node& l, const Node& r) {
+    if (l.mx == r.mx)
+        return {l.mx, min(l.mi, r.mi), l.mxct + r.mxct, l.su + r.su};
+    else if (l.mx > r.mx)
+        return {l.mx, min(l.mi, r.mi), l.mxct, l.su + r.su};
+    else
+        return {r.mx, min(l.mi, r.mi), r.mxct, l.su + r.su};
+}
+
+class SegTree {
    public:
     int N;
-    vector<int> p;
+    vector<Node> vl;
 
-    explicit DisjointSet(int _N) {
+    SegTree() = default;
+    SegTree(int _N) {
         N = _N;
-        p.reserve(N);
-        for (int i = 0; i < N; i++) p.emplace_back(i);
+        vl.resize((N << 2) + 1);
     }
 
-    inline int find(int u) {
-        return p[u] = (p[u] == u ? u : find(p[u]));
+    Node init(int s, int e, int n, const vector<i64>& A) {
+        if (s == e) return vl[n] = {A[s], A[s], 1, A[s]};
+
+        int m = (s + e) >> 1, k = n << 1;
+        return vl[n] = init(s, m, k, A) + init(m + 1, e, k + 1, A);
+    }
+    void init(const vector<i64>& A) {
+        init(1, N, 1, A);
     }
 
-    inline void mer(int u, int v) {
-        p[find(u)] = find(v);
+    void propagate(int s, int e, int n) {
+        i64 len = e - s + 1;
+
+        if (s != e) {
+            int k = n << 1;
+
+            for (auto i : {k, k + 1}) {
+                if (vl[n].lsq) {
+                    if (vl[n].lsqt) {
+                        vl[i].lsq = vl[n].lsq;
+                        vl[i].lsqt = true;
+                    } else {
+                        i64 tmx, tmi;
+
+                        if (vl[i].lsqt)
+                            tmx = tmi = vl[i].lsq + vl[i].lz;
+                        else if (vl[i].lsq)
+                            tmx = vl[i].lsq + vl[i].lz, tmi = vl[i].lsq + vl[i].lz - 1;
+                        else
+                            tmx = vl[i].mx + vl[i].lz, tmi = vl[i].mi + vl[i].lz;
+
+                        if (tmx == tmi) {
+                            if (tmx == vl[n].mx)
+                                vl[i].lsq = vl[n].lsq;
+                            else
+                                vl[i].lsq = vl[n].lsq - 1;
+
+                            vl[i].lsqt = true;
+                        } else {
+                            vl[i].lsq = vl[n].lsq;
+                            vl[i].lsqt = false;
+                        }
+                    }
+
+                    vl[i].lz = 0;
+                }
+
+                if (vl[n].lz) {
+                    vl[i].lz += vl[n].lz;
+                }
+            }
+        }
+
+        if (vl[n].lsq) {
+            if (vl[n].lsqt) {
+                vl[n].mx = vl[n].mi = vl[n].lsq;
+                vl[n].su = vl[n].lsq * len;
+                vl[n].mxct = len;
+            } else {
+                // mx - mi = 1, there exists integer k that satisfies k^2 = mx
+                vl[n].mx = vl[n].lsq;
+                vl[n].mi = vl[n].lsq - 1;
+                vl[n].su = (vl[n].lsq - 1) * len + vl[n].mxct;
+            }
+        }
+
+        if (vl[n].lz) {
+            vl[n].mx += vl[n].lz;
+            vl[n].mi += vl[n].lz;
+            vl[n].su += vl[n].lz * len;
+        }
+        
+        vl[n].lsq = 0;
+        vl[n].lsqt = false;
+        vl[n].lz = 0;
     }
 
-    inline bool sset(int u, int v) {
-        return find(u) == find(v);
+    void update_sum(int s, int e, int n, int l, int r, i64 d) {
+        propagate(s, e, n);
+
+        if (r < s || e < l) return;
+        if (l <= s && e <= r) {
+            vl[n].lz = d;
+            propagate(s, e, n);
+            return;
+        }
+
+        int m = (s + e) >> 1, k = n << 1;
+        update_sum(s, m, k, l, r, d);
+        update_sum(m + 1, e, k + 1, l, r, d);
+        vl[n] = vl[k] + vl[k + 1];
+    }
+    void update_sum(int l, int r, i64 d) {
+        update_sum(1, N, 1, l, r, d);
     }
 
-    inline void clear() {
-        for (int i = 0; i < N; i++) p[i] = i;
+    void update_sqrt(int s, int e, int n, int l, int r) {
+        propagate(s, e, n);
+
+        if (r < s || e < l) return;
+        if (l <= s && e <= r) {
+            i64 mxs = fsqrt(vl[n].mx);
+            i64 mis = fsqrt(vl[n].mi);
+
+            if (mxs == mis) {
+                vl[n].lsq = mxs;
+                vl[n].lsqt = true;
+                propagate(s, e, n);
+                return;
+            }
+
+            if (vl[n].mx - vl[n].mi == 1) {
+                vl[n].lsq = mxs;
+                vl[n].lsqt = false;
+                propagate(s, e, n);
+                return;
+            }
+        }
+
+        int m = (s + e) >> 1, k = n << 1;
+        update_sqrt(s, m, k, l, r);
+        update_sqrt(m + 1, e, k + 1, l, r);
+        vl[n] = vl[k] + vl[k + 1];
+    }
+    void update_sqrt(int l, int r) {
+        update_sqrt(1, N, 1, l, r);
+    }
+
+    i64 query(int s, int e, int n, int l, int r) {
+        propagate(s, e, n);
+
+        if (r < s || e < l) return 0LL;
+        if (l <= s && e <= r) return vl[n].su;
+
+        int m = (s + e) >> 1, k = n << 1;
+        return query(s, m, k, l, r) + query(m + 1, e, k + 1, l, r);
+    }
+    i64 query(int l, int r) {
+        return query(1, N, 1, l, r);
     }
 };
 
@@ -78,112 +207,43 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N, M, K, Q;
-    cin >> N >> M >> K >> Q;
+    int N;
+    cin >> N;
 
-    vector<pi> E(M);
-    vector<vector<pi>> G(N);
-    for (int i = 0; i < M; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        --u, --v;
+    vector<i64> A(N + 1);
+    for (int i = 1; i <= N; i++) cin >> A[i];
 
-        E[i] = {u, v};
-        G[u].emplace_back(v, w);
-        G[v].emplace_back(u, w);
-    }
+    SegTree ST(N);
+    ST.init(A);
 
-    vector<int> festival(K);
-    for (auto& el : festival) {
-        cin >> el;
-        --el;
-    }
+    int Q;
+    cin >> Q;
 
-    vector<pi> queries(Q);
-    for (auto& [u, v] : queries) {
-        cin >> u >> v;
-        --u, --v;
-    }
+    while (Q--) {
+        int qt;
+        cin >> qt;
 
-    vector<pi> D(N);
-    vector<int> dist = dijkstra(G, festival);
-    for (int i = 0; i < N; i++) D[i] = {dist[i], i};
-    sort(iterall(D), greater<>());
-    sort(iterall(E), [&dist](const pi& lhs, const pi& rhs) {
-        return min(dist[lhs.first], dist[lhs.second]) > min(dist[rhs.first], dist[rhs.second]);
-    });
+        int L, R;
+        i64 X;
+        switch (qt) {
+            case 1:
+                cin >> L >> R >> X;
+                ST.update_sum(L, R, X);
+                break;
 
-    DisjointSet DS(M);
+            case 2:
+                cin >> L >> R;
+                ST.update_sqrt(L, R);
+                break;
 
-    vector<pi> validE;
-    vector<int> requiredDist;
-    validE.reserve(M);
-    requiredDist.reserve(M);
-    for (auto [u, v] : E) {
-        if (DS.sset(u, v)) continue;
-        DS.mer(u, v);
-        validE.emplace_back(u, v);
-        requiredDist.emplace_back(min(dist[u], dist[v]));
-    }
-    validE.shrink_to_fit();
-    requiredDist.shrink_to_fit();
-    const int VM = validE.size();
-    DS.clear();
+            case 3:
+                cin >> L >> R;
+                cout << ST.query(L, R) << "\n";
+                break;
 
-    vector<int> lo(Q), hi(Q, N - 1);
-    {
-        vector<vector<int>> qri(N);
-        for (int i = 0; i < Q; i++) {
-            auto [u, v] = queries[i];
-            qri[u].emplace_back(i);
-            qri[v].emplace_back(i);
+            default:
+                break;
         }
-
-        for (int j = 0; j < N; j++) {
-            for (auto i : qri[D[j].second]) {
-                lo[i] = j;
-            }
-        }
-    }
-
-    vector<vector<int>> pbs(N);
-
-    while (true) {
-        bool termination = true;
-        int cur = 0;
-
-        for (int i = 0; i < Q; i++) {
-            if (lo[i] != hi[i]) {
-                termination = false;
-                pbs[(lo[i] + hi[i]) >> 1].emplace_back(i);
-            }
-        }
-
-        if (termination) break;
-
-        for (int j = 0; j < N; j++) {
-            while (cur < VM && requiredDist[cur] >= D[j].first) {
-                auto [u, v] = validE[cur];
-                DS.mer(u, v);
-                ++cur;
-            }
-
-            for (auto i : pbs[j]) {
-                auto [u, v] = queries[i];
-
-                if (DS.sset(u, v))
-                    hi[i] = j;
-                else
-                    lo[i] = j + 1;
-            }
-        }
-
-        for (auto& vec : pbs) vec.clear();
-        DS.clear();
-    }
-
-    for (int i = 0; i < Q; i++) {
-        cout << D[lo[i]].first << '\n';
     }
 
     return 0;
